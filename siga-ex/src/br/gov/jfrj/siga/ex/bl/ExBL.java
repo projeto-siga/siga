615e05ed05bc57c5105948866d750c1782bf7215
ogi <orlandogomes@jfrj.jus.br>
2012-05-17 15:42:14 -0300

    Alteração do método duplicarDocumento em EXbl.java, incluindo o método getPessoaAtual em novoDoc.setLotaSubscritor e novoDoc.setSubscritor.

--- siga-ex/src/br/gov/jfrj/siga/ex/bl/ExBL.java
+++ siga-ex/src/br/gov/jfrj/siga/ex/bl/ExBL.java
@@ -2558,7 +2558,7 @@ public class ExBL extends CpBL {
 				&& !doc.getLotaDestinatario().isFechada())
 			novoDoc.setLotaDestinatario(doc.getLotaDestinatario());
 
-		novoDoc.setLotaSubscritor(doc.getLotaSubscritor());
+		novoDoc.setLotaSubscritor(doc.getSubscritor().getPessoaAtual().getLotacao());
 		novoDoc.setNmArqDoc(doc.getNmArqDoc());
 		novoDoc.setNmDestinatario(doc.getNmDestinatario());
 		novoDoc.setNmFuncaoSubscritor(doc.getNmFuncaoSubscritor());
@@ -2570,7 +2570,7 @@ public class ExBL extends CpBL {
 		novoDoc.setOrgaoExternoDestinatario(doc.getOrgaoExternoDestinatario());
 		novoDoc.setExMobilPai(doc.getExMobilPai());
 		novoDoc.setOrgaoUsuario(doc.getOrgaoUsuario());
-		novoDoc.setSubscritor(doc.getSubscritor());
+		novoDoc.setSubscritor(doc.getSubscritor().getPessoaAtual());
 		novoDoc.setTitular(doc.getTitular());
 		novoDoc.setLotaTitular(doc.getLotaTitular());
 		novoDoc.setNumAntigoDoc(doc.getNumAntigoDoc());

4c21ec5b02b2f777d7c207fc0c86573a06071b03
tah <crivano@jfrj.jus.br>
2012-10-30 12:31:17 -0200

    Continuando a integrar o SIGA-GC

---
+++ sigagc/app/models/EntidadePlay.java
@@ -0,0 +1,61 @@
+package models;
+
+import controllers.SrConfiguracaoBL;
+import br.gov.jfrj.siga.cp.CpConfiguracao;
+import br.gov.jfrj.siga.cp.CpTipoConfiguracao;
+import br.gov.jfrj.siga.dp.dao.CpDao;
+import play.db.jpa.GenericModel;
+import play.db.jpa.JPA;
+
+public abstract class EntidadePlay extends GenericModel implements
+		HistoricoPersistivel {
+
+	private EntidadeHistoricoSuporte suporte = new EntidadeHistoricoSuporte(
+			this);
+
+	@Override
+	public Persistivel salvar() throws Exception {
+		suporte.salvar();
+		return this;
+	}
+
+	@Override
+	public void  finalizar() throws Exception {
+		suporte.finalizar();
+	}
+
+	@Override
+	public void salvarSimples() throws Exception {
+		save();
+	}
+
+	@Override
+	public void darRefresh() throws Exception {
+		refresh();
+	}
+
+	@Override
+	public void flushSeNecessario() throws Exception {
+
+	}
+
+	@Override
+	public void destacar() {
+		JPA.em().detach(this);
+	}
+
+	@Override
+	public Persistivel buscarPorId(Long id) {
+		return (Persistivel) findById(id);
+	}
+
+	public SrConfiguracao getConfiguracao(SrItemConfiguracao item,
+			SrServico servico, long idTipo) throws Exception {
+
+		SrConfiguracao conf = new SrConfiguracao(item, servico, JPA.em().find(
+				CpTipoConfiguracao.class, idTipo));
+
+		return SrConfiguracaoBL.get().buscarConfiguracao(conf);
+	}
+
+}

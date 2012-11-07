05dcd1daddef8b0ef857cdd74b779771f7d41977
kpf <markenson@jfrj.jus.br>
2012-11-07 18:24:46 -0200

    - Inclusão do da cadeia do certificado AC_SERASA_JUS_V1 - Remoção de hífen na matrícula (TJBA tem hífen no certificado, impedindo conversão para long)

--- siga-cd-base/src/br/gov/jfrj/siga/cd/ac/CertificadoX509ConhecidoEnum.java
+++ siga-cd-base/src/br/gov/jfrj/siga/cd/ac/CertificadoX509ConhecidoEnum.java
@@ -46,7 +46,8 @@ public enum CertificadoX509ConhecidoEnum {
 	AC_CAIXA_JUS_V2(CertificadoACEnum.AC_CAIXA_JUS_V2.toX509Certificate()),
 	AC_CERTISIGN_G6(CertificadoACEnum.AC_CERTISIGN_G6.toX509Certificate()),
 	AC_RFB_V3(CertificadoACEnum.AC_RFB_V3.toX509Certificate()),
-	AC_CERTISIGN_RFB_G4(CertificadoACEnum.AC_CERTISIGN_RFB_G4.toX509Certificate())
+	AC_CERTISIGN_RFB_G4(CertificadoACEnum.AC_CERTISIGN_RFB_G4.toX509Certificate()),
+	AC_SERASA_JUS_V1(CertificadoACEnum.AC_SERASA_JUS_V1.toX509Certificate())
 	;
 	
 	

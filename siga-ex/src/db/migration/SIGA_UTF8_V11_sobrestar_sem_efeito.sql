31291ffa1baa68561378277e0319c6d980b9f8fa
dud <andre.silva@jfrj.jus.br>
2013-02-04 08:13:39 -0200

    Comandos sql para as funcionalidades de sobrestar e de tornar um documento sem efeito.

---
+++ siga-ex/src/db/migration/SIGA_UTF8_V11_sobrestar_sem_efeito.sql
@@ -0,0 +1,3 @@
+insert into siga.ex_tipo_movimentacao values(46, 'Sobrestar');
+insert into siga.ex_tipo_movimentacao values(47, 'Desobrestar');
+insert into siga.ex_tipo_movimentacao values(48, 'Tornar sem Efeito');

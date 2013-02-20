7bbce05c65203ee41cdb3326174891cfc4752fa4
tah <crivano@jfrj.jus.br>
2013-02-20 12:22:47 -0300

    Checkin completo do siga-play-module.

---
+++ siga-play-module/commands.py
@@ -0,0 +1,35 @@
+# Here you can create play commands that are specific to the module, and extend existing commands
+
+MODULE = 'siga-play-module'
+
+# Commands that are specific to your module
+
+COMMANDS = ['siga-play-module:hello']
+
+def execute(**kargs):
+    command = kargs.get("command")
+    app = kargs.get("app")
+    args = kargs.get("args")
+    env = kargs.get("env")
+
+    if command == "siga-play-module:hello":
+        print "~ Hello"
+
+
+# This will be executed before any command (new, run...)
+def before(**kargs):
+    command = kargs.get("command")
+    app = kargs.get("app")
+    args = kargs.get("args")
+    env = kargs.get("env")
+
+
+# This will be executed after any command (new, run...)
+def after(**kargs):
+    command = kargs.get("command")
+    app = kargs.get("app")
+    args = kargs.get("args")
+    env = kargs.get("env")
+
+    if command == "new":
+        pass
---
+++ siga-play-module/src/br/gov/jfrj/siga/dp/dao/CpDaoUtil.java
@@ -0,0 +1,18 @@
+package br.gov.jfrj.siga.dp.dao;
+
+import java.io.UnsupportedEncodingException;
+
+import org.hibernate.Hibernate;
+
+public class CpDaoUtil {
+
+	public static java.sql.Blob createBlob(String conteudo) {
+		try {
+			return CpDao.getInstance().getSessao().getLobHelper()
+					.createBlob(conteudo.getBytes("ISO-8859-1"));
+		} catch (UnsupportedEncodingException e) {
+			throw new Error(e);
+		}
+	}
+
+}

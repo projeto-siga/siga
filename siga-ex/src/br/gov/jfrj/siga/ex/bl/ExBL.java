c8baa50b578687824f258efac5fa3ea9b9625ae3
kpf <markenson@jfrj.jus.br>
2013-08-22 17:44:37 -0300

    - Desativa a verificacao se classificacao documental esta associada a documento ao excluir classificacao

--- siga-ex/src/br/gov/jfrj/siga/ex/bl/ExBL.java
+++ siga-ex/src/br/gov/jfrj/siga/ex/bl/ExBL.java
@@ -5250,6 +5250,19 @@ public class ExBL extends CpBL {
 							+ sb.toString());
 		}
 
+		/*AVISO: 
+		 * 
+		 *  
+		 * O código abaixo foi comentado para permitir a atualização da tabela de classificação documental
+		 * enquanto a funcionalidade de reclassificação de documentos nõa está disponível.
+		 * 
+		 * 
+		 *  
+		 */
+		
+		
+		
+		/*
 		List<ExDocumento> docs = ExDao.getInstance()
 				.consultarExDocumentoPorClassificacao(null,
 						MascaraUtil.getInstance().getMscTodosDoMaiorNivel(),
@@ -5261,7 +5274,11 @@ public class ExBL extends CpBL {
 					"Não é possível excluir a classificação documental, pois já foi associada a documento(s)."
 							+ sb.toString());
 		}
-
+		 */
+		
+		
+		
+		
 		for (ExVia exVia : exClass.getExViaSet()) {
 			dao().excluirComHistorico(exVia, dt, idCadastrante);
 		}

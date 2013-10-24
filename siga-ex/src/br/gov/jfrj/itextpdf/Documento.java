2cec3ade0135f12574c7db0f6ec75c406574b8b7
Renato Crivano <renato.crivano@gmail.com>
2013-10-24 12:32:39 -0200

    Desabilitado o iFrame de Pdfs na visão de dossiê html.

--- siga-ex/src/br/gov/jfrj/itextpdf/Documento.java
+++ siga-ex/src/br/gov/jfrj/itextpdf/Documento.java
@@ -1300,12 +1300,12 @@ public class Documento {
 								+ ((ExMovimentacao) an.getArquivo())
 										.getDescrMov());
 					}
-				sb.append("<iframe style=\"visibility: visible; margin: 0px; padding: 0px; display: block;height: 30em; border: 1px solid black;\" src=\"http://"
-						+ servernameport
-						+ contextpath
-						+ "/arquivo/exibir.action?arquivo="
-						+ an.getArquivo().getReferenciaPDF()
-						+ "&semmarcas=1\" width=\"100%\" frameborder=\"0\" scrolling=\"auto\"></iframe>");
+//				sb.append("<iframe style=\"visibility: visible; margin: 0px; padding: 0px; display: block;height: 30em; border: 1px solid black;\" src=\"http://"
+//						+ servernameport
+//						+ contextpath
+//						+ "/arquivo/exibir.action?arquivo="
+//						+ an.getArquivo().getReferenciaPDF()
+//						+ "&semmarcas=1\" width=\"100%\" frameborder=\"0\" scrolling=\"auto\"></iframe>");
 
 				// sb.append("<br/>");
 				sb.append("</div>");

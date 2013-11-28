f72361897fbf10bcc93b955d7e2b17a3f5afb730
Markenson <markensonfranca@gmail.com>
2013-11-28 11:42:47 -0200

    inclui biblioteca ojdbc6

--- lib/configure.bat
+++ lib/configure.bat
@@ -4,4 +4,5 @@ call mvn install:install-file -Dfile=ss_css2.jar -DgroupId=com.steadystate.css.p
 call mvn install:install-file -Dfile=jbpm-jpdl.jar -DgroupId=jbpm -DartifactId=jbpm-jpdl -Dversion=1.0.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=jbpm-identity.jar -DgroupId=jbpm -DartifactId=jbpm-identity -Dversion=1.0.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=WikiParser.jar -DgroupId=ys.wikiparser -DartifactId=WikiParser -Dversion=1.0.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.3.0 -Dpackaging=jar
 echo Libs configuradas com sucesso

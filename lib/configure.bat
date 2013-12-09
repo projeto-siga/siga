71183e44a3c9b9f337a613271ceb2a907363fa41
Markenson <markensonfranca@gmail.com>
2013-12-09 12:17:19 -0200

    Inclui hibernate validator legacy ao repositorio, pois o repositorio publico esta com problema

--- lib/configure.bat
+++ lib/configure.bat
@@ -5,4 +5,6 @@ call mvn install:install-file -Dfile=jbpm-jpdl.jar -DgroupId=jbpm -DartifactId=j
 call mvn install:install-file -Dfile=jbpm-identity.jar -DgroupId=jbpm -DartifactId=jbpm-identity -Dversion=1.0.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=WikiParser.jar -DgroupId=ys.wikiparser -DartifactId=WikiParser -Dversion=1.0.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.3.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=hibernate-validator-legacy-3.1.0.GA.jar -DgroupId=org.hibernate -DartifactId=hibernate-validator-legacy -Dversion=3.1.0.GA -Dpackaging=jar
+
 echo Libs configuradas com sucesso

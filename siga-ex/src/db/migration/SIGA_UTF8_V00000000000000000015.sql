b750807a1d0a7c5ebbeebcb362542dde7da7733a
andresousa17 <andresousa17@gmail.com>
2014-01-22 18:57:55 -0200

    Atualiza tabela de classificação quando a classificação possui his_id_ini null

---
+++ siga-ex/src/db/migration/SIGA_UTF8_V00000000000000000015.sql
@@ -0,0 +1,6 @@
+-------------------------------------------
+--	SCRIPT:CLASS DOC
+-------------------------------------------
+
+--EX_CLASSIFICACAO
+update SIGA.ex_classificacao set his_id_ini = id_classificacao where his_id_ini is null;

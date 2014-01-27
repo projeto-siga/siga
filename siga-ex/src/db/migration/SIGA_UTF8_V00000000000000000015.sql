ac4f7ac2f0033f8323bf6f994c3ec136b4679b30
Markenson <markensonfranca@gmail.com>
2014-01-27 12:25:08 -0200

    Reposicionamento de arquivos 2

--- siga-ex/src/db/migration/SIGA_UTF8_V00000000000000000015.sql
+++
@@ -1,6 +0,0 @@
--------------------------------------------
---	SCRIPT:CLASS DOC
--------------------------------------------
-
---EX_CLASSIFICACAO
-update SIGA.ex_classificacao set his_id_ini = id_classificacao where his_id_ini is null;

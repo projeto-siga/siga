fa328956c15dd9dbe28ba9b1b90e16ec43713c4c
Markenson <markensonfranca@gmail.com>
2014-01-31 17:19:22 -0200

    Correcao do git 3

--- siga-ex/src/br/gov/jfrj/siga/ex/AbstractExEmailNotificacao.java
+++ siga-ex/src/br/gov/jfrj/siga/ex/AbstractExEmailNotificacao.java
@@ -20,17 +20,24 @@ package br.gov.jfrj.siga.ex;
 import java.io.Serializable;
 
 import br.gov.jfrj.siga.dp.DpLotacao;
+import br.gov.jfrj.siga.dp.DpPessoa;
 import br.gov.jfrj.siga.model.Objeto;
 
 public class AbstractExEmailNotificacao extends Objeto  {
 	
 	/** The composite primary key value. */
 	private java.lang.Long idEmailNotificacao;
-	
+
 	private DpLotacao dpLotacao;
-	
+
+	private DpPessoa dpPessoa;
+
 	private String email;
-	
+
+	private DpLotacao lotacaoEmail;
+
+	private DpPessoa pessoaEmail;
+
 	public java.lang.Long getIdEmailNotificacao() {
 		return idEmailNotificacao;
 	}
@@ -38,25 +45,36 @@ public class AbstractExEmailNotificacao extends Objeto  {
 	public void setIdEmailNotificacao(java.lang.Long idEmailNotificacao) {
 		this.idEmailNotificacao = idEmailNotificacao;
 	}
-
 	public DpLotacao getDpLotacao() {
 		return dpLotacao;
 	}
-
+	public DpPessoa getDpPessoa() {
+		return dpPessoa;
+	}
 	public void setDpLotacao(DpLotacao dpLotacao) {
 		this.dpLotacao = dpLotacao;
+	}	
+	public void setDpPessoa(DpPessoa dpPessoa) {
+		this.dpPessoa = dpPessoa;
 	}
-
+	public DpLotacao getLotacaoEmail() {
+		return lotacaoEmail;
+	}
+	public void setLotacaoEmail(DpLotacao lotacaoEmail) {
+		this.lotacaoEmail = lotacaoEmail;
+	}
 	public String getEmail() {
 		return email;
 	}
-
+	public DpPessoa getPessoaEmail() {
+		return pessoaEmail;
+	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	public void setPessoaEmail(DpPessoa pessoaEmail) {
+		this.pessoaEmail = pessoaEmail;
+	}
 	public AbstractExEmailNotificacao() {
 	}
-	
-	
 }

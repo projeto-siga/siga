a9e1f9a8378f8f9fa18cfa9dd7379ef3744cd4c3
Markenson <markensonfranca@gmail.com>
2014-03-19 10:16:32 -0300

    Versao inicial da aplicacao

--- siga-dump/src/main/java/br/gov/jfrj/siga/dump/App.java
+++ siga-dump/src/main/java/br/gov/jfrj/siga/dump/App.java
@@ -1,13 +1,149 @@
 package br.gov.jfrj.siga.dump;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+
+public class App {
+
+	private final static List<String> _tabs=new ArrayList<String>();
+	private static String _arq = null;
+	
+	
+	private List<String> tabelas;
+	private String arquivoSaida;
+	private Connection connection;
+	
+	private List<String> inserts = new ArrayList<String>();
+	private List<String> updates = new ArrayList<String>();
+	
+	public App(List<String> tabelas, String arquivoSaida) {
+		this.tabelas = tabelas;
+		this.arquivoSaida = arquivoSaida;
+	}
+
+	public static void main(String[] args) throws SQLException {
+		processarArgumentos(args);
+		App app = new App(_tabs,_arq);
+		app.dump();
+	}
+
+	/**
+	 * Executa o dump
+	 * @throws SQLException 
+	 */
+	private void dump() throws SQLException {
+		 DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+		 connection = DriverManager.getConnection( "jdbc:oracle:thin:@192.168.56.101:1521:xe", "siga", "siga" );
+		 
+		 for (String t:tabelas) {
+			 dump(t);
+		}
+	}
+
+	private List<String> dump(String nomeTabela) throws SQLException {
+		PreparedStatement stm = connection.prepareStatement("select * from " + nomeTabela + " ORDER BY ID_MOD");
+		ResultSet rs = stm.executeQuery();
+		
+		List<String> colunas = new ArrayList<String>();
+		for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+			colunas.add(rs.getMetaData().getColumnName(i));
+		}
+		
+		List<Object> valores = new ArrayList<Object>();
+		
+		while(rs.next()){
+			
+			for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+				if (rs.getMetaData().getColumnType(i) == Types.BLOB){
+					valores.add(getAspas(blobToString(rs.getBytes(i))));
+				}else if (rs.getMetaData().getColumnType(i) == Types.VARCHAR){
+					valores.add(getAspas(rs.getString(i)));
+				}else {
+					valores.add(rs.getObject(i));	
+				}
+				
+			}
+			
+			String pk = getPrimaryKey(nomeTabela) ;
+			inserts.add(getInsertCmd(nomeTabela,colunas,valores));
+			updates.add(getUpdateCmd(nomeTabela,pk,colunas,valores));
+			
+			valores = new ArrayList<Object>();
+		}
+		
+		List<String> result = new ArrayList<String>();
+		result.addAll(inserts);
+		result.addAll(updates);
+
+		return result;
+	}
+
+	private String getAspas(String str) {
+		return "'" + str + "'";
+	}
+
+	private String getPrimaryKey(String nomeTabela) throws SQLException {
+		ResultSet rsPK = connection.getMetaData().getPrimaryKeys(null, null, nomeTabela);
+		rsPK.next();
+		return rsPK.getString(4);
+	}
+
+	private String blobToString(byte[] bytes) {
+		if (bytes!=null){
+			return new String(bytes);	
+		}
+		return null;
+	}
+
+	private String getInsertCmd(String nomeTabela,List<String> colunas, List<Object> valores) {
+		String lstColunas = colunas.toString().replaceAll("\\[|\\]", "");
+		String lstValores = valores.toString().replaceAll("\\[|\\]", "");
+		return "INSERT INTO " + nomeTabela + " (" + lstColunas + ") VALUES (" + lstValores + ");";
+	}
+
+	private String getUpdateCmd(String nomeTabela,String pk, List<String> colunas, List<Object> valores) {
+		List<String> lstSet = new ArrayList<String>();
+		String valorPk = null;
+		for (int i = 0; i < colunas.size(); i++) {
+			lstSet.add(colunas.get(i) + "=" + valores.get(i));
+			if (colunas.get(i).equals(pk)){
+				valorPk = valores.get(i).toString();
+			}
+		}
+		
+		String updt =  "UPDATE " + nomeTabela + " SET " + lstSet + " WHERE " + pk + " = " + valorPk;
+		return updt.replaceAll("\\[|\\]", "");
+	}
+
+	/**
+	 * Extrai os arqumentos passados na linha de comando
+	 * @param pars
+	 */
+	private static void processarArgumentos(String[] pars) {
+		if (pars.length < 2) {
+			System.err.println("Número de Parametros inválidos!");
+		}
+
+		for (String param : Arrays.asList(pars)) {
+			if (param.startsWith("-tabelas=")) {
+				String t = param.split("=")[1];
+				_tabs.addAll(Arrays.asList(t.split(",")));
+			}
+			if (param.startsWith("-arquivoSaida=")) {
+				String a = param.split("=")[1];
+				_arq=a;
+			}
+			
+		}
+
+	}
+	
 }

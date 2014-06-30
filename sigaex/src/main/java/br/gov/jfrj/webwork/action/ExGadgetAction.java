1ee0106a932fb37da6addf7956b799095e2e2d98
Chrystinne <chrystinne@gmail.com>
2014-06-30 15:13:54 -0300

    SIGA-DOC: Removendo método slowExecute da classe ExGadgetAction que não está mais sendo usado.

--- sigaex/src/main/java/br/gov/jfrj/webwork/action/ExGadgetAction.java
+++ sigaex/src/main/java/br/gov/jfrj/webwork/action/ExGadgetAction.java
@@ -65,20 +65,6 @@ public class ExGadgetAction extends ExActionSupport {
 		this.documentoViaSel = documentoViaSel;
 	}
 
-	public String slowExecute() throws Exception {
-		listEstados = dao().consultarPorResponsavel(getTitular(),
-				getLotaTitular());
-		documentoViaSel = new ExMobilSelecao();
-		super.getRequest().setAttribute(
-				"_cadastrante",
-				super.getTitular().getSigla()
-						+ "@"
-						+ super.getLotaTitular().getOrgaoUsuario()
-								.getSiglaOrgaoUsu()
-						+ super.getLotaTitular().getSigla());
-		return Action.SUCCESS;
-	}
-
 	@Override
 	public String execute() throws Exception {
 		if (getIdTpFormaDoc() == null || getIdTpFormaDoc() == 0)

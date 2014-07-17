6b914c7fdb4bcd8a1fe26a9a85298fab77b88f8a
Markenson <markensonfranca@gmail.com>
2014-07-17 12:41:36 -0300

    Altera para sysdate para evitar o erro ORA-01843: not a valid month

--- siga-cp/src/main/resources/db/migration/CORPORATIVO_UTF8_V00000000000000000020.sql
+++ siga-cp/src/main/resources/db/migration/CORPORATIVO_UTF8_V00000000000000000020.sql
@@ -23,16 +23,16 @@ insert into corporativo.cp_servico (id_servico,sigla_servico,desc_servico,id_ser
 
 insert into corporativo.cp_configuracao (id_configuracao,his_dt_ini,id_sit_configuracao,id_tp_configuracao,id_servico,id_tp_lotacao)
     values (corporativo.cp_configuracao_seq.nextval,
-              (select to_char(systimestamp,'dd/mm/rrrr hh24:mi:ss') from dual),
-              (select sit.id_sit_configuracao from cp_situacao_configuracao sit where sit.dsc_sit_configuracao = 'Pode'),202,
+              (select sysdate from dual),
+              (select sit.id_sit_configuracao from corporativo.cp_situacao_configuracao sit where sit.dsc_sit_configuracao = 'Pode'),202,
               (select cp.id_servico from corporativo.cp_servico cp where cp.sigla_servico = 'FS-AUD'),
               (select tp.id_tp_lotacao from corporativo.cp_tipo_lotacao tp where tp.sigla_tp_lotacao = 'JUD')
            );  
              
 insert into corporativo.cp_configuracao (id_configuracao,his_dt_ini,id_sit_configuracao,id_tp_configuracao,id_servico,id_tp_lotacao)
     values (corporativo.cp_configuracao_seq.nextval,
-              (select to_char(systimestamp,'dd/mm/rrrr hh24:mi:ss') from dual),
-              (select sit.id_sit_configuracao from cp_situacao_configuracao sit where sit.dsc_sit_configuracao = 'Pode'),202,
+              (select sysdate from dual),
+              (select sit.id_sit_configuracao from corporativo.cp_situacao_configuracao sit where sit.dsc_sit_configuracao = 'Pode'),202,
               (select cp.id_servico from corporativo.cp_servico cp where cp.sigla_servico = 'FS-VIDEO'),
               (select tp.id_tp_lotacao from corporativo.cp_tipo_lotacao tp where tp.sigla_tp_lotacao = 'JUD')
            ); 

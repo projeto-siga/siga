59da2c95d3223ca7f2b2ccfc7505deceb039fc80
Markenson <markensonfranca@gmail.com>
2014-07-25 12:59:14 -0300

    Verificação de nullpointer

--- siga-wf/src/main/java/br/gov/jfrj/siga/wf/bl/WfBL.java
+++ siga-wf/src/main/java/br/gov/jfrj/siga/wf/bl/WfBL.java
@@ -432,7 +432,15 @@ public class WfBL extends CpBL {
 	 */
 	public void encerrarProcessInstanceDaTarefa(Long idTI, Date dtFim) throws AplicacaoException {
 		TaskInstance ti = WfContextBuilder.getJbpmContext().getJbpmContext().getTaskInstance(idTI);
+		if (ti==null){
+			throw new AplicacaoException("Tarefa não encontrada!");
+		}
+		if (dtFim==null){
+			throw new AplicacaoException("Data inválida!");
+		}
+		
 		encerrarProcessInstance(ti.getProcessInstance().getId(), dtFim);
+		
 	}
 	
 	public String gravarProcessDefinition(String xml) {

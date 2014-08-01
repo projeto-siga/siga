12f845465ddc7269af726dcf51386717ad2cb89a
andresousa17 <andresousa17@gmail.com>
2014-08-01 13:18:58 -0300

    Retorna Bean CalculoPCD para o freemarker.

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/util/FuncoesEL.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/util/FuncoesEL.java
@@ -983,4 +983,23 @@ public class FuncoesEL {
 		return PublicacaoDJEBL.obterListaTiposMaterias(mod.getHisIdIni());
 	}
 	
+	public static CalculoPCD calculoPCD(String cargo, String funcao, DpPessoa beneficiario, String dataInicio, String dataFim, 
+			Boolean solicitaAuxTransporte, Boolean carroOficial, Integer pernoite, Float descontoSalario, Float valorConcedido, Boolean executorMandado ) {
+		CalculoPCD calculo = new CalculoPCD();
+		
+		calculo.setCargo(cargo);
+		calculo.setFuncao(funcao);
+		calculo.setBeneficiario(beneficiario);
+		calculo.setDataInicio(dataInicio);
+		calculo.setDataFim(dataFim);
+		calculo.setSolicitaAuxTransporte(solicitaAuxTransporte);
+		calculo.setCarroOficial(carroOficial);
+		calculo.setPernoite(pernoite);
+		calculo.setDescontoSalario(descontoSalario);
+		calculo.setValorConcedido(valorConcedido);
+		calculo.setExecutorMandado(executorMandado);
+		
+		
+		return calculo;
+	}
 }

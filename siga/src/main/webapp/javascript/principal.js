c0892d376891aa38ba27edd000f86a8655ed1d05
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2014-08-14 17:12:42 -0300

    refactoring + correção de erro 500

--- siga/src/main/webapp/javascript/principal.js
+++ siga/src/main/webapp/javascript/principal.js
@@ -27,7 +27,17 @@ window.Siga = {
             params: {
                 idTpFormaDoc: 1
             },
-            viewId: "left"
+            viewId: "left",
+            submodules: {
+                processos: {
+                    name: "processos",
+                    url: "/sigaex/expediente/doc/gadget.action",
+                    params: {
+                        idTpFormaDoc: 2
+                    },
+                    viewId: "leftbottom"
+                }
+            }
         },
         sigasr: {
             name: "sigasr",
@@ -43,22 +53,23 @@ window.Siga = {
         }
     },
 
-    subModules: {
-        processos: {
-            name: "processos",
-            url: "/sigaex/expediente/doc/gadget.action",
-            params: {
-                idTpFormaDoc: 2
-            },
-            viewId: "leftbottom"
-        }
-    },
-
     currentTimeInMillis: function(){
         return new Date($.now()).getTime();
     },
 
     display: function(target, text){
+        var self = this;
+        console.log("entrei no display");
+        if (text.indexOf("<HTML") > -1 || text.indexOf("<title>") > -1){
+            console.log("autenticação falhou");
+
+            $.each(self.modules, function(module){
+                if (target.attr("id") == this.viewId){
+                    self.loadModule(module);
+                }
+            });
+        }
+
         target.append(text);
         $(target.find(".loading")).hide();
     },
@@ -76,6 +87,16 @@ window.Siga = {
         return {url: action, params: samlJson};
     },
 
+    loadSubModules: function(model){
+        debugger;
+        var self = this;
+        if (model.submodules){
+            $.each(model.submodules, function(){
+                self.loadModule(this);
+            });
+        }
+    },
+
     ajaxCall: function(ajax, doneCallback){
         var self = this;
         ajax.params["ts"] = self.currentTimeInMillis();
@@ -84,11 +105,23 @@ window.Siga = {
             url: ajax.url,
             type: ajax.type,
             data: ajax.params,
+            timeout: 1500,
+            statusCode: {
+                404: function() {
+                    if (ajax.target != null)
+                        ajax.target.html("M&oacute;dulo indispon&iacute;vel");
+                },
+                500: function() {
+                    if (ajax.target != null)
+                        ajax.target.html("Erro interno do servidor. Por favor, entre em contato com um administrador.");
+                }
+            },
             beforeSend: function(){
                 if (ajax.target != null)
                     $(ajax.target.find(".loading")).show();
             }
         }).fail(function(){
+            console.log("fail!");
             if (ajax.target != null)
                 ajax.target.html("M&oacute;dulo indispon&iacute;vel");
         }).done(function(response){
@@ -99,52 +132,43 @@ window.Siga = {
         });
     },
 
-    loadSubModule: function(model){
-        var self = this;
-        if (model.name == "sigaex"){
-            model = this.subModules["processos"];
-            var target = $("#"+model.viewId);
-            self.ajaxCall({url: model.url, type: "GET", params: model.params, target: target}, function(responseText){
-                self.display(target, responseText);
-            });
-        }
-    },
-
     loadModule: function(model){
         var self = this;
         var target = $("#"+model.viewId);
 
+        console.log("Ajax request to: "+model.url);
         self.ajaxCall({url: model.url, type: "GET", params: model.params, target: target}, function(textResponse) {
+            console.log("resposta do 1 GET-> "+model.name);
             // Verifica se o SP foi previamente inicializado, caso nao tenha sido apenas renderiza.
             if (textResponse.indexOf("SAMLRequest") > -1){
+                console.log("Efetuando processamento pelo picketlink: "+model.name);
                 var params = self.picketlinkResponse(textResponse);
 
                 // Envia um POST para o IDP com o atributo SAMLRequest da ultima requisicao
-                self.ajaxCall({url: params.url, type: "POST", params: params.params, target: null}, function(textResponse){
+                console.log("Efetuando primeiro POST para: "+model.name);
+                self.ajaxCall({url: params.url, type: "POST", params: params.params, target: target}, function(textResponse){
+                    console.log("resposta do 1 POST -> "+model.name);
                     var params = self.picketlinkResponse(textResponse);
 
-                    self.ajaxCall({url: params.url, type: "POST", params: params.params, target: null}, function(textResponse){
-                        if (textResponse instanceof Array) {
-                            console.log("Autenticacao falhou para: "+model.name);
-                            console.log(" result -> "+textResponse);
-                        }else{
-                            self.display(target, textResponse);
-                            self.loadSubModule(model);
-                        }
+                    console.log("Efetuando o 2 POST para: "+model.name);
+                    self.ajaxCall({url: params.url, type: "POST", params: params.params, target: target}, function(textResponse){
+                        console.log("resposta do 2 POST -> "+model.name);
+                        self.display(target, textResponse);
+                        self.loadSubModules(model);
                     });
+
                 });
             }else{
                 self.display(target, textResponse);
-                self.loadSubModule(model);
             }
         });
     },
 
     loadModules: function(){
-        this.loadModule(this.modules["sigaex"]);
-        this.loadModule(this.modules["sigawf"]);
-        this.loadModule(this.modules["sigagc"]);
-        this.loadModule(this.modules["sigasr"]);
+        var self = this;
+        $.each(self.modules, function(){
+            self.loadModule(this);
+        });
     }
 }
 var Siga = window.Siga;

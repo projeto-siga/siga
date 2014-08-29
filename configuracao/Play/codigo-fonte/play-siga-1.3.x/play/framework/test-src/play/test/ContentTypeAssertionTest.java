e0dc5fa8717890584f2e3a3c31adf938606c6e24
Karina Macedo <kmacedovarela@gmail.com>
2014-08-29 15:54:04 -0300

    codigo fonte do play
    Codigo alterado..

---
+++ configuracao/Play/codigo-fonte/play-siga-1.3.x/play/framework/test-src/play/test/ContentTypeAssertionTest.java
@@ -0,0 +1,18 @@
+package play.test;
+
+import static play.test.FunctionalTest.*;
+
+import org.junit.Test;
+
+import play.mvc.Http.Response;
+
+public class ContentTypeAssertionTest {
+
+	@Test(expected=AssertionError.class)
+	public void givenContentTypeIsMissing_shouldThrowAssertionError() {
+		Response responseWithoutContentType = new Response();
+
+		assertContentType("text/html", responseWithoutContentType);
+	}
+
+}

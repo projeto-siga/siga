bc523a0886fe3fec4aee2e2ef98cff26ce29af88
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2014-10-02 12:51:16 -0300

    url do cross domain agora no siga.properties

--- sigaidp/src/main/java/br/gov/jfrj/siga/picketlink/CORSValve.java
+++ sigaidp/src/main/java/br/gov/jfrj/siga/picketlink/CORSValve.java
@@ -10,14 +10,15 @@ import org.apache.catalina.valves.ValveBase;
 
 
 public class CORSValve extends ValveBase{
-
+	
 	@Override
 	public void invoke(Request request, Response response) throws IOException,ServletException {
-		int porta = request.getServerPort();
+		//int porta = request.getServerPort();
 		// Esse 363 é o número da diferença da porta https pra http ex: 8443 - 8080
 		// Com isso se a aplicação estiver em uma porta diferente da padrão não precisa modificar esse método.
-		porta = porta - 363;
-		response.addHeader("Access-Control-Allow-Origin", "http://"+request.getServerName()+":"+porta);
+		//porta = porta - 363;
+		String url = System.getProperty("sigaidp.crossdomain.url");
+		response.addHeader("Access-Control-Allow-Origin", url);
 		response.addHeader("Access-Control-Allow-Methods", "POST, GET");
 		response.addHeader("Access-Control-Max-Age", "3600");
 		response.addHeader("Access-Control-Allow-Credentials", "true");

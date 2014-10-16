9223caa5628a8862c3cddac20c7400db5e35b2d1
Markenson <markensonfranca@gmail.com>
2014-10-16 15:52:21 -0300

    Inclui GRANTS para CP_COMPLEXO

---
+++ siga-cp/src/main/resources/db/migration/CORPORATIVO_UTF8_V27_0__Description.sql
@@ -0,0 +1,6 @@
+-------------------------------------------
+--	SCRIPT:PERM CP COMPLEXO SIGA e SIGAWF
+-------------------------------------------
+
+GRANT SELECT ON "CORPORATIVO"."CP_COMPLEXO" TO "SIGAWF";
+GRANT SELECT ON "CORPORATIVO"."CP_COMPLEXO" TO "SIGA";

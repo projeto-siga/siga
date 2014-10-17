bda4c925811b3fd4078d176050d560397b922f98
Renato Crivano <renato.crivano@gmail.com>
2014-10-17 17:29:13 -0300

    SIGA-DOC: Aceitantdo conteudo = null no WS de criarDocumento

--- sigaex/src/main/java/br/gov/jfrj/siga/ex/service/impl/ExServiceImpl.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/service/impl/ExServiceImpl.java
@@ -643,6 +643,8 @@ public class ExServiceImpl implements ExService {
 			doc.setExMobilSet(new TreeSet<ExMobil>());
 			doc.getExMobilSet().add(mob);
 			
+			if (conteudo == null)
+				conteudo = "";
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			baos.write(conteudo.getBytes());
 			

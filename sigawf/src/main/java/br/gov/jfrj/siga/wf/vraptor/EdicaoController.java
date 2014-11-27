8d0e6a13d52aafb3a4c8539366cd33fe9a2f5b7f
Renato Crivano <renato.crivano@gmail.com>
2014-11-27 15:21:31 -0200

    SIGA-WF: Ativando a pagina de edicao em angularjs

--- sigawf/src/main/java/br/gov/jfrj/siga/wf/vraptor/EdicaoController.java
+++ sigawf/src/main/java/br/gov/jfrj/siga/wf/vraptor/EdicaoController.java
@@ -27,6 +27,8 @@ public class EdicaoController extends WfController {
 		super(request, result, dao, so, util);
 	}
 
+	@Get
+	@Path("/app/edicao/form/{procedimento}")
 	public void form(String procedimento) {
 		result.include("procedimento", procedimento);
 	}

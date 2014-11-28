198228d837c0c06c83f45d143bece936a47bd117
diegofrsilva <diegofrsilva@gmail.com>
2014-11-28 16:01:02 -0200

    Item 13 - Adequacao do cadastro da Solicitacao para a inclusao automatica em lista de prioridade. Remocao de classe css das grids com expansao

--- sigasr/app/db/migration/SIGASR_UTF8_V00000000000000000018.sql
+++ sigasr/app/db/migration/SIGASR_UTF8_V00000000000000000018.sql
@@ -1,2 +1,6 @@
 --adicionando prioridade a solicitacao
 alter table sigasr.sr_solicitacao add PRIORIDADE number(10,0);
+
+INSERT INTO corporativo.cp_tipo_configuracao (id_tp_configuracao, dsc_tp_configuracao, id_sit_configuracao) 
+	VALUES (303, 'Inclusão automática em lista', 1);
+  

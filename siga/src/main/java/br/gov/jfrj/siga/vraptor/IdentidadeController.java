21af0d05072998ba712765a5cf2c8f62e22b6891
Uanderson Soares Gonçalves <uanderson.soares@db1.com.br>
2014-12-17 15:17:21 -0200

    Alterações foram feitas no arquivo para iniciar a construção do controlador.

--- siga/src/main/java/br/gov/jfrj/siga/vraptor/IdentidadeController.java
+++ siga/src/main/java/br/gov/jfrj/siga/vraptor/IdentidadeController.java
@@ -1,11 +1,26 @@
 package br.gov.jfrj.siga.vraptor;
 
+import javax.servlet.http.HttpServletRequest;
+
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.gov.jfrj.siga.base.AplicacaoException;
+import br.gov.jfrj.siga.dp.dao.CpDao;
 
 @Resource
-public class IdentidadeController {
+public class IdentidadeController extends SigaController {
 
+	public IdentidadeController(HttpServletRequest request, Result result, SigaObjects so) {
+		super(request, result, CpDao.getInstance(), so);
+
+		result.on(AplicacaoException.class).forwardTo(this).appexception();
+		result.on(Exception.class).forwardTo(this).exception();
+	}
 	
-	
+	@Get("/app/gi/identidade/listar")	
+	public void listar() {
+		
+	}
 	
 }

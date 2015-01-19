514ef7d588d68557b0e2fb56b7b7c4f75ac86001
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2015-01-19 17:48:00 -0200

    remoção do spring do siga-ws

--- siga-ws/src/main/java/br/gov/jfrj/siga/Service.java
+++ siga-ws/src/main/java/br/gov/jfrj/siga/Service.java
@@ -18,22 +18,20 @@
  ******************************************************************************/
 package br.gov.jfrj.siga;
 
-import br.gov.jfrj.siga.cd.service.CdService;
-import br.gov.jfrj.siga.ex.service.ExService;
-import br.gov.jfrj.siga.gi.service.GiService;
-import br.gov.jfrj.siga.wf.service.WfService;
-import org.springframework.context.support.AbstractApplicationContext;
-
-import javax.xml.namespace.QName;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.xml.namespace.QName;
+
+import br.gov.jfrj.siga.cd.service.CdService;
+import br.gov.jfrj.siga.ex.service.ExService;
+import br.gov.jfrj.siga.gi.service.GiService;
+import br.gov.jfrj.siga.wf.service.WfService;
+
 public abstract class Service {
 
 	public static final String ERRO = "ERRO: ";
-
-	protected static AbstractApplicationContext context = null;
 
 	static WfService wf = null;
 	static ExService ex = null;

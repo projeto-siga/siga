905ccd1cf7bcf57e6826fd6c5a684c775b5525bc
Gabriel Amaral <gabriel.amaral@db1.com.br>
2015-01-26 14:15:56 -0200

    OSI_FS0010 -  Item 5 Padronizar apresentação do "carregando". - FINAL

--- sigasr/public/javascripts/jquery-config.js
+++ sigasr/public/javascripts/jquery-config.js
@@ -1,7 +1,15 @@
 /**
  * Registra o block que ira mostrar/esconder o sinal de "carregando..."
  */
-var objBlock = { message: '<h2> Carregando... </h2>' };
+var objBlock = { css: { 
+    border: 'none', 
+    padding: '15px', 
+    backgroundColor: '#000', 
+    '-webkit-border-radius': '10px', 
+    '-moz-border-radius': '10px', 
+    opacity: .5, 
+    color: '#fff' 
+}, message: '<h2 style="color : #fff;"> Carregando... </h2>' };
 
 $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
 	jQuery.blockUI(objBlock);

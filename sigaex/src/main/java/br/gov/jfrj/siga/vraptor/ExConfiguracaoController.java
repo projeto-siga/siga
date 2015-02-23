6e1a6a37613c7dbd4ec6887b34c6e23d55bb2dbd
unknown <jefferson.ferreira@MGA898.db1.com.br>
2015-02-23 11:55:23 -0300

    Parcial commit ExConfiguracaoController.

--- sigaex/src/main/java/br/gov/jfrj/siga/vraptor/ExConfiguracaoController.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/vraptor/ExConfiguracaoController.java
@@ -7,29 +7,41 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import javax.persistence.EntityManager;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.view.Results;
+import br.gov.jfrj.siga.base.AplicacaoException;
 import br.gov.jfrj.siga.cp.CpTipoConfiguracao;
 import br.gov.jfrj.siga.dp.CpOrgaoUsuario;
-import br.gov.jfrj.siga.dp.dao.CpDao;
 import br.gov.jfrj.siga.ex.ExConfiguracao;
 import br.gov.jfrj.siga.ex.ExFormaDocumento;
 import br.gov.jfrj.siga.ex.ExModelo;
 import br.gov.jfrj.siga.ex.ExTipoMovimentacao;
 import br.gov.jfrj.siga.ex.bl.Ex;
 import br.gov.jfrj.siga.ex.bl.ExConfiguracaoComparator;
+import br.gov.jfrj.siga.hibernate.ExDao;
+import br.gov.jfrj.siga.libs.webwork.DpCargoSelecao;
+import br.gov.jfrj.siga.libs.webwork.DpFuncaoConfiancaSelecao;
+import br.gov.jfrj.siga.libs.webwork.DpLotacaoSelecao;
+import br.gov.jfrj.siga.libs.webwork.DpPessoaSelecao;
+import br.gov.jfrj.siga.vraptor.builder.ExConfiguracaoBuilder;
 
 @Resource
-public class ExConfiguracaoController extends SigaController {
-	
-	private static final String VERIFICADOR_ACESSO = "DOC:Módulo de Documentos;FE:Ferramentas;CFG:Configurações";
+public class ExConfiguracaoController extends ExController {
 
-	public ExConfiguracaoController(HttpServletRequest request, Result result, CpDao dao, SigaObjects so, EntityManager em) {
-		super(request, result, dao, so, em);
+	private static final String VERIFICADOR_ACESSO = "DOC:Módulo de Documentos;FE:Ferramentas;CFG:Configurações";
+	
+	public ExConfiguracaoController(HttpServletRequest request, HttpServletResponse response, ServletContext context, Result result, SigaObjects so,
+			EntityManager em) {
+		super(request, response, context, result, ExDao.getInstance(), so, em);
+
+		result.on(AplicacaoException.class).forwardTo(this).appexception();
+		result.on(Exception.class).forwardTo(this).exception();
 	}
 
 	@Get("app/expediente/configuracao/listar")
@@ -37,7 +49,35 @@ public class ExConfiguracaoController extends SigaController {
 		assertAcesso(VERIFICADOR_ACESSO);
 		result.include("listaTiposConfiguracao", getListaTiposConfiguracao());
 		result.include("orgaosUsu", getOrgaosUsu());
-		
+
+	}
+
+	@Get("app/expediente/configuracao/editar")
+	public void edita(Long id, boolean campoFixo, 
+			Long idOrgaoUsu, Long idTpMov, Long idTpDoc,
+			Long idMod, Long idFormaDoc,
+			Long idNivelAcesso, Long idSituacao, Long idTpConfiguracao,
+			DpPessoaSelecao pessoaSel, DpLotacaoSelecao lotacaoSel, DpCargoSelecao cargoSel, 
+			DpFuncaoConfiancaSelecao funcaoSel, ExClassificacaoSelecao classificacaoSel,
+			Long idOrgaoObjeto) throws Exception {
+
+		ExConfiguracao config = new ExConfiguracao();
+
+		if (id != null) {
+			config = daoCon(id);
+		} else if(campoFixo) {
+
+			final ExConfiguracaoBuilder configuracaoBuilder = ExConfiguracaoBuilder.novaInstancia()
+					.setIdNivelAcesso(idNivelAcesso).setIdTpMov(idTpMov).setIdTpDoc(idTpDoc)
+					.setIdMod(idMod).setIdFormaDoc(idFormaDoc).setIdNivelAcesso(idNivelAcesso)
+					.setIdSituacao(idSituacao).setIdTpConfiguracao(idTpConfiguracao)
+					.setPessoaSel(pessoaSel).setLotacaoSel(lotacaoSel).setCargoSel(cargoSel)
+					.setFuncaoSel(funcaoSel).setClassificacaoSel(classificacaoSel).setIdOrgaoObjeto(idOrgaoObjeto);
+
+			config = configuracaoBuilder.construir(dao());
+		}
+		escreverForm(config);
+
 	}
 
 	@Get("app/expediente/configuracao/listar_cadastradas")
@@ -88,6 +128,72 @@ public class ExConfiguracaoController extends SigaController {
 		this.getRequest().setAttribute("listConfig", listConfig);
 		this.getRequest().setAttribute("tpConfiguracao", config.getCpTipoConfiguracao());
 
+	}
+
+	private ExConfiguracao daoCon(long id) {
+		return dao().consultar(id, ExConfiguracao.class, false);
+	}
+	
+	private void escreverForm(ExConfiguracao c) throws Exception {
+		if (c.getOrgaoUsuario() != null)
+			result.include("idOrgaoUsu", c.getOrgaoUsuario().getIdOrgaoUsu());
+
+		if (c.getExTipoMovimentacao() != null)
+			result.include("idTpMov", c.getExTipoMovimentacao().getIdTpMov());
+
+		if (c.getExTipoDocumento() != null)
+			result.include("idTpDoc", c.getExTipoDocumento().getIdTpDoc());
+
+		if (c.getExTipoFormaDoc() != null)
+			result.include("idTpFormaDoc", c.getExTipoFormaDoc().getIdTipoFormaDoc());
+
+		if (c.getExFormaDocumento() != null)
+			result.include("idFormaDoc", c.getExFormaDocumento().getIdFormaDoc());
+
+		if (c.getExModelo() != null)
+			result.include("idMod", c.getExModelo().getIdMod());
+
+		if (c.getExNivelAcesso() != null)
+			result.include("idNivelAcesso", c.getExNivelAcesso().getIdNivelAcesso());
+
+		if (c.getCpSituacaoConfiguracao() != null)
+			result.include("idSituacao", c.getCpSituacaoConfiguracao().getIdSitConfiguracao());
+
+		if (c.getCpTipoConfiguracao() != null)
+			result.include("idTpConfiguracao", c.getCpTipoConfiguracao().getIdTpConfiguracao());
+
+		if (c.getDpPessoa() != null) {
+			DpPessoaSelecao pessoaSelecao = new DpPessoaSelecao();
+			pessoaSelecao.buscarPorObjeto(c.getDpPessoa());
+			result.include("pessoaSel", pessoaSelecao);
+		}
+
+		if (c.getLotacao() != null) {
+			DpLotacaoSelecao lotacaoSelecao = new DpLotacaoSelecao();
+			lotacaoSelecao.buscarPorObjeto(c.getLotacao());
+			result.include("lotacaoSel", lotacaoSelecao );
+		}
+
+		if (c.getCargo() != null) {
+			DpCargoSelecao cargoSelecao = new DpCargoSelecao();
+			cargoSelecao.buscarPorObjeto(c.getCargo());
+			result.include("cargoSel", cargoSelecao);
+		}
+
+		if (c.getFuncaoConfianca() != null) {
+			DpFuncaoConfiancaSelecao funcaoConfiancaSelecao = new DpFuncaoConfiancaSelecao();
+			funcaoConfiancaSelecao.buscarPorObjeto(c.getFuncaoConfianca());
+			result.include("funcaoSel", funcaoConfiancaSelecao);
+		}
+
+		if (c.getExClassificacao() != null) {
+			ExClassificacaoSelecao classificacaoSelecao = new ExClassificacaoSelecao();
+			classificacaoSelecao.buscarPorObjeto(c.getExClassificacao());
+			result.include("classificacaoSel", classificacaoSelecao);
+		}
+
+		if (c.getOrgaoObjeto() != null)
+			result.include("idOrgaoObjeto", c.getOrgaoObjeto().getIdOrgaoUsu());
 	}
 
 	@SuppressWarnings("all")

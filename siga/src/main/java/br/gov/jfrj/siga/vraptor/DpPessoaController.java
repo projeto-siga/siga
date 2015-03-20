daeee103e6d9d827c98298a782dfc89dfc2ad5b3
Gabriel Amaral <gabriel.amaral@db1.com.br>
2015-03-20 16:08:27 -0300

    [PRJ_TRF2][695] - OSI_FS0019 - 15. MIGRAÇÃO DA TELA DE BUSCAR PESSOA - commit inicial

---
+++ siga/src/main/java/br/gov/jfrj/siga/vraptor/DpPessoaController.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Copyright (c) 2006 - 2011 SJRJ.
+ * 
+ *     This file is part of SIGA.
+ * 
+ *     SIGA is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     SIGA is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with SIGA.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/*
+ * Criado em  13/09/2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package br.gov.jfrj.siga.vraptor;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+
+import br.com.caelum.vraptor.Result;
+import br.gov.jfrj.siga.base.AplicacaoException;
+import br.gov.jfrj.siga.base.Texto;
+import br.gov.jfrj.siga.cp.model.DpLotacaoSelecao;
+import br.gov.jfrj.siga.dp.DpLotacao;
+import br.gov.jfrj.siga.dp.DpPessoa;
+import br.gov.jfrj.siga.dp.dao.CpDao;
+import br.gov.jfrj.siga.dp.dao.DpPessoaDaoFiltro;
+import br.gov.jfrj.siga.model.Selecionavel;
+
+public class DpPessoaController extends SigaSelecionavelControllerSupport<DpPessoa, DpPessoaDaoFiltro> {
+	
+	private Long orgaoUsu;
+	
+	public DpPessoaController(HttpServletRequest request, Result result, CpDao dao,
+			SigaObjects so, EntityManager em) {
+		super(request, result, dao, so, em);
+		
+		result.on(AplicacaoException.class).forwardTo(this).appexception();
+		result.on(Exception.class).forwardTo(this).exception();
+		
+		setSel(new DpPessoa());
+		setItemPagina(10);
+	}
+
+	public void aBuscar() {
+		DpLotacao lotacaoTitular = getLotaTitular();
+		if ( param("postback") == null 
+				&& lotacaoTitular != null ) {
+			orgaoUsu = lotacaoTitular.getIdOrgaoUsuario();
+		}
+//		return super.aBuscar();
+	}
+	
+	 public void aExibir() {
+
+	 }
+
+	@Override
+	public DpPessoaDaoFiltro createDaoFiltro() {
+		final DpLotacaoSelecao lotacaoSel = new DpLotacaoSelecao();
+		final DpPessoaDaoFiltro flt = new DpPessoaDaoFiltro();
+		flt.setNome(Texto.removeAcentoMaiusculas(getNome()));
+		flt.setLotacao(lotacaoSel.buscarObjeto());
+		flt.setIdOrgaoUsu(orgaoUsu);
+		
+		String buscarFechadas = param("buscarFechadas");
+		flt.setBuscarFechadas(buscarFechadas != null ? Boolean
+				.valueOf(buscarFechadas) : false);
+		flt.setSituacaoFuncionalPessoa("");
+		
+		return flt;
+	}
+
+	@Override
+	public Selecionavel selecionarPorNome(final DpPessoaDaoFiltro flt)
+			throws AplicacaoException {
+		Selecionavel sel = null;
+
+		// Acrescenta o sesb e repete a busca
+		final String sigla = flt.getSigla();
+		flt.setSigla(getTitular().getSesbPessoa() + sigla);
+		sel = dao().consultarPorSigla(flt);
+		if (sel != null)
+			return sel;
+		flt.setSigla(sigla);
+
+		// Procura por nome
+		flt.setIdOrgaoUsu(getLotaTitular().getOrgaoUsuario().getIdOrgaoUsu());
+		flt.setNome(Texto.removeAcentoMaiusculas(flt.getSigla()));
+		flt.setSigla(null);
+		final List pessoas = dao().consultarPorFiltro(flt);
+		if (pessoas != null)
+			if (pessoas.size() == 1)
+				return (DpPessoa) pessoas.get(0);
+		return null;
+	}
+	
+}

c78c4863abda6f086bb2ee4e406f9788282e97d0
Heidi Kussakawa <heidiks@gmail.com>
2015-03-23 14:14:10 -0300

    [PRJ_TRF2][705] - OSI_FS0019 - 25. MIGRAÇÃO DA TELA DE BUSCAR CARGO - VERSIONAMENO ARQUIVOS

---
+++ siga/src/main/java/br/gov/jfrj/siga/vraptor/DpCargoController.java
@@ -0,0 +1,65 @@
+package br.gov.jfrj.siga.vraptor;
+
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.gov.jfrj.siga.base.Texto;
+import br.gov.jfrj.siga.dp.DpCargo;
+import br.gov.jfrj.siga.dp.dao.CpDao;
+import br.gov.jfrj.siga.dp.dao.DpCargoDaoFiltro;
+
+@Resource
+public class DpCargoController extends
+		SigaSelecionavelControllerSupport<DpCargo, DpCargoDaoFiltro> {
+	
+	public DpCargoController(HttpServletRequest request, Result result,
+			CpDao dao, SigaObjects so, EntityManager em) {
+		super(request, result, dao, so, em);
+	}
+	
+	@Get
+	@Post
+	@Path("/app/cargo/buscar")
+	public void busca(String nome,Long idOrgaoUsu, Integer offset, String postback) throws Exception{
+		Long orgaoUsu;
+		
+		if (param("postback") == null)
+			orgaoUsu = getLotaTitular().getOrgaoUsuario().getIdOrgaoUsu();
+		else
+			orgaoUsu = idOrgaoUsu;
+		
+		this.getP().setOffset(offset);
+		
+		if (nome == null)
+			nome = "";
+		
+		super.aBuscar(nome, postback);
+		
+		result.include("param", getRequest().getParameterMap());
+		result.include("request",getRequest());
+		result.include("itens",getItens());
+		result.include("tamanho",getTamanho());
+		result.include("orgaosUsu", getOrgaosUsu());
+		result.include("idOrgaoUsu",orgaoUsu);
+		result.include("nome",nome);
+		result.include("postbak",postback);
+		result.include("offset",offset);
+	}
+
+	@Override
+	public DpCargoDaoFiltro createDaoFiltro() {
+		final DpCargoDaoFiltro flt = new DpCargoDaoFiltro();
+		flt.setNome(Texto.removeAcentoMaiusculas(getNome()));
+		flt.setIdOrgaoUsu(paramLong("orgaoUsu"));
+		if (flt.getIdOrgaoUsu() == null)
+			flt.setIdOrgaoUsu(getLotaTitular().getOrgaoUsuario()
+					.getIdOrgaoUsu());
+		return flt;
+	}
+
+}

4f528d7594033f9d55d08c1e8e160005d1ac6d9d
andresousa17 <andresousa17@gmail.com>
2015-03-30 17:46:09 -0300

    Coloca o teste das 17h antes do teste de 2 dias no pedido e agendamento do dje

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/util/DatasPublicacaoDJE.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/util/DatasPublicacaoDJE.java
@@ -74,10 +74,10 @@ public class DatasPublicacaoDJE {
 		
 			if (isDisponibilizacaoDMais30())
 				return "Data de disponibilização está além do limite: mais de 31 dias a partir de hoje";
-			else if (isDisponibilizacaoAntesDeDMais2())
-				return "Data de disponibilização não permitida: menos de 2 dias a partir de hoje";
 			else if (sao17Horas() && apenasSolicitacao)
 				return "Data de disponibilização não permitida: Excedido Horário de Solicitação (17 Horas). Defina a disponibilização para um dia depois do escolhido";
+			else if (isDisponibilizacaoAntesDeDMais2())
+				return "Data de disponibilização não permitida: menos de 2 dias a partir de hoje";
 			else if (isDisponibilizacaoDomingo())
 				return "Data de disponibilização é domingo";
 			else if (isDisponibilizacaoSabado())

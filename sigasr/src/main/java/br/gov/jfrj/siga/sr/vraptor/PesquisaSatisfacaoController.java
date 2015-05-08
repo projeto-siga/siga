6c23a8fd979916b0f9b9d5e7eb693dd1509fd87e
jefferson.ferreira <jefferson.ferreira@db1.com.br>
2015-05-08 14:34:16 -0300

    51517 - LISTA DE PESQUISAS DE SATISFAÇÃO

--- sigasr/src/main/java/br/gov/jfrj/siga/sr/vraptor/PesquisaSatisfacaoController.java
+++ sigasr/src/main/java/br/gov/jfrj/siga/sr/vraptor/PesquisaSatisfacaoController.java
@@ -1,7 +1,9 @@
 package br.gov.jfrj.siga.sr.vraptor;
 
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
@@ -9,9 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
 import br.gov.jfrj.siga.cp.CpComplexo;
 import br.gov.jfrj.siga.dp.CpOrgaoUsuario;
 import br.gov.jfrj.siga.dp.dao.CpDao;
+import br.gov.jfrj.siga.model.Objeto;
+import br.gov.jfrj.siga.sr.model.SrPergunta;
 import br.gov.jfrj.siga.sr.model.SrPesquisa;
 import br.gov.jfrj.siga.sr.model.SrTipoPergunta;
 import br.gov.jfrj.siga.sr.validator.SrValidator;
@@ -29,16 +34,49 @@ public class PesquisaSatisfacaoController extends SrController {
 	@SuppressWarnings("unchecked")
 	@Path("/listar/{mostrarDesativados}")
 	public void listar(boolean mostrarDesativados) throws Exception {
-		
+
 		List<SrPesquisa> pesquisas = SrPesquisa.listar(mostrarDesativados);
 		List<SrTipoPergunta> tipos = SrTipoPergunta.buscarTodos();
 		List<CpOrgaoUsuario> orgaos = CpOrgaoUsuario.AR.findAll();
 		List<CpComplexo> locais = CpComplexo.AR.findAll();
-		
+
 		result.include("pesquisas", pesquisas);
 		result.include("tipos", tipos);
 		result.include("orgaos", orgaos);
 		result.include("locais", locais);
+	}
+
+	//@AssertAcesso(ADM_ADMINISTRAR)
+	@Path("/desativar")
+	public void desativar(Long id) throws Exception {
+		SrPesquisa pesq = SrPesquisa.AR.findById(id);
+		pesq.finalizar();
+
+		result.use(Results.http()).body(pesq.toJson());
+	}
+
+	// @AssertAcesso(ADM_ADMINISTRAR)
+	@Path("/reativar")
+	public void reativar(Long id) throws Exception {
+		SrPesquisa pesq = SrPesquisa.AR.findById(id);
+		pesq.salvar();
+
+		result.use(Results.http()).body(pesq.toJson());
+	}
+	
+	// @AssertAcesso(ADM_ADMINISTRAR)
+	@Path("/gravar")
+	public void gravar(SrPesquisa pesquisa, Set<SrPergunta> perguntaSet) throws Exception {
+		pesquisa = (SrPesquisa) Objeto.getImplementation(pesquisa);
+		pesquisa.setPerguntaSet((pesquisa.getPerguntaSet() != null) ? pesquisa
+				.getPerguntaSet() : new HashSet<SrPergunta>());
+		pesquisa.salvar();
+
+		result.use(Results.http()).body(pesquisa.toJson());
+	}
+	
+	public void listarDesativados() throws Exception {
+		listar(Boolean.TRUE);
 	}
 
 

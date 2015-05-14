41fc769d2ef49694051bd2a2f29f05d472d927ff
Anderson Zanichelli <anderzanic@gmail.com>
2015-05-14 17:33:44 -0300

    [PRJ_TRF2][821] - OSI_FS0021 PRJ_OSI - 01 ADAPTAÇÃO DO PROJETO PLAY PARA O USO DO FRAMEWORK VRAPTOR

---
+++ sigasr/src/main/java/br/gov/jfrj/siga/sr/vraptor/converter/SrConvertableEntityConverter.java
@@ -0,0 +1,10 @@
+package br.gov.jfrj.siga.sr.vraptor.converter;
+
+import br.com.caelum.vraptor.Convert;
+import br.gov.jfrj.siga.sr.model.SrConvertableEntity;
+import br.gov.jfrj.siga.vraptor.converter.BaseEntityConverter;
+
+@Convert(SrConvertableEntity.class)
+public class SrConvertableEntityConverter extends BaseEntityConverter<SrConvertableEntity>{
+
+}

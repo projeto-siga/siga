fa12d82d57f1d9532b8aa2960f29e444892f6c50
Leonardo Lopes <leonardo.lopes@db1.com.br>
2015-05-25 13:36:57 -0300

    Removendo os metodos/paginas de testes.

--- sigasr/src/main/java/br/gov/jfrj/siga/sr/vraptor/SolicitacaoController.java
+++ sigasr/src/main/java/br/gov/jfrj/siga/sr/vraptor/SolicitacaoController.java
@@ -9,11 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
-import br.com.caelum.vraptor.Validator;
-import br.com.caelum.vraptor.validator.ValidationMessage;
 import br.gov.jfrj.siga.dp.dao.CpDao;
 import br.gov.jfrj.siga.sr.model.SrAcao;
-import br.gov.jfrj.siga.sr.model.SrMeioComunicacao;
 import br.gov.jfrj.siga.sr.model.SrSolicitacao;
 import br.gov.jfrj.siga.sr.model.SrSolicitacao.SrTarefa;
 import br.gov.jfrj.siga.sr.notifiers.Correio;
@@ -24,53 +21,22 @@ import br.gov.jfrj.siga.vraptor.SigaObjects;
 @Path("app/solicitacao")
 public class SolicitacaoController extends SrController {
     private Correio correio;
-    private Validator validator;
 
-    public SolicitacaoController(HttpServletRequest request, Result result, CpDao dao, SigaObjects so, EntityManager em, SrValidator srValidator, Correio correio, Validator validator) {
+    public SolicitacaoController(HttpServletRequest request, Result result, CpDao dao, SigaObjects so, EntityManager em, SrValidator srValidator, Correio correio) {
         super(request, result, dao, so, em, srValidator);
         this.correio = correio;
-        this.validator = validator;
     }
 
-    @Path("/teste")
-    public void teste(boolean banco) {
-        SrSolicitacao solicitacao = new SrSolicitacao();
-        if (banco) {
-            solicitacao = (SrSolicitacao) SrSolicitacao.AR.all().fetch().get(0);
-        }
-        
-        //TODO: controllers.SigaApplication.podeUtilizarServico('SIGA;GC')
-        boolean podeUtilizarServicoSigaGC = false;
-        
-        result.include("meiosComunicadaoList", SrMeioComunicacao.values());
-        result.include("locaisDisponiveis", solicitacao.getLocaisDisponiveis());
-        result.include("solicitacao", solicitacao);
-        result.include("itemConfiguracao",solicitacao.getItemConfiguracao());
-        result.include("podeUtilizarServicoSigaGC",podeUtilizarServicoSigaGC);
-    }
-
-    @Path("/testeErro")
-    public void testeErro() {
-        validator.add(new ValidationMessage("Chamas", "nome.teste"));
-        validator.add(new ValidationMessage("Eternas", "nome.teste"));
-        validator.add(new ValidationMessage("do", "nome.teste"));
-        validator.add(new ValidationMessage("Aconchego", "nome.teste"));
-
-        validator.onErrorUsePageOf(this).teste(false);
-
-    }
-    
     @Path("/exibirAcao")
     public void exibirAcao(SrSolicitacao solicitacao) throws Exception {
         Map<SrAcao, List<SrTarefa>> acoesEAtendentes = solicitacao.getAcoesEAtendentes();
-        result.include("solicitacao",solicitacao);
-        result.include("acoesEAtendentes",acoesEAtendentes);
+        result.include("solicitacao", solicitacao);
+        result.include("acoesEAtendentes", acoesEAtendentes);
     }
-    
+
     @Path("/exibirAtributos")
     public void exibirAtributos(SrSolicitacao solicitacao) throws Exception {
-        result.include("solicitacao",solicitacao);
+        result.include("solicitacao", solicitacao);
     }
 
 }
-

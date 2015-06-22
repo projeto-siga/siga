1f19f7ce55ca465945fe2d61dffbccdb59df9099
Guilherme Chaddad <guilherme.rufino@db1.com.br>
2015-06-22 15:16:55 -0300

    [PRJ_TRF2][1112] - OSI_FS0023 - SIGA-SR - PRJ_DES - 04 ERROS PAGINA INICIAL NÃO APRESENTA CADASTRO DE SOLICITAÇÃO

--- sigasr/src/main/java/br/gov/jfrj/siga/sr/vraptor/PrincipalController.java
+++ sigasr/src/main/java/br/gov/jfrj/siga/sr/vraptor/PrincipalController.java
@@ -1,13 +1,24 @@
 package br.gov.jfrj.siga.sr.vraptor;
 
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.Validator;
+import br.gov.jfrj.siga.dp.dao.CpDao;
+import br.gov.jfrj.siga.sr.validator.SrValidator;
+import br.gov.jfrj.siga.vraptor.SigaObjects;
 
 @Resource
-public class PrincipalController {
-
+public class PrincipalController  extends SrController{
+	public PrincipalController(HttpServletRequest request, Result result, CpDao dao, SigaObjects so, EntityManager em,  SrValidator srValidator, Validator validator) {
+        super(request, result, dao, so, em, srValidator);
+    }
 	@Path("/app/principal")
-	public void principal() {
+	public void principal() throws Exception {
 		//Principal
+		result.redirectTo(SolicitacaoController.class).editar(null);
 	}
 }

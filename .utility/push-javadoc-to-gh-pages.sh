4a8b70b7558b4e5218c40eb97956448396f723b1
Renato Crivano <renato.crivano@gmail.com>
2015-09-01 14:25:47 -0300

    TRAVIS: melhorando os javadocs e colocando o mvn em modo quiet. Também, desabilitando os testes do siga-integration-test

--- .utility/push-javadoc-to-gh-pages.sh
+++ .utility/push-javadoc-to-gh-pages.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+echo -e “some vars…\n”
+echo -e “$TRAVIS_REPO_SLUG”
+echo -e “\n”
+echo -e “$TRAVIS_JDK_VERSION”
+echo -e “\n”
+echo -e “$TRAVIS_PULL_REQUEST”
+echo -e “\n”
+echo -e “$TRAVIS_BRANCH”
+echo -e “\n”
+pwd
+echo -e “\n”
+ls
+echo -e “\n”
+
 if [ "$TRAVIS_REPO_SLUG" == “projeto-siga/siga” ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == “vraptor” ]; then
 
   echo -e "Publishing javadoc...\n"
 
-  cp -R build/docs/javadoc $HOME/javadoc-latest
+  cp -R target/site/apidocs $HOME/javadoc-latest
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
@@ -14,7 +28,7 @@ if [ "$TRAVIS_REPO_SLUG" == “projeto-siga/siga” ] && [ "$TRAVIS_JDK_VERSION"
   cd gh-pages
   git rm -rf ./javadoc
   pwd
-  cp -Rf $HOME/target/site/apidocs ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
   git add -f .
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null

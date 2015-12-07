4ca659c6e9d1b3a099e603ea49a7f5a96a272233
Renato Crivano <renato.crivano@gmail.com>
2015-12-07 17:00:34 -0200

    GSA: Corrigindo o nome do pacote

--- siga-gc-gsa/src/main/java/br/gov/jfrj/siga/gc/gsa/BufferingPusher.java
+++ siga-gc-gsa/src/main/java/br/gov/jfrj/siga/gc/gsa/BufferingPusher.java
@@ -16,7 +16,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with SIGA.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package br.gov.jfrj.siga.ex.gsa;
+package br.gov.jfrj.siga.gc.gsa;
 
 import java.util.ArrayList;
 import java.util.logging.Logger;

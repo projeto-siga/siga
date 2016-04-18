199ae8ca2646c01df3eb239a76f5aad5043c19b8
Markenson <markensonfranca@gmail.com>
2016-04-18 16:11:27 -0300

    Coloca expedientes ordenados pelo número. Closes #793

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExBL.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExBL.java
@@ -39,6 +39,7 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -7394,6 +7395,7 @@ public class ExBL extends CpBL {
 			}
 		}
 
+		Collections.sort(assinaveis, new ExAssinavelComparador());
 		return assinaveis;
 	}
 }

0005734b7dd58eaab78c865257759c9519bbff14
Markenson França <kpf@corp.jfrj.gov.br>
2016-08-26 14:05:19 -0300

    Inclui arquivos iniciais para administracao LDAP

---
+++ siga/src/main/java/br/gov/jfrj/siga/vraptor/AdminIntegracaoLDAPController.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2006 - 2011 SJRJ.
+ * 
+ *     This file is part of SIGA.
+ * 
+ *     SIGA is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     SIGA is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with SIGA.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+/*
+ * Criado em  13/09/2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package br.gov.jfrj.siga.vraptor;
+
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.gov.jfrj.siga.dp.dao.CpDao;
+
+@Resource
+public class AdminIntegracaoLDAPController extends SigaController {
+	
+	public AdminIntegracaoLDAPController(HttpServletRequest request, Result result, SigaObjects so, EntityManager em) {
+		super(request, result, CpDao.getInstance(), so, em);
+	}
+	
+	@Get("/app/admin/ldap/administrar")
+	public void administrar() throws Exception {
+	}
+	
+
+}
---
+++ siga/src/main/webapp/WEB-INF/page/admin/ldap/administrar.jsp
@@ -0,0 +1,19 @@
+<%@ include file="/WEB-INF/page/include.jsp"%>
+
+<siga:pagina popup="false" titulo="Administração de integração ao LDAP">
+	<div class="gt-bd clearfix">
+		<div class="gt-content clearfix">
+
+			<h1>Administração de integração ao LDAP</h1>
+
+			<h2>Testar Conexão</h2>
+			<div class="gt-form gt-content-box"></div>
+			
+			<h2>Trocar Senha</h2>
+			<div class="gt-form gt-content-box"></div>
+
+			<h2>Parâmetros carregados</h2>
+			<div class="gt-form gt-content-box"></div>
+			
+		</div>
+</siga:pagina>

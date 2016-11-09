f34f6fc57043341f8941049d9df64680c3cde42a
rafaela <rafaela00castro@gmail.com>
2016-11-09 14:55:29 -0200

    SR: adiciona novos tipos de campos (data, hora, textarea) e uma classe de validação. tarefa 1 #1087

--- sigasr/src/main/webapp/javascripts/atributo.editavel.js
+++ sigasr/src/main/webapp/javascripts/atributo.editavel.js
@@ -23,12 +23,12 @@ AtributoEditavel.prototype = {
 	constructor: AtributoEditavel,
 	
 	iniciar: function() {
-		AtributoEditavel.DIV_EDITAVEL = this.getElementoComAtributoEditavel();
-		AtributoEditavel.FORM = this.construirForm();
+		AtributoEditavel.ELEMENTO_EDITAVEL = this.getElementoComAtributoEditavel();
+		AtributoEditavel.FORM_EDITAVEL = this.construirForm();
 	},
 	
 	construirForm: function() {
-		var form = new FormEditavel(this.propriedades, AtributoEditavel.DIV_EDITAVEL);
+		var form = new FormEditavel(this.propriedades, AtributoEditavel.ELEMENTO_EDITAVEL);
 		return form.construir();
 	},
 	
@@ -38,7 +38,7 @@ AtributoEditavel.prototype = {
 	
 	editar: function() {
 		this.iniciar();
-		AtributoEditavel.DIV_EDITAVEL.hide().after(AtributoEditavel.FORM);
+		AtributoEditavel.ELEMENTO_EDITAVEL.hide().after(AtributoEditavel.FORM_EDITAVEL);
 	},
 	
 	toString: function() {
@@ -47,16 +47,16 @@ AtributoEditavel.prototype = {
 	}
 }
 
-AtributoEditavel.FORM = "";
-AtributoEditavel.DIV_EDITAVEL = "";
+AtributoEditavel.FORM_EDITAVEL = "";
+AtributoEditavel.ELEMENTO_EDITAVEL = "";
 
 
 /**
  * @class formEditavel
  **/
-var FormEditavel = function (propriedades, divEditavel) {
+var FormEditavel = function (propriedades, elementoEditavel) {
 	this.propriedades = propriedades;
-	this.divEditavel = divEditavel;
+	this.elementoEditavel = elementoEditavel;
 	
 	this.form = "<form class='form-editavel' action='#' method='post' enctype='multipart/form-data'>" +	
 						"<div class='div-campo-editavel' style='float: left; margin-right: 11px;'></div>" +
@@ -99,18 +99,16 @@ FormEditavel.prototype = {
 	},
 	
 	acaoOk: function() {
-		var div = this.divEditavel;
-		var url = this.propriedades.urlDestino;
-		var form = this.form;
-		var param = this.getParamToBlock();
-		form.find('.botao-editavel > .ok').click(function(event) {
+		var self = this;
+		self.form.find('.botao-editavel > .ok').click(function(event) {
 			event.preventDefault();
-			form.block(param);
-			Siga.ajax(url, form.serialize(), "POST", function(response) {
-				div.find('span').text(response);
-				div.show();
-				form.unblock();
-				form.remove();
+			var parametros = self.getParamToBlock();
+			self.form.block(parametros);
+			Siga.ajax(self.propriedades.urlDestino, self.form.serialize(), "POST", function(response) {
+				self.elementoEditavel.find('span').text(response);
+				self.elementoEditavel.show();
+				self.form.unblock();
+				self.form.remove();
 			});
 		});
 	},
@@ -127,10 +125,10 @@ FormEditavel.prototype = {
 	},
 	
 	acaoCancelar: function() {
-		var div = this.divEditavel;
+		var self = this;
 		this.form.find('.botao-editavel > .cancelar').click(function() {
-			div.show();
-			this.form.remove();
+			self.elementoEditavel.show();
+			self.form.remove();
 		});
 	},
 	
@@ -151,10 +149,17 @@ FormEditavel.prototype = {
 	}
 }
 
+/**
+ * @class campoEditavel
+ * constroi o campo de acordo com os tipos existentes [DATA, TEXTO, NUM_INTEIRO...]
+ **/
 var CampoEditavel = function (propriedades) {
 	this.campoFactory =  new CampoEditavelFactory(propriedades).getInstancia();
 }
 
+/**
+ * metodos
+ **/
 CampoEditavel.prototype = {
 	constructor: CampoEditavel,
 	
@@ -162,11 +167,16 @@ CampoEditavel.prototype = {
 		this.campoFactory.iniciar();
 		this.campoFactory.inserirParametros();
 		this.campoFactory.inserirNomeEValor();
-		
+		this.campoFactory.inserirMascara();
+	
 		return this.campoFactory.campo;
 	}
 }
 
+/**
+ * @class campoEditavelFactory
+ * define o tipo de campo que sera colocado dentro da @class formEditavel atraves da @class campoEditavel
+ **/
 var CampoEditavelFactory = function(propriedades) {
 	this.propriedades = propriedades;
 	this.input 		= "<input class='campo-editavel' />";
@@ -180,16 +190,23 @@ CampoEditavelFactory.prototype = {
 	getInstancia: function() {
 		if (this.propriedades.tipo === 'TEXTO')
 			return new Texto(this.propriedades, this.input);
-		if (this.propriedades.tipo === 'NUM_INTEIRO' || this.propriedades.tipo === 'NUM_DECIMAL')
+		if (this.propriedades.tipo === 'NUM_INTEIRO' || this.propriedades.tipo === 'NUM_DECIMAL' || this.propriedades.tipo === 'HORA')
 			return new Numero(this.propriedades, this.input);
 		if (this.propriedades.tipo === 'VL_PRE_DEFINIDO')
 			return new ValorPreDefinido(this.propriedades, this.select);
 		if (this.propriedades.tipo === 'HIDDEN')
 			return new Hidden(this.propriedades, this.input);
+		if (this.propriedades.tipo === 'TEXT_AREA')
+			return new Textarea(this.propriedades, this.textarea);
+		if (this.propriedades.tipo === 'DATA')
+			return new Data(this.propriedades, this.input);
 	}
 }
 
-
+/**
+ * @class campoEditavelAbstract
+ * contem os comportamentos comuns entre as diversas @class que existem para cada tipo de campo
+ **/
 var CampoEditavelAbstract = function(propriedades, campo) {
 	this.propriedades = propriedades;
 	this.campo = campo;
@@ -204,26 +221,31 @@ CampoEditavelAbstract.prototype = {
 	},
 	
 	inserirNomeEValor: function() {
-		this.campo.attr("name", this.propriedades.nome)
-		 		  .attr("value", this.propriedades.valor);
+		this.campo.prop("name", this.propriedades.nome)
+		 		  .prop("value", this.propriedades.valor);
 	},
 
 	inserirAtributos: function(arrayComParamentros) {
-		var campo = this.campo;
+		var self = this;
 		arrayComParamentros.forEach(function(elemento) {
-			campo.attr(elemento.key, elemento.value);
+			self.campo.prop(elemento.key, elemento.value);
 		});
-		this.campo = campo;
 	},
 	
 	inserirOpcoes: function() {
-		var campo = this.campo;
+		var self = this;
 		var arrayComValores = this.propriedades.valoresPreDefinidos.slice(1, -1).split(",");
 		arrayComValores.forEach(function(elemento) {
-			campo.append($('<option>', {value: elemento.trim()})
+			self.campo.append($('<option>', {value: elemento.trim()})
 				 .text(elemento.trim())); 
 		});
-		this.campo = campo;
+	},
+	
+	inserirMascara: function() {
+		var validador = new Validador(this.campo, this.propriedades.tipo);
+		var nomeFuncao = validador.getNomeFuncaoParaInserirMascara();
+		if (typeof validador[nomeFuncao] == 'function')
+			validador[nomeFuncao]();
 	}
 }
 
@@ -282,6 +304,57 @@ ValorPreDefinido.prototype.inserirParametros = function() {
 };
 
 
+var Textarea = function(propriedades, campo) {
+	CampoEditavelAbstract.call(this, propriedades, campo);
+	this.coluna = {key: 'cols', value: '75'};
+	this.linha = {key: 'rows', value: '5'};
+	this.maximo = {key: 'maxlength', value: '255'};
+}
+
+Textarea.prototype = new CampoEditavelAbstract(this.propriedades, this.campo); 
+
+Textarea.prototype.constructor = Textarea;
+	
+Textarea.prototype.getParametros = function() {
+	return new Array(this.coluna, this.linha, this.maximo);
+};
+	
+Textarea.prototype.inserirParametros = function() {
+	var arrParam = this.getParametros();
+	this.inserirAtributos(arrParam);
+};
+
+
+var Data = function(propriedades, campo) {
+	CampoEditavelAbstract.call(this, propriedades, campo);
+	this.tipo = {key: 'type', value: 'text'};
+	this.id = {key: 'id', value: propriedades.nome.replace('.','')};
+}
+
+Data.prototype = new CampoEditavelAbstract(this.propriedades, this.campo); 
+
+Data.prototype.constructor = Data;
+	
+Data.prototype.getParametros = function() {
+	return new Array(this.tipo, this.id);
+};
+	
+Data.prototype.inserirParametros = function() {
+	var arrParam = this.getParametros();
+	this.inserirAtributos(arrParam);
+	this.inserirDatepicker();
+};
+
+Data.prototype.inserirDatepicker = function() {
+	this.campo.datepicker({
+	    dateFormat: 'dd/mm/yy',
+	    monthNames: ["Janeiro", "Fevereiro", "Mar\u00E7o", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+	    dayNamesMin: ["Dom","Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+		firstDay: 0
+	});
+};
+
+
 var Hidden = function(propriedades, campo) {
 	CampoEditavelAbstract.call(this, propriedades, campo);
 	this.tipo = {key: 'type', value: 'hidden'};
@@ -299,3 +372,65 @@ Hidden.prototype.inserirParametros = function() {
 	var arrParam = this.getParametros();
 	this.inserirAtributos(arrParam);
 };
+
+
+/**
+ * @class validador
+ */
+var Validador = function(campo, tipo) {
+	this.campo = campo;
+	this.tipo = tipo;
+}
+/**
+ * metodos
+ **/
+Validador.prototype = {
+	constructor: Validador,
+	
+	getNomeFuncaoParaInserirMascara: function() {
+		return 	'inserirMascara_' + this.tipo;
+	},
+	
+	inserirMascara_HORA: function() {
+		var self = this;
+		this.campo.mask("99:99", {completed: function() {
+			self.validar_HORA(this.val());
+		}});
+	},
+	
+	validar_HORA: function(valor) {
+		var tempo = valor.split(':');
+		var hora = tempo[0];
+		var minuto = tempo[1];
+		if (hora > 23 || minuto > 59) {
+			this.mostrarMensagem(this.campo, 'Horário inválido para o padrão 24h') 
+			return;
+		}
+		this.removerMensagem(this.campo);
+	},
+	
+	inserirMascara_NUM_INTEIRO: function() {
+		this.campo.mask('9?99999999', {placeholder: ' '});
+	},
+	
+	inserirMascara_NUM_DECIMAL: function() {
+		$.mask.definitions['d'] = '[0-9,]';
+		this.campo.mask('9?dddddddd', {placeholder: ' '});
+	},
+	
+	inserirMascara_DATA: function() {
+		this.campo.mask('99/99/9999');
+	},
+	
+	mostrarMensagem: function(campo, msg) {
+		var span = $('<span>'); 
+		span.addClass('error')
+			.css({'margin-left':'10px', 'color':'red'})
+			.text(msg)
+			.insertAfter(campo);
+	},
+	
+	removerMensagem: function(campo) {
+		campo.siblings('span.error').remove();
+	}
+}

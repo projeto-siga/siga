0fd39b5819461b214a983854bd219d81d7237cef
chrystinne <chrystinne@gmail.com>
2017-02-22 19:08:39 -0300

    SR: Inclui mais um motivo de fechamento de chamados (Tarefa concluída". Closes #1160.

--- sigasr/src/main/java/br/gov/jfrj/siga/sr/model/SrTipoMotivoFechamento.java
+++ sigasr/src/main/java/br/gov/jfrj/siga/sr/model/SrTipoMotivoFechamento.java
@@ -3,12 +3,13 @@ package br.gov.jfrj.siga.sr.model;
 public enum SrTipoMotivoFechamento {
 
 	ATENDIMENTO_CONCLUÍDO(1, "Usuário atendido. Chamado pode ser fechado."),
+	ATENDIMENTO_CONCLUIDO_PARCIALMENTE(2, "Atendido parcialmente. Requer atuação de outra equipe."),
+	TAREFA_CONCLUÍDA(9, "Tarefa concluída."),
 	ERRO_CLASSIFICACAO(3, "Devolução por erro de escalonamento."), 
 	DADOS_INCORRETOS(4, "Script de atendimento não cumprido - Requer mais informações."),
 	SOLUCAO_ALTERNATIVA(5, "Solução de contorno criada/aplicada - Avaliar abertura de Problema."),
 	ATENDIMENTO_NEGADO(6, "Chamado não atendido - Não atende pré-requisitos."),
 	//ATENDIMENTO_EM_OUTRA_SOLICITACAO(7, "Atendimento efetuado em outra solicitação"),
-	ATENDIMENTO_CONCLUIDO_PARCIALMENTE(2, "Atendido parcialmente. Requer atuação de outra equipe."),
 	ATENDIMENTO_OUTRO_CANAL(8, "Solicitação já se encontra em atendimento por outro canal.");
 	
     private int idTipoMotivoFechamento;

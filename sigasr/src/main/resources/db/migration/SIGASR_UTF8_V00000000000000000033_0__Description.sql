fc9d7c5cd61cb2adb4f693ca7fed13ebc44b3701
Markenson <markensonfranca@gmail.com>
2017-08-15 18:55:47 -0300

    Update SIGASR_UTF8_V00000000000000000033_0__Description.sql
    Inclui comentário no banco de dados

--- sigasr/src/main/resources/db/migration/SIGASR_UTF8_V00000000000000000033_0__Description.sql
+++ sigasr/src/main/resources/db/migration/SIGASR_UTF8_V00000000000000000033_0__Description.sql
@@ -1,3 +1,4 @@
 -- Adiciona a coluna UST à tabela sr_tipo_acao 
 -- O campo UST indica um número de UST para o tipo de ação  
 alter table sigasr.sr_tipo_acao add UST NUMBER default 0;
+comment on column SIGASR.SR_TIPO_ACAO is 'O campo UST indica um número de UST para o tipo de ação';

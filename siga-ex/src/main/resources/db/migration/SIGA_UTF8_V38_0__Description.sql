c2dc2cb8e979d64ced859d003926ee6ec84b9481
Joao Luis <joaoluis@trf2.jus.br>
2017-09-06 14:01:31 -0300

    closes #1210 - Inclusão dos perfis liquidante e diretor/coordenador para atender mudança no workflow e modelo eof-pagamento.

---
+++ siga-ex/src/main/resources/db/migration/SIGA_UTF8_V38_0__Description.sql
@@ -0,0 +1,3 @@
+-- Inclui dois papéis novos para utilziação no modelo - eof pagamento (https://github.com/projeto-siga/siga/issues/1210)
+Insert into SIGA.EX_PAPEL (ID_PAPEL,DESC_PAPEL) values ('5','Liquidante');
+Insert into SIGA.EX_PAPEL (ID_PAPEL,DESC_PAPEL) values ('6','Diretor/Coordenador');

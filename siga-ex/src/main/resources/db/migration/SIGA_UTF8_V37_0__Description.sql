83fafca00ed40dbb6495c7809d6ec08aa9bc2431
Markenson <markensonfranca@gmail.com>
2017-10-20 17:26:18 -0200

    Ajusta numeracao da sequence antes de executar o insert (pois estava causando erro no build do docker)

--- siga-ex/src/main/resources/db/migration/SIGA_UTF8_V37_0__Description.sql
+++ siga-ex/src/main/resources/db/migration/SIGA_UTF8_V37_0__Description.sql
@@ -1,2 +1,5 @@
 -- Inclui um modelo de documento capturado (https://github.com/projeto-siga/docker/issues/29)
+-- necessario chamar o nextval para acertar a numeracao da sequence com os modelos
+select SIGA.EX_MODELO_SEQ.nextval from dual; 
 Insert into SIGA.EX_MODELO (ID_MOD,NM_MOD,DESC_MOD,conteudo_blob_mod,CONTEUDO_TP_BLOB,ID_FORMA_DOC,HIS_ID_INI,HIS_ATIVO) values (SIGA.EX_MODELO_SEQ.nextval,'Planta','Planta',utl_raw.cast_to_raw(' '),'template/freemarker',(SELECT ID_FORMA_DOC FROM SIGA.EX_FORMA_DOCUMENTO WHERE DESCR_FORMA_DOC = 'Documento Capturado'),SIGA.EX_MODELO_SEQ.currval,'1');
+

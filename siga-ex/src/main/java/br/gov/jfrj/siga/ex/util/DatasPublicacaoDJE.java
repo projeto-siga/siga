28f594c85eb7523b57e77eec52599f9ffb2ddb8c
Adriana <adrigrassi@gmail.com>
2017-11-07 12:11:51 -0200

    DOC: Alteração para criticar se a data de disponibilização escolhida pelo usuário é feriado antes das outras críticas.

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/util/DatasPublicacaoDJE.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/util/DatasPublicacaoDJE.java
@@ -75,6 +75,9 @@ public class DatasPublicacaoDJE {
 				return "Data de disponibilização está além do limite: mais de 31 dias a partir de hoje";
 			else if (foraDoHorarioDe11as17horas() && apenasSolicitacao)
 				return "Data de disponibilização não permitida: Excedido Horário de Solicitação (17 Horas). Defina a disponibilização com mais de 2 dias a partir de hoje";
+			else if (isDisponibilizacaoFeriado())
+				return "Data de disponibilização ￩ feriado: "
+						+ getFeriadoDisponibilizacao().getDscFeriado();
 			else if (isDisponibilizacaoAntesDeDMais2())
 				return "Data de disponibilização não permitida: menos de 2 dias a partir de hoje";
 			else if (isDisponibilizacaoDomingo())

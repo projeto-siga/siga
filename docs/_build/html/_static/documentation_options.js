f121272f9689957510bd9dc48853dbb6b366cfb0
Renato Crivano <renato.crivano@gmail.com>
2018-04-05 12:42:43 -0300

    DOC: Removendo e ignorando o diretório de _build do manual

--- docs/_build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '6.0.2',
-    LANGUAGE: 'pt_BR',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};

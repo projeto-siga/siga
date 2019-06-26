e133b46ed7259c22834e8e549ed0b4259a471923
Newton <nyhei@apoioprodesp.sp.gov.br>
2019-06-26 16:14:38 -0300

    209 - Tela de Marcadores

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/Mesa.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/Mesa.java
@@ -319,7 +319,12 @@ public class Mesa {
 
 		//
 		RETENCAO_INSS(1002, "Retenção de INSS", "fas fa-tag", "",
-				GrupoDeMarcadorEnum.ALERTA);
+				GrupoDeMarcadorEnum.ALERTA),
+		//
+		PRIORITARIO(1003, "Prioritário", "fas fa-star", "", GrupoDeMarcadorEnum.ALERTA),
+		//		
+		RESTRICAO_ACESSO(1004, "Restrição de Acesso", "fas fa-user-secret", "", GrupoDeMarcadorEnum.ALERTA);
+		//		
 
 		private MarcadorEnum(int id, String nome, String icone,
 				String descricao, GrupoDeMarcadorEnum grupo) {

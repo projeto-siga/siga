09ad3193a07be58dd97835dcb7f2773e992cc1a5
Dinarde <dinarde@gmail.com>
2019-07-14 22:06:11 -0300

    #137 - Sintaxe de Pesquisa de Modelo por Nome + Descrição
    Criado o item palavra-chave na lista para suportar a descrição e a busca é realizada pela concatenação do Nome com a Descrição do Modelo

--- sigaex/src/main/java/br/gov/jfrj/siga/util/ListaHierarquica.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/util/ListaHierarquica.java
@@ -30,6 +30,27 @@ public class ListaHierarquica {
 			}
 		}
 	}
+	
+	/* Com Busca complementar*/
+	public void add(String text, String keywords, Long value, boolean selected) {
+		if (text == null || value == null)
+			return;
+		String as[] = text.split(DIVIDER);
+		for (int i = 0; i < as.length; i++) {
+			String s = as[i];
+
+			// igual ao anterior
+			if (i < groups.size() && s.equals(groups.get(i)))
+				continue;
+			else {
+				groups = groups.subList(0, i);
+				groups.add(s);
+				boolean group = i < as.length - 1;
+				l.add(new ListaHierarquicaItem(i + 1, s, group ? null : Texto.removeAcentoMaiusculas(text.concat(" - " + keywords)), keywords,
+						group ? null : value, group, group ? false : selected));
+			}
+		}
+	}
 
 	public List<ListaHierarquicaItem> getList() {
 		return l;

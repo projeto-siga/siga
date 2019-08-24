9405dad9d33d4e999ee7f2ac4d3750979472b18e
Dinarde <dinarde@gmail.com>
2019-08-24 15:23:27 -0300

    Retirada a aplicação dos índices

--- siga-ex/src/main/resources/db/migration/SIGA_UTF8_V56_0__Indice para Tabela Ex_Boletim_Doc.sql
+++ siga-ex/src/main/resources/db/migration/SIGA_UTF8_V56_0__Indice para Tabela Ex_Boletim_Doc.sql
@@ -4,4 +4,4 @@ ALTER SESSION SET CURRENT_SCHEMA=siga;
 --  DDL for Index ID_BOLETIM
 --------------------------------------------------------
 
-CREATE INDEX "SIGA"."IDX_ID_BOLETIM" ON "SIGA"."EX_BOLETIM_DOC" ("ID_BOLETIM");
+-- CREATE INDEX "SIGA"."IDX_ID_BOLETIM" ON "SIGA"."EX_BOLETIM_DOC" ("ID_BOLETIM");
--- siga-ex/src/main/resources/db/migration/SIGA_UTF8_V57_0__Indice para Tabela Ex Tp Forma Doc.sql
+++ siga-ex/src/main/resources/db/migration/SIGA_UTF8_V57_0__Indice para Tabela Ex Tp Forma Doc.sql
@@ -4,4 +4,4 @@ ALTER SESSION SET CURRENT_SCHEMA=siga;
 --  DDL for Index ID_BOLETIM
 --------------------------------------------------------
 
-CREATE INDEX "SIGA"."IDX_EX_TP_FORMA_DOC" ON "SIGA"."EX_TP_FORMA_DOC" ("ID_TP_DOC");
+-- CREATE INDEX "SIGA"."IDX_EX_TP_FORMA_DOC" ON "SIGA"."EX_TP_FORMA_DOC" ("ID_TP_DOC");

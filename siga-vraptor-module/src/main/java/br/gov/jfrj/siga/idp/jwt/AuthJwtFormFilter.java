87da203e3497a2db1e770ec8ca8fcea241777ec6
jlo <joaoluis@trf2.jus.br>
2019-10-07 18:35:05 -0300

    GERAL: Tratando jwtexpiredexception redirecinando para tela de login para neste caso não enviar mensagem.

--- siga-vraptor-module/src/main/java/br/gov/jfrj/siga/idp/jwt/AuthJwtFormFilter.java
+++ siga-vraptor-module/src/main/java/br/gov/jfrj/siga/idp/jwt/AuthJwtFormFilter.java
@@ -28,6 +28,7 @@ import br.gov.jfrj.siga.cp.bl.Cp;
 import br.gov.jfrj.siga.model.ContextoPersistencia;
 
 import com.auth0.jwt.JWTAudienceException;
+import com.auth0.jwt.JWTExpiredException;
 import com.auth0.jwt.JWTVerifyException;
 
 public class AuthJwtFormFilter implements Filter {
@@ -205,7 +206,7 @@ public class AuthJwtFormFilter implements Filter {
 		
 		//Envia Mensagem para Tela de Login
 		HttpSession session = req.getSession(false);	
-		if (e.getClass() != SigaJwtInvalidException.class) {
+		if (e.getClass() != SigaJwtInvalidException.class && e.getClass() != JWTExpiredException.class)  {
 			session.setAttribute("loginMensagem", SigaMessages.getMessage("login.erro.jwt"));
 		} else 
 			session.setAttribute("loginMensagem", "");

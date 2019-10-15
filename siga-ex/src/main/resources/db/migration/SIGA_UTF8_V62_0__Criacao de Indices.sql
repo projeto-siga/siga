b5bd2ac5351735fa172ce46c1f7a477ca612f4d3
Unknown <unknown@example.com>
2019-10-15 16:50:59 -0300

    #528 - Criação de Índices e Ajuste de Trigger

---
+++ siga-ex/src/main/resources/db/migration/SIGA_UTF8_V62_0__Criacao de Indices.sql
@@ -0,0 +1,19 @@
+CREATE INDEX siga.iacs_ex_movimentacao_00001 ON
+    siga.ex_movimentacao (
+        id_mobil,
+        id_tp_mov,
+        id_mov_canceladora
+    )
+        PCTFREE 10 INITRANS 20 MAXTRANS 255 COMPUTE STATISTICS COMPRESS 2 NOLOGGING TABLESPACE "SIGA_DATA_TS"
+            PARALLEL 16;
+
+ALTER INDEX siga.iacs_ex_movimentacao_00001 PARALLEL ( DEGREE 1);
+CREATE INDEX siga.iacs_ex_mobil_00001 ON 
+	siga.ex_mobil
+             ( 
+                          id_doc, 
+                          id_mobil 
+             ) 
+         pctfree 10 INITRANS 20 MAXTRANS 255 compute STATISTICS COMPRESS 2 nologging tablespace "SIGA_DATA_TS" 
+             PARALLEL 16;
+ALTER INDEX siga.iacs_ex_movimentacao_00001 PARALLEL ( degree 1 );

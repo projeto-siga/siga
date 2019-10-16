1d9416924f040d726715d12c8de40ed76572b9f0
Adriana <adrigrassi@gmail.com>
2019-10-16 16:17:50 -0300

    DOC: retirando o arquivo que criava a configuração Juntada Automática novamente. (Cód. 41).

--- siga-ex/src/main/resources/db/migration/SIGA_UTF8_V51_1__Juntada_Automatica.sql
+++
@@ -1,5 +0,0 @@
-﻿---------------------------------------------------------
---	SCRIPT: Criando a configuração Juntada automática
----------------------------------------------------------
-
-insert into corporativo.cp_tipo_configuracao values(41, 'Juntada Automática', 5);

77697490934cd2b5397f124cb6e6d8edbaf07c14
Dinarde Bezerra <dinarde@gmail.com>
2019-10-21 19:28:16 -0300

    Versão 8.0.1.47
    HOTFIX: Sigla de Numeração de Vias mais otimização de processamento para documento capturado. Mais correção do refazer sem documento pai

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExBL.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExBL.java
@@ -3211,9 +3211,13 @@ public class ExBL extends CpBL {
 			Set<ExVia> setVias = doc.getSetVias();
 			
 			//Libera gravação e obtém numero gerado para processar documento
-			dao().gravar(doc);
-			ContextoPersistencia.flushTransaction();
-			doc.setNumExpediente(obterNumeroGerado(doc));
+			//Não necessita da numeração e processamento para documento capturado.
+			//TODO: Trazer rotina de numeração da Trigger para Java. Dando Lock apenas na EX_DOCUMENTO_NUMERACAO. Contra: Se transação falhar abaixo o número será perdido
+			if (!doc.isCapturado()) {
+				dao().gravar(doc);
+				ContextoPersistencia.flushTransaction();
+				doc.setNumExpediente(obterNumeroGerado(doc));
+			}
 			
 			processar(doc, false, false);
 
@@ -4291,15 +4295,16 @@ public class ExBL extends CpBL {
 				obterMetodoPorString(funcao, doc);
 			}
 			
-			//Gerar movimentação REFAZER para Mobil Pai
-			final ExMovimentacao mov = criarNovaMovimentacao(
-					ExTipoMovimentacao.TIPO_MOVIMENTACAO_REFAZER,
-					cadastrante, lotaCadastrante, doc.getExMobilPai(), null, null, null,
-					null, null, null);
-			mov.setDescrMov("Documento refeito. <br /> Documento Cancelado: " + doc.getSigla() + ".<br /> Novo Documento:  " + novoDoc);
+			//Gerar movimentação REFAZER para Mobil Pai se existir
+			if (doc.getExMobilPai() != null) {
+				final ExMovimentacao mov = criarNovaMovimentacao(
+						ExTipoMovimentacao.TIPO_MOVIMENTACAO_REFAZER,
+						cadastrante, lotaCadastrante, doc.getExMobilPai(), null, null, null,
+						null, null, null);
+				mov.setDescrMov("Documento refeito. <br /> Documento Cancelado: " + doc.getSigla() + ".<br /> Novo Documento:  " + novoDoc);			
+				gravarMovimentacao(mov);
+			}
 			
-			gravarMovimentacao(mov);
-
 			concluirAlteracaoDocComRecalculoAcesso(novoDoc);
 			// atualizarWorkflow(doc, null);
 			return novoDoc;

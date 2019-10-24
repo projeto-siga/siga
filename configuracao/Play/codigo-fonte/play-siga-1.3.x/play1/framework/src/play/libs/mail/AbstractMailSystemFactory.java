9f206d41009db159e1cd31fd9e7f3294b6eb9157
Baylon Aguiar <baylon@trf2.jus.br>
2019-10-24 15:51:57 -0300

    Remoção de diretório antigo, sem utilidade.

--- configuracao/Play/codigo-fonte/play-siga-1.3.x/play1/framework/src/play/libs/mail/AbstractMailSystemFactory.java
+++
@@ -1,9 +0,0 @@
-package play.libs.mail;
-
-public abstract class AbstractMailSystemFactory {
-
-    public static final AbstractMailSystemFactory DEFAULT = new DefaultMailSystemFactory();
-
-    public abstract MailSystem currentMailSystem();
-
-}

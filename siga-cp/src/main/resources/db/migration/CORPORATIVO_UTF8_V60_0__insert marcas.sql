a866b48009746f28c6b2be042309fe70e124b6f1
Dinarde Bezerra <dinarde@gmail.com>
2019-12-10 16:10:12 -0300

    Acertos nos scripts flyway

---
+++ siga-cp/src/main/resources/db/migration/CORPORATIVO_UTF8_V60_0__insert marcas.sql
@@ -0,0 +1,3 @@
+ALTER SESSION SET CURRENT_SCHEMA=corporativo;
+insert into CORPORATIVO.CP_MARCADOR values(71, 'Pronto para Assinar', 2, 25);
+insert into CORPORATIVO.CP_MARCADOR values(72, 'Como Revisor', 2, 25);
--- siga-ex/src/main/resources/db/migration/SIGA_UTF8_V49_0__Description.sql
+++ siga-ex/src/main/resources/db/migration/SIGA_UTF8_V49_0__Description.sql
@@ -1,7 +1,2 @@
--------------------------------------------------
---	SCRIPT: Revisão de documentos
--------------------------------------------------
-
---insert into SIGA.EX_TIPO_MOVIMENTACAO (ID_TP_MOV,DESCR_TIPO_MOVIMENTACAO) values (65,'Solicitação de Assinatura');
-update SIGA.EX_TIPO_MOVIMENTACAO set DESCR_TIPO_MOVIMENTACAO = 'Solicitação de Assinatura' where ID_TP_MOV = 65;
-insert into CORPORATIVO.CP_MARCADOR values(71, 'Pronto para Assinar', 2, 25);
+ALTER SESSION SET CURRENT_SCHEMA=siga;
+insert into SIGA.EX_TIPO_MOVIMENTACAO (ID_TP_MOV,DESCR_TIPO_MOVIMENTACAO) values (65,'Solicitação de Assinatura');

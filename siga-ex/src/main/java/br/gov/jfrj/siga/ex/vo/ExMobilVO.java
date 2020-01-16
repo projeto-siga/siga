fd239a24e402e0efdab8f74a542f0dcbc941f79d
Dinarde Bezerra <dinarde@gmail.com>
2020-01-16 16:46:15 -0300

    756* - Desfazer Tramitação a partir da segunda Tramitação
    Não aparecia o botão de desfazer a tramitação a partir da segunda tramitação realizada.

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/vo/ExMobilVO.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/vo/ExMobilVO.java
@@ -539,7 +539,7 @@ public class ExMobilVO extends ExVO {
 					.getExTipoMovimentacao().getIdTpMov() != ExTipoMovimentacao.TIPO_MOVIMENTACAO_GERAR_PROTOCOLO 
 				&& ((SigaBaseProperties.getString("siga.local") != null && "GOVSP".equals(SigaBaseProperties.getString("siga.local")) && 
 					mob.getUltimaMovimentacaoNaoCancelada().getExTipoMovimentacao().getIdTpMov() == ExTipoMovimentacao.TIPO_MOVIMENTACAO_TRANSFERENCIA &&
-					mob.getDoc().getCadastrante().equals(titular)) || (mob.getUltimaMovimentacaoNaoCancelada().getExTipoMovimentacao().getIdTpMov() != ExTipoMovimentacao.TIPO_MOVIMENTACAO_TRANSFERENCIA || 
+					mob.getUltimaMovimentacaoNaoCancelada().getCadastrante().equals(titular)) || (mob.getUltimaMovimentacaoNaoCancelada().getExTipoMovimentacao().getIdTpMov() != ExTipoMovimentacao.TIPO_MOVIMENTACAO_TRANSFERENCIA || 
 						((SigaBaseProperties.getString("siga.local") != null && !"GOVSP".equals(SigaBaseProperties.getString("siga.local"))))))
 			)
 			addAcao("arrow_undo",

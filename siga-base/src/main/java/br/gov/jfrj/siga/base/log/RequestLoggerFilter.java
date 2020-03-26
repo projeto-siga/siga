4eb7b01a72d718a0e9dea5177018a8f7714174f2
Renato Crivano <renato.crivano@gmail.com>
2020-03-26 10:53:56 -0300

    RequestLoggerFilter passa a manter o ServletRequest em uma ThreadLocal para poder loggar chamadas à webservices Soap

--- siga-base/src/main/java/br/gov/jfrj/siga/base/log/RequestLoggerFilter.java
+++ siga-base/src/main/java/br/gov/jfrj/siga/base/log/RequestLoggerFilter.java
@@ -12,30 +12,37 @@ import javax.servlet.ServletResponse;
 import br.gov.jfrj.siga.base.AplicacaoException;
 
 public class RequestLoggerFilter implements Filter {
+	static ThreadLocal<ServletRequest> servletRequest = new ThreadLocal<>();
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		servletRequest.set(request);
 		long inicio = System.currentTimeMillis();
 		try {
 			chain.doFilter(request, response);
 		} catch (Exception e) {
 			if (isAplicacaoException(e))
 				throw e;
-			long duracao = System.currentTimeMillis() - inicio;
-			new RequestExceptionLogger(request, e, duracao, getLoggerName()).logar();
+			logException(request, inicio, e);
 			throw e;
 		}
 
 	}
 
-	private boolean isAplicacaoException(Throwable e) {
+	public static void logException(ServletRequest request, long inicio, Exception e) {
+		if (request == null)
+			request = servletRequest.get();
+		long duracao = System.currentTimeMillis() - inicio;
+		new RequestExceptionLogger(request, e, duracao, getLoggerName()).logar();
+	}
+
+	public static boolean isAplicacaoException(Throwable e) {
 		while (e != null) {
 			if (e instanceof AplicacaoException)
 				return true;
@@ -46,7 +53,7 @@ public class RequestLoggerFilter implements Filter {
 		return false;
 	}
 
-	protected String getLoggerName() {
+	public static String getLoggerName() {
 		return "br.gov.jfrj.siga.request.log";
 	}
 

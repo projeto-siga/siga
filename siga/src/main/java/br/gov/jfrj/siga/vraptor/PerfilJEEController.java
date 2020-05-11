53bcbec15804a6cbcbc5c2b630ce2d3c4eca935c
Renato Crivano <renato.crivano@gmail.com>
2020-05-11 13:45:46 -0300

    Acrescentando marca de @Transacional no PerfilJEE controller

--- siga/src/main/java/br/gov/jfrj/siga/vraptor/PerfilJEEController.java
+++ siga/src/main/java/br/gov/jfrj/siga/vraptor/PerfilJEEController.java
@@ -120,6 +120,7 @@ public class PerfilJEEController extends GrupoController {
 		result.redirectTo(MessageFormat.format("/app/gi/perfilJEE/editar?idCpGrupo={0}", novoIdCpGrupo.toString()));
 	}	
 
+	@Transacional
 	@Post("/app/gi/perfilJEE/excluir")
 	public void excluir(Long idCpGrupo) throws Exception {
 		assertAcesso("PERFILJEE:Gerenciar grupos de email");

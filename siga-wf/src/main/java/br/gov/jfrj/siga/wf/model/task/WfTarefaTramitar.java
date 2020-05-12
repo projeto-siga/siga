1e3ac2da3a09cde49e20f1c20730ece0eaf1462b
Renato Crivano <renato.crivano@gmail.com>
2020-05-12 13:18:50 -0300

    WF: Corrigindo tarefa de tramitar

--- siga-wf/src/main/java/br/gov/jfrj/siga/wf/model/task/WfTarefaTramitar.java
+++ siga-wf/src/main/java/br/gov/jfrj/siga/wf/model/task/WfTarefaTramitar.java
@@ -10,15 +10,17 @@ import br.gov.jfrj.siga.base.util.Utils;
 import br.gov.jfrj.siga.parser.SiglaParser;
 import br.gov.jfrj.siga.wf.model.WfDefinicaoDeTarefa;
 import br.gov.jfrj.siga.wf.model.WfProcedimento;
+import br.gov.jfrj.siga.wf.util.WfResp;
 
 public class WfTarefaTramitar implements Task<WfDefinicaoDeTarefa, WfProcedimento> {
 
 	@Override
 	public TaskResult execute(WfDefinicaoDeTarefa td, WfProcedimento pi, Engine engine) throws Exception {
-		if (Utils.empty(pi.getPrincipal()))
-			Service.getExService().transferir(pi.getPrincipal(),
-					SiglaParser.makeSigla(pi.getEventoPessoa(), pi.getEventoLotacao()),
-					SiglaParser.makeSigla(pi.getEventoPessoa(), pi.getEventoLotacao()), false);
+		if (!Utils.empty(pi.getPrincipal())) {
+			WfResp destino = pi.calcResponsible(td);
+			String siglaDestino = SiglaParser.makeSigla(destino.getPessoa(), destino.getLotacao());
+			Service.getExService().transferir(pi.getPrincipal(), siglaDestino, null, true);
+		}
 		return new TaskResult(TaskResultKind.DONE, null, null, null, null);
 	}
 

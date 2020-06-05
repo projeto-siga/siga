03b205d065073cabf3b94b64ef35376b62b6c057
Newton <ny2345@gmail.com>
2020-06-05 12:56:18 -0300

    1214 - Removendo relacionamento do documento anterior

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExBL.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExBL.java
@@ -4244,7 +4244,7 @@ public class ExBL extends CpBL {
 			novoDoc.setLotaTitular(doc.getLotaTitular().getLotacaoAtual());
 
 		novoDoc.setNumAntigoDoc(doc.getNumAntigoDoc());
-		novoDoc.setExDocAnterior(doc);
+		novoDoc.setIdDocAnterior(doc.getIdDoc());
 		// novoDoc.setNumPaginas(novoDoc.getContarNumeroDePaginas());
 
 		ExMobil mob = new ExMobil();
@@ -6841,7 +6841,7 @@ public class ExBL extends CpBL {
 		// doc.setDestinatario(null);
 		doc.setExBoletimDocSet(null);
 		doc.setExClassificacao(null);
-		doc.setExDocAnterior(null);
+		doc.setIdDocAnterior(null);
 		doc.setExFormaDocumento(null);
 		doc.setExMobilAutuado(null);
 		doc.setExMobilPai(null);

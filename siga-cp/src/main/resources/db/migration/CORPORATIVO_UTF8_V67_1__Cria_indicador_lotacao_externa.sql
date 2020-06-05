1a9ddf88b617dea9749e46aaae4728311ff68b31
Baylon Aguiar <baylon@trf2.jus.br>
2020-06-05 17:59:44 -0300

    Excluindo comando repetido, erro causado pela numeracao diferente entre branches.

--- siga-cp/src/main/resources/db/migration/CORPORATIVO_UTF8_V67_1__Cria_indicador_lotacao_externa.sql
+++
@@ -1,4 +0,0 @@
----------------------------------------------------------------------------    
---  Coluna de number 0 ou 1 para informar se orgão é externo 
----------------------------------------------------------------------------
-ALTER TABLE CORPORATIVO.DP_LOTACAO ADD IS_EXTERNA_LOTACAO NUMBER(1,0) DEFAULT 0;

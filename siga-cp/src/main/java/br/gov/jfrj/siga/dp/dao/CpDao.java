f12fd5777793e4749f48aeac13e501b5cb6475dd
daniarmando <daniel.armando@live.com>
2020-07-03 09:30:23 -0300

    GOVSP1310#Problema resetar senha: não comparar histórico auditável caso for instância CpIdentidade

--- siga-cp/src/main/java/br/gov/jfrj/siga/dp/dao/CpDao.java
+++ siga-cp/src/main/java/br/gov/jfrj/siga/dp/dao/CpDao.java
@@ -1924,7 +1924,7 @@ public class CpDao extends ModeloDao {
 			return gravarComHistorico(oNovo, identidadeCadastrante);
 		}
 
-		if (oNovo.semelhante(oAntigo, 0)) {
+		if (!(oNovo instanceof CpIdentidade) && (oNovo.semelhante(oAntigo, 0))) {
 			return oAntigo;
 		}
 

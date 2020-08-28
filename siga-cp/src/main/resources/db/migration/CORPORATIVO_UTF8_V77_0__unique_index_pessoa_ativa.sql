65cf9eba597c45cef46b2f62b310208b8b20b777
Dinarde Bezerra <dinarde@gmail.com>
2020-08-27 23:04:05 -0300

    Desativado CPV77 pois precisa tratar os dados antes

--- siga-cp/src/main/resources/db/migration/CORPORATIVO_UTF8_V77_0__unique_index_pessoa_ativa.sql
+++ siga-cp/src/main/resources/db/migration/CORPORATIVO_UTF8_V77_0__unique_index_pessoa_ativa.sql
@@ -1,4 +1,5 @@
 --------------------------------------------------------------------------------------------------
 --	SCRIPT: PERMITE APENAS UMA PESSOA ATIVA POR ID_PESSOA_INICIAL
 --------------------------------------------------------------------------------------------------
-CREATE UNIQUE INDEX corporativo.dp_pessoa_unique_pessoa_ativa on CORPORATIVO.DP_PESSOA (CASE WHEN DATA_FIM_PESSOA IS NULL THEN ID_PESSOA_INICIAL END);
+--Desativado pois precisa tratar os dados antes
+--CREATE UNIQUE INDEX corporativo.dp_pessoa_unique_pessoa_ativa on CORPORATIVO.DP_PESSOA (CASE WHEN DATA_FIM_PESSOA IS NULL THEN ID_PESSOA_INICIAL END);

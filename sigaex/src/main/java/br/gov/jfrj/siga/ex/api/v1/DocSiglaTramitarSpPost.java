ea48c05c5e7929080f3a0507a2a4f39a0a5b1512
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-09-18 14:54:03 -0300

    Refatoração da tramitação.
    Os Enums TramitacaoTipoDestinoEnum realizam a tramitação propriamente
    dita com os parâmetros devidamente preenchidos de acordo com o tipo de
    destino.

--- sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/DocSiglaTramitarSpPost.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/DocSiglaTramitarSpPost.java
@@ -4,15 +4,11 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeParseException;
 import java.util.Date;
-import java.util.Objects;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.crivano.swaggerservlet.SwaggerException;
 
-import br.gov.jfrj.siga.dp.CpOrgao;
-import br.gov.jfrj.siga.dp.DpLotacao;
-import br.gov.jfrj.siga.dp.DpPessoa;
 import br.gov.jfrj.siga.ex.ExMobil;
 import br.gov.jfrj.siga.ex.api.v1.IExApiV1.DocSiglaTramitarSpPostRequest;
 import br.gov.jfrj.siga.ex.api.v1.IExApiV1.DocSiglaTramitarSpPostResponse;
@@ -22,109 +18,6 @@ import br.gov.jfrj.siga.hibernate.ExDao;
 import br.gov.jfrj.siga.vraptor.SigaObjects;
 
 public class DocSiglaTramitarSpPost implements IDocSiglaTramitarSpPost {
-
-	void efetuarTramitacaoParaLotacao(SigaObjects so, ExMobil mob, Date dtDevolucao, Date dt,
-			DocSiglaTramitarSpPostRequest req, DocSiglaTramitarSpPostResponse resp) throws SwaggerException {
-		DpLotacao lot = new DpLotacao();
-		lot.setSigla(req.destinatario.replace("-", ""));
-		lot = ExDao.getInstance().consultarPorSigla(lot);
-		if (Objects.isNull(lot)) {
-			throw new SwaggerException("Não foi encontrado Órgão com sigla " + req.destinatario, 404, null, req, resp,
-					null);
-		}
-		Ex.getInstance().getBL().transferir(//
-				null, // CpOrgao orgaoExterno
-				null, // String obsOrgao
-				so.getCadastrante(), // DpPessoa cadastrante
-				so.getCadastrante().getLotacao(), // DpLotacao lotaCadastrante
-				mob, // ExMobil mob
-				dt, // final Date dtMov
-				dt, // Date dtMovIni
-				dtDevolucao, // Date dtFimMov
-				lot, // DpLotacao lotaResponsavel
-				null, // final DpPessoa responsavel
-				null, // DpLotacao lotaDestinoFinal
-				null, // DpPessoa destinoFinal
-				null, // DpPessoa subscritor
-				so.getCadastrante(), // DpPessoa titular
-				null, // ExTipoDespacho tpDespacho. Que tipo de despacho?
-				true, // final boolean fInterno: QUANDO USO ISSO?
-				null, // String descrMov
-				null, // String conteudo
-				null, // String nmFuncaoSubscritor
-				false, // boolean forcarTransferencia
-				false // boolean automatico
-		);
-
-	}
-
-	void efetuarTramitacaoParaUsuario(SigaObjects so, ExMobil mob, Date dtDevolucao, Date dt,
-			DocSiglaTramitarSpPostRequest req, DocSiglaTramitarSpPostResponse resp) throws SwaggerException {
-		DpPessoa usuarioDestino = new DpPessoa();
-		usuarioDestino.setSigla(req.destinatario);
-		usuarioDestino = ExDao.getInstance().consultarPorSigla(usuarioDestino);
-		if (Objects.isNull(usuarioDestino)) {
-			throw new SwaggerException("Não foi encontrado Usuário com a Matrícula " + req.destinatario, 404, null, req,
-					resp, null);
-		}
-
-		Ex.getInstance().getBL().transferir(//
-				null, // CpOrgao orgaoExterno
-				null, // String obsOrgao
-				so.getCadastrante(), // DpPessoa cadastrante
-				so.getCadastrante().getLotacao(), // DpLotacao lotaCadastrante
-				mob, // ExMobil mob
-				dt, // final Date dtMov
-				dt, // Date dtMovIni
-				dtDevolucao, // Date dtFimMov
-				usuarioDestino.getLotacao(), // DpLotacao lotaResponsavel
-				usuarioDestino, // final DpPessoa responsavel
-				null, // DpLotacao lotaDestinoFinal
-				null, // DpPessoa destinoFinal
-				null, // DpPessoa subscritor
-				so.getCadastrante(), // DpPessoa titular
-				null, // ExTipoDespacho tpDespacho. Que tipo de despacho?
-				true, // final boolean fInterno: QUANDO USO ISSO?
-				null, // String descrMov
-				null, // String conteudo
-				null, // String nmFuncaoSubscritor
-				false, // boolean forcarTransferencia
-				false // boolean automatico
-		);
-	}
-
-	void efetuarTramitacaoParaOrgaoExtrno(SigaObjects so, ExMobil mob, Date dtDevolucao, Date dt,
-			DocSiglaTramitarSpPostRequest req, DocSiglaTramitarSpPostResponse resp) throws SwaggerException {
-		CpOrgao orgaoExternoDestino = ExDao.getInstance().getOrgaoFromSiglaExata(req.destinatario);
-		if (Objects.isNull(orgaoExternoDestino)) {
-			throw new SwaggerException("Não foi encontrado Órgão Externo com o código " + req.destinatario, 404, null,
-					req, resp, null);
-		}
-
-		Ex.getInstance().getBL().transferir(//
-				orgaoExternoDestino, // CpOrgao orgaoExterno
-				req.observacao, // String obsOrgao
-				so.getCadastrante(), // DpPessoa cadastrante
-				so.getCadastrante().getLotacao(), // DpLotacao lotaCadastrante
-				mob, // ExMobil mob
-				dt, // final Date dtMov
-				dt, // Date dtMovIni
-				dtDevolucao, // Date dtFimMov
-				null, // DpLotacao lotaResponsavel
-				null, // final DpPessoa responsavel
-				null, // DpLotacao lotaDestinoFinal
-				null, // DpPessoa destinoFinal
-				null, // DpPessoa subscritor
-				so.getCadastrante(), // DpPessoa titular
-				null, // ExTipoDespacho tpDespacho. Que tipo de despacho?
-				true, // final boolean fInterno: QUANDO USO ISSO?
-				null, // String descrMov
-				null, // String conteudo
-				null, // String nmFuncaoSubscritor
-				false, // boolean forcarTransferencia
-				false // boolean automatico
-		);
-	}
 
 	@Override
 	public String getContext() {
@@ -192,19 +85,7 @@ public class DocSiglaTramitarSpPost implements IDocSiglaTramitarSpPost {
 
 			Date dt = ExDao.getInstance().consultarDataEHoraDoServidor();
 
-			switch (tipoDestino) {
-			case ORGAO:
-				this.efetuarTramitacaoParaLotacao(so, mob, dataDevolucao, dt, req, resp);
-				break;
-			case USUARIO:
-				this.efetuarTramitacaoParaUsuario(so, mob, dataDevolucao, dt, req, resp);
-				break;
-			case EXTERNO:
-				this.efetuarTramitacaoParaOrgaoExtrno(so, mob, dataDevolucao, dt, req, resp);
-				break;
-			}
-
-			return;
+			tipoDestino.efetuarTramitacaoParaDestino(so, mob, dataDevolucao, dt, req, resp);
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
 			throw e;

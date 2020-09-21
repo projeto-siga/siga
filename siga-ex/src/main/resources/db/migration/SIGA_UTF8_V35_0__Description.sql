0f299a2238ffd76588002f9e394e39d075f8b1c2
Renato Crivano <renato.crivano@gmail.com>
2020-09-21 13:06:42 -0300

    Corrigindo SQLs de migração para que o Flyway voltasse a funcionar

--- siga-ex/src/main/resources/db/migration/SIGA_UTF8_V35_0__Description.sql
+++ siga-ex/src/main/resources/db/migration/SIGA_UTF8_V35_0__Description.sql
@@ -1,7 +1,7 @@
 --SCRIPT DESATIVADO.
 --MOTIVO: EXECUTA MESMA ROTINA DO CORPORATIVO_UTF8_V33_0__Description.sql
---insert into corporativo.cp_marcador values(1000, 'Urgente', 2);
+--insert into corporativo.cp_marcador(id_marcador, descr_marcador, ord_marcador) values(1000, 'Urgente', 2);
 
---insert into corporativo.cp_marcador values(1001, 'Idoso', 2);
+--insert into corporativo.cp_marcador(id_marcador, descr_marcador, ord_marcador) values(1001, 'Idoso', 2);
 
---insert into corporativo.cp_marcador values(1002, 'Retenção de INSS', 2);
+--insert into corporativo.cp_marcador(id_marcador, descr_marcador, ord_marcador) values(1002, 'Retenção de INSS', 2);

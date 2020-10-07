776885a9f5e214152035d8ec1ee541f9a97a8b30
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-10-07 10:27:30 -0300

    Refatoração e simplificação de DocumentoSiglaArquivoGet.

--- sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/DocumentoSiglaArquivoGet.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/DocumentoSiglaArquivoGet.java
@@ -5,58 +5,31 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.crivano.swaggerservlet.PresentableUnloggedException;
-import com.crivano.swaggerservlet.SwaggerAuthorizationException;
 import com.crivano.swaggerservlet.SwaggerServlet;
 
 import br.gov.jfrj.itextpdf.Status;
-import br.gov.jfrj.siga.base.AplicacaoException;
 import br.gov.jfrj.siga.ex.ExMobil;
 import br.gov.jfrj.siga.ex.api.v1.IExApiV1.DocumentoSiglaArquivoGetRequest;
 import br.gov.jfrj.siga.ex.api.v1.IExApiV1.DocumentoSiglaArquivoGetResponse;
 import br.gov.jfrj.siga.ex.api.v1.IExApiV1.IDocumentoSiglaArquivoGet;
-import br.gov.jfrj.siga.ex.bl.Ex;
-import br.gov.jfrj.siga.hibernate.ExDao;
-import br.gov.jfrj.siga.model.ContextoPersistencia;
-import br.gov.jfrj.siga.persistencia.ExMobilDaoFiltro;
-import br.gov.jfrj.siga.vraptor.SigaObjects;
 
 @AcessoPublicoEPrivado
 public class DocumentoSiglaArquivoGet implements IDocumentoSiglaArquivoGet {
 
 	@Override
 	public void run(DocumentoSiglaArquivoGetRequest req, DocumentoSiglaArquivoGetResponse resp) throws Exception {
-		try {
-			String usuario = ContextoPersistencia.getUserPrincipal();
+		String usuario = SwaggerHelper.buscarEValidarUsuarioLogado();
 
-			if (usuario == null)
-				throw new SwaggerAuthorizationException("Usuário não está logado");
+		ExMobil mob = SwaggerHelper.buscarEValidarMobil(req.sigla, req, resp);
+		String filename = "text/html".equals(req.contenttype)
+				? (req.volumes ? mob.doc().getReferenciaPDF() : mob.getReferenciaPDF())
+				: (req.volumes ? mob.doc().getReferenciaHtml() : mob.getReferenciaHtml());
 
-			final ExMobilDaoFiltro filter = new ExMobilDaoFiltro();
-			filter.setSigla(req.sigla);
-			ExMobil mob = (ExMobil) ExDao.getInstance().consultarPorSigla(filter);
-			if (mob == null)
-				throw new PresentableUnloggedException(
-						"Não foi possível encontrar um documento a partir da sigla fornecida");
+		HttpServletRequest request = SwaggerServlet.getHttpServletRequest();
+		final String contextpath = request.getContextPath();
+		final String servernameport = request.getServerName() + ":" + request.getServerPort();
 
-			HttpServletRequest request = SwaggerServlet.getHttpServletRequest();
-			SigaObjects so = new SigaObjects(request);
-			if (!Ex.getInstance().getComp().podeAcessarDocumento(so.getTitular(), so.getLotaTitular(), mob))
-				throw new AplicacaoException(
-						"Acesso ao documento " + mob.getSigla() + " permitido somente a usuários autorizados. ("
-								+ so.getTitular().getSigla() + "/" + so.getLotaTitular().getSiglaCompleta() + ")");
-
-			String filename = "text/html".equals(req.contenttype)
-					? (req.volumes ? mob.doc().getReferenciaPDF() : mob.getReferenciaPDF())
-					: (req.volumes ? mob.doc().getReferenciaHtml() : mob.getReferenciaHtml());
-			final String servernameport = request.getServerName() + ":" + request.getServerPort();
-			final String contextpath = request.getContextPath();
-
-			iniciarGeracaoDePdf(req, resp, usuario, filename, contextpath, servernameport);
-		}catch (Exception e) {
-			e.printStackTrace(System.out);
-			throw e;
-		}
+		iniciarGeracaoDePdf(req, resp, usuario, filename, contextpath, servernameport);
 	}
 
 	public static void iniciarGeracaoDePdf(DocumentoSiglaArquivoGetRequest req, DocumentoSiglaArquivoGetResponse resp,

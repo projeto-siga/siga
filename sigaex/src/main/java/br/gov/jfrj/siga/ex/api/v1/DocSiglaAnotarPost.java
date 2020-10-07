64e5ec466011cde508d5f002f497be599cd58492
Rafael Afonso <rafonso@magnasistemas.com.br>
2020-10-07 06:18:04 -0300

    Anotação: Uso efetivo do texto da anotação.
    O texto da anotação não estava sendo de fato passado ao servidor. Além
    disso a classe responsável foi refatorada.

--- sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/DocSiglaAnotarPost.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/DocSiglaAnotarPost.java
@@ -11,45 +11,32 @@ import br.gov.jfrj.siga.ex.api.v1.IExApiV1.IDocSiglaAnotarPost;
 import br.gov.jfrj.siga.ex.bl.CurrentRequest;
 import br.gov.jfrj.siga.ex.bl.Ex;
 import br.gov.jfrj.siga.ex.bl.RequestInfo;
-import br.gov.jfrj.siga.hibernate.ExDao;
-import br.gov.jfrj.siga.persistencia.ExMobilDaoFiltro;
 import br.gov.jfrj.siga.vraptor.SigaObjects;
 
 public class DocSiglaAnotarPost implements IDocSiglaAnotarPost {
 
 	@Override
-	public void run(DocSiglaAnotarPostRequest req,
-			DocSiglaAnotarPostResponse resp) throws Exception {
-		
+	public void run(DocSiglaAnotarPostRequest req, DocSiglaAnotarPostResponse resp) throws Exception {
+		CurrentRequest.set(
+				new RequestInfo(null, SwaggerServlet.getHttpServletRequest(), SwaggerServlet.getHttpServletResponse()));
 		SwaggerHelper.buscarEValidarUsuarioLogado();
-		CurrentRequest.set(new RequestInfo(null, SwaggerServlet.getHttpServletRequest(), SwaggerServlet.getHttpServletResponse()));
-		
+
 		SigaObjects so = SwaggerHelper.getSigaObjects();
-		so.assertAcesso("DOC:Módulo de Documentos;" + "");
 
-		try {
-			DpPessoa cadastrante = so.getCadastrante();
-			DpLotacao lotaCadastrante = cadastrante.getLotacao();
-			DpPessoa titular = cadastrante;
-			DpLotacao lotaTitular = cadastrante.getLotacao();
+		DpPessoa cadastrante = so.getCadastrante();
+		DpLotacao lotaCadastrante = cadastrante.getLotacao();
+		DpPessoa titular = cadastrante;
+		DpLotacao lotaTitular = cadastrante.getLotacao();
 
-			ExMobilDaoFiltro flt = new ExMobilDaoFiltro();
-			flt.setSigla(req.sigla);
-			ExMobil mob = ExDao.getInstance().consultarPorSigla(flt);
+		ExMobil mob = SwaggerHelper.buscarEValidarMobil(req.sigla, so, req, resp, "Documento a Anotar");
 
-			Utils.assertAcesso(mob, titular, lotaTitular);
+		Utils.assertAcesso(mob, titular, lotaTitular);
 
-			Ex.getInstance()
-					.getBL()
-					.anotar(cadastrante, lotaCadastrante, mob, null, null,
-							null, null, cadastrante, req.anotacao, null);
-			resp.status = "OK";
-		} catch (Exception e) {
-			e.printStackTrace(System.out);
-			throw e;
-		}
+		Ex.getInstance().getBL().anotar(cadastrante, lotaCadastrante, mob, null, null, null, null, cadastrante,
+				req.anotacao, null);
+
+		resp.status = "OK";
 	}
-
 
 	@Override
 	public String getContext() {

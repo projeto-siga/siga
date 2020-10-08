0aecbff3b1080248348fdf8fced0ed80703f044f
Renato Crivano <renato.crivano@gmail.com>
2020-10-08 14:39:57 -0300

    Integrando o Siga-Le no repositório "siga" e adaptando para usar a API REST do sigaex.war

---
+++ siga-le/src/main/frontend/src/components/MyInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label v-if="label" :for="name">{{label}}</label>
+    <div v-if="!edit">{{value}}</div>
+    <input type="text" v-if="edit" :id="name" class="form-control" 
+      :placeholder="placeholder" v-mask="mask != undefined ? mask : ''"
+      :disabled="disabled"
+      v-bind:value="value" v-on:input="$emit('input', $event.target.value)" v-on:change="$emit('change', $event.target.value)" 
+      :name="name" :class="{ 'is-invalid': error }">
+    <div v-if="error" class="invalid-feedback">{{error}}</div>
+  </div>
+</template>
+
+<script>
+import AwesomeMask from 'awesome-mask'
+
+export default {
+  name: 'my-input',
+  props: ['value', 'label', 'name', 'placeholder', 'mask', 'edit', 'error', 'disabled'],
+  data() {
+    return {}
+  },
+  directives: {
+    mask: AwesomeMask
+  }
+}
+</script>
---
+++ siga-le/src/main/frontend/src/components/Sobre.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container content pt-5">
+    <div class="row">
+      <div class="col-md-12 sobre">
+        <div class="headline pb-1">
+          <h2>Por que Siga-Le?</h2>
+        </div>
+  
+        <p>O Siga-Le é uma versão mais leve do Siga-Doc, criada especialmente para o uso em celulares e outros dispositivos móveis.</p>
+  
+        <div class="headline pb-1 pt-3">
+          <h2>Como faço para usar?</h2>
+        </div>
+  
+        <p>O Siga-Le está disponível para todas as pessoas que já tem um usuário ativo no Siga-Doc, basta utilizar a mesma senha de acesso. Lembre-se apenas que o Siga-Le não possui todas as funcionalidades do Siga-Doc, apenas o suficiente para atender as necessidades do usuário móvel.</p>
+        <p>
+          Atenção, assim como o Siga-Doc, o Siga-Le mantém o usuário autenticado por 18h após o
+          <i>login</i>. Fechar o navegador não causa automaticamente um
+          <i>logout</i>. Para forçar a desconexão do usuário, é necessário clicar na opção "Logout" do menu principal.
+        </p>
+  
+        <div class="headline pb-1 pt-3">
+          <h2>Sobre o Siga-Le</h2>
+        </div>
+  
+        <p>O Siga-Le foi desenvolvido em 2018 pela Coordenadoria de Sistemas Administrativos (COSADM), parte da Secretaria de Tecnologia da Informação do Tribunal Regional Federal da 2ª Região.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sobre'
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+</style>

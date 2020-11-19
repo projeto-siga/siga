6d283f01f3142756612d8a345fb7ad48b0c4188b
Joao Luis <joaoluis@trf2.jus.br>
2020-11-19 18:53:09 -0300

    TP:EAP 7.2 Inclusão filtro para inicializar entityManager pela primeira vez evitando a mensagem EM Nulo !

---
+++ sigatp/src/main/java/br/gov/jfrj/siga/tp/util/TpThreadFilter.java
@@ -0,0 +1,66 @@
+package br.gov.jfrj.siga.tp.util;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import br.gov.jfrj.siga.dp.dao.CpDao;
+import br.gov.jfrj.siga.model.ContextoPersistencia;
+
+public class TpThreadFilter implements Filter {
+	
+	private EntityManager em;
+	
+	/**
+	 * @deprecated CDI eyes only
+	 */
+	public TpThreadFilter() {
+		super();
+	}
+	
+	@Inject
+	public TpThreadFilter(EntityManager em)  {
+		this.em = em;
+	}
+
+	public void doFilter(final ServletRequest request,
+			final ServletResponse response, final FilterChain chain)
+			throws IOException, ServletException {
+		try {
+			ContextoPersistencia.setEntityManager(em);
+			CpDao.freeInstance();
+			CpDao.getInstance();
+			chain.doFilter(request, response);
+		} catch (Exception e) {
+
+			throw new ServletException(e);
+		} finally {
+			CpDao.freeInstance();
+			ContextoPersistencia.setEntityManager(null);
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	protected String getLoggerName() {
+		return "br.gov.jfrj.siga.sr";
+	}
+}

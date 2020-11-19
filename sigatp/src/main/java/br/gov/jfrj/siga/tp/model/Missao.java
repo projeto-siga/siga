49b7755b5276d8a275b64204865b4357765494a2
Joao Luis <joaoluis@trf2.jus.br>
2020-11-19 20:06:17 -0300

    TP:MYSQL Correção de mapeamento do nome da tabela errado

--- sigatp/src/main/java/br/gov/jfrj/siga/tp/model/Missao.java
+++ sigatp/src/main/java/br/gov/jfrj/siga/tp/model/Missao.java
@@ -43,7 +43,7 @@ import br.gov.jfrj.siga.uteis.SiglaDocumentoType;
 
 @Entity
 @Audited
-@Table(name = "nissao", schema = "sigatp")
+@Table(name = "missao", schema = "sigatp")
 public class Missao extends TpModel implements ConvertableEntity, Comparable<Missao>, SequenceMethods {
 
 	/**

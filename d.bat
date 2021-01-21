5b8aafd78ab68bde2ab20c07b61c649908d927ce
Andre Carvalho <darlansc@yahoo.com.br>
2021-01-21 14:09:30 -0300

    layout

--- d.bat
+++ d.bat
@@ -1,5 +1,7 @@
-del C:\usr\Teletrabalho\ProcessoRio\jboss\opt\java\jboss-eap-7.2\standalone\deployments\siga*.*
+call mvn clean
+call mvn -pl "!siga-dump,!siga-ex-gsa,!siga-ex-sinc,!siga-gc-gsa,!sigagc,!sigasr,!sigapp,!siga-ldap-cli,!sigatp,!siga-wf,!sigawf" clean install -DskipTests=true
+del C:\iplanrio\jboss-eap-7.2\standalone\deployments\siga*.*
 cd target
-copy *.* c:\usr\Teletrabalho\ProcessoRio\jboss\opt\java\jboss-eap-7.2\standalone\deployments\
+copy *.* C:\iplanrio\jboss-eap-7.2\standalone\deployments\
 cd ..
 cls
--- sigaex/src/main/java/br/gov/jfrj/siga/ex/relatorio/dinamico/relatorios/RelTeste.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/relatorio/dinamico/relatorios/RelTeste.java
@@ -60,141 +60,43 @@ public class RelTeste extends RelatorioTemplate {
 		this.setTitle("Relação Modelos - Teste ");
 
 		this.addColuna("Número", 18, RelatorioRapido.ESQUERDA, false); //NUM. PROCESSO
-		this.addColuna("Dt.Despacho", 10, RelatorioRapido.ESQUERDA, false); //DATA DESPACHO
-		this.addColuna("Dt.Recebimento", 10, RelatorioRapido.ESQUERDA, false);   //DATA RECEBIMENTO
-		this.addColuna("Dt.Saída", 10, RelatorioRapido.ESQUERDA, false);   //,DATA SAÍDA,
+		this.addColuna("Dt.Movimentação", 15, RelatorioRapido.ESQUERDA, false); //DATA DESPACHO
+//		this.addColuna("Dt.Recebimento", 10, RelatorioRapido.ESQUERDA, false);   //DATA RECEBIMENTO
+//		this.addColuna("Dt.Saída", 10, RelatorioRapido.ESQUERDA, false);   //,DATA SAÍDA,
 		this.addColuna("Despacho", 30, RelatorioRapido.ESQUERDA, false);   //COD. DESPACHO +DESCR. DESPACHO 
-		this.addColuna("Assunto", 30, RelatorioRapido.ESQUERDA, false);   //COD. ASSUNTO+ DESCR. ASSUNTO,
-		this.addColuna("Órgão Origem", 30, RelatorioRapido.ESQUERDA, false);   //ORG. ORIGEM + DESCR. ORG. ORIGEM
-		this.addColuna("Órgão Destino", 30, RelatorioRapido.ESQUERDA, false);   //ORG. DESTINO + DESCR. ORG. DESTINO
-		this.addColuna("Matr.Digitador", 10, RelatorioRapido.ESQUERDA, false);   //MATR. DIGITADOR
+		this.addColuna("Assunto", 30, RelatorioRapido.ESQUERDA, true);   //COD. ASSUNTO+ DESCR. ASSUNTO,
+		this.addColuna("Órgão Origem", 40, RelatorioRapido.ESQUERDA, false);   //ORG. ORIGEM + DESCR. ORG. ORIGEM
+		this.addColuna("Órgão Destino", 40, RelatorioRapido.ESQUERDA, true);   //ORG. DESTINO + DESCR. ORG. DESTINO
+		this.addColuna("Matr.Digitador", 15, RelatorioRapido.ESQUERDA, false);   //MATR. DIGITADOR
 		
 		return this;
 	}
 
-	/*@Override
-	public Collection processarDados() throws Exception {
- 		
-		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-
-		List<String> d = new ArrayList<String>();
-
-		List<String> listaFinal = new ArrayList<String>();
-
-		String sql =" select d from ExDocumento d join d.exMobilSet as mob join mob.exMovimentacaoSet as m";
-		Query query = ContextoPersistencia.em().createQuery( sql );
-		
-
-		List<ExDocumento> lista = query.getResultList();
-		
-		
-		for (ExDocumento doc : lista) {
-			
-			
-			String numeroProcesso = doc.getCodigo();
-			
-			String codAssunto = doc.getExClassificacao().getCodificacao();
-			String descAssunto = doc.getExClassificacao().getDescrClassificacao();
-			
-			String codOrgDestino ="";
-			String descOrgaoDestino ="";
-			
-			if (doc.getLotaDestinatario() !=  null) {
-				codOrgDestino =  String.valueOf( doc.getLotaDestinatario().getIdLotacao() );
-				descOrgaoDestino = doc.getLotaDestinatario().getNomeLotacao();
-			}
-			
-			for (ExMovimentacao m : doc.getExMovimentacaoSet()){
-				
-				String dataDespacho = formatter.format(m.getDtTimestamp());
-				String dataRecebimento = formatter.format(m.getDtTimestamp());
-				String dataSaida = formatter.format(m.getDtTimestamp());
-				
-				String codDespacho = String.valueOf( m.getExTipoMovimentacao().getIdTpMov());
-				String descDespacho =m.getExTipoMovimentacao().getDescricao();
-				
-				
-				String codOrgaoOrigem  ="";
-				String descOrgaoOrigem ="";
-				//TODO verificar com Ivan o Origem 
-			if (m.getLotacao() != null ){
-				 codOrgaoOrigem  = String.valueOf(m.getLotacao().getId() );
-				 descOrgaoOrigem = m.getLotacao().getDescricao();
-			}
-				String digitador = m.getCadastrante()!= null ? String.valueOf( m.getCadastrante().getMatricula() ) :"";
-			
-				
-				//NUM. PROCESSO,DATA DESPACHO,DATA RECEBIMENTO,DATA SAÍDA,COD. DESPACHO,DESCR. DESPACHO,
-				//COD. ASSUNTO,DESCR. ASSUNTO,ORG. ORIGEM,DESCR. ORG. ORIGEM,ORG. DESTINO,DESCR. ORG. DESTINO,MATR. DIGITADOR
-				
-				listaFinal.add( numeroProcesso);
-				listaFinal.add(dataDespacho);
-				listaFinal.add(dataRecebimento);
-				listaFinal.add(dataSaida);
-				listaFinal.add(codDespacho);
-				listaFinal.add(descDespacho);
-				listaFinal.add(codAssunto);
-				listaFinal.add(descAssunto);
-				listaFinal.add(codOrgaoOrigem);
-				listaFinal.add(descOrgaoOrigem);
-				listaFinal.add(codOrgDestino);
-				listaFinal.add(descOrgaoDestino);
-				listaFinal.add(digitador);
-			}
-		}
-			
-		
-		return listaFinal;
-	}
-*/
-	
-	public String montarQueryGeraldo(){
+	public String montarConsulta(){
 		
 		String sqlDescricaoOrgao ="";
 		
 		String sqlOrgaoOrigem =	
-				"SELECT  l.id_lotacao  "+
-				"FROM corporativo.dp_lotacao l2 "+
-				"WHERE l2.id_lotacao = ("+
-				"	SELECT  m3.id_lota_resp "+
-				"   FROM siga.ex_movimentacao m3  "+
-				" 	WHERE m3.id_mov = ("+
-				"		SELECT  max (m2.id_mov)   "+
-				" 		FROM siga.ex_movimentacao m2   "+
-				" 			INNER JOIN siga.ex_mobil mb2 on (mb2.id_mobil = m2.id_mobil)  "+
+				"SELECT  l.id_lotacao FROM corporativo.dp_lotacao l2  WHERE l2.id_lotacao = ("+
+				"	SELECT  m3.id_lota_resp  FROM siga.ex_movimentacao m3   WHERE m3.id_mov = ("+
+				"		SELECT  max (m2.id_mov) FROM siga.ex_movimentacao m2  INNER JOIN siga.ex_mobil mb2 on (mb2.id_mobil = m2.id_mobil)  "+
 				" 		WHERE mb2.id_doc = d.id_doc  AND m2.id_mov < m.id_mov )	)	)";
 		
 		String sqlOrgaoOrigemDescricao =
-				"SELECT  l.nome_lotacao "+
-				"FROM corporativo.dp_lotacao l2  "+
-				"WHERE l2.id_lotacao = ("+
-				"	SELECT  m3.id_lota_resp "+
-				"	FROM siga.ex_movimentacao m3   "+
-				" 	WHERE m3.id_mov =("+
-				"		SELECT  max (m2.id_mov) 	"+
-				"		FROM siga.ex_movimentacao m2  "+
-				" 			INNER JOIN siga.ex_mobil mb2 on (mb2.id_mobil = m2.id_mobil)  "+
+				"SELECT  l.nome_lotacao  FROM corporativo.dp_lotacao l2  WHERE l2.id_lotacao = ("+
+				"	SELECT  m3.id_lota_resp  FROM siga.ex_movimentacao m3  	WHERE m3.id_mov =("+
+				"		SELECT  max (m2.id_mov) FROM siga.ex_movimentacao m2  	INNER JOIN siga.ex_mobil mb2 on (mb2.id_mobil = m2.id_mobil)  "+
 				" 		WHERE mb2.id_doc = d.id_doc AND m2.id_mov < m.id_mov )	)	 ";
 		
-		String sqlColunas =" SELECT  u.acronimo_orgao_usu || '-' ||	f.sigla_forma_doc || '-' ||	d.ano_emissao || '/' ||	lpad (d.num_expediente, 5, '0') NUM_PROCESSO, "+
-				"	m.dt_timestamp DATA_DESPACHO, "+
-				"	m.dt_timestamp DATA_RECEBIMENTO, "+
-				"	m.dt_timestamp DATA_SAÍDA, "+
-				"	m.id_tp_mov COD_DESPACHO, "+
-				"	tm.descr_tipo_movimentacao DESCR_DESPACHO, "+
-				"	c.codificacao COD_ASSUNTO, "+
-				"	c.descr_classificacao DESCR_ASSUNTO, ("+
-				sqlOrgaoOrigem + 
-				" ORG_ORIGEM, ("+
-				sqlOrgaoOrigemDescricao +
-				" ) DESCR_ORG_ORIGEM,  "+
-				" 	l.id_lotacao ORG_DESTINO,  "+
-				" 	l.nome_lotacao DESCR_ORG_DESTINO,  "+
-				" 	m.id_cadastrante MATR_DIGITADOR";
-				
+		String sqlColunas = " X1.NUM_PROCESSO,	X1.DATA_DESPACHO,  X1.DATA_RECEBIMENTO, X1.DATA_SAIDA,  X1.COD_DESPACHO,  X1.DESCR_DESPACHO, X1.COD_ASSUNTO,  X1.DESCR_ASSUNTO,  X1.ORG_ORIGEM,  X1.DESCR_ORG_ORIGEM,  X1.ORG_DESTINO,  X1.DESCR_ORG_DESTINO,  X1.MATR_DIGITADOR ";
+					
 		String sql =
-				  " SELECT  X1.*   FROM (	"+
-						  sqlColunas +
+				" SELECT "+ sqlColunas  +"	FROM (	"+
+				" SELECT  u.acronimo_orgao_usu || '-' ||	f.sigla_forma_doc || '-' ||	d.ano_emissao || '/' ||	lpad (d.num_expediente, 5, '0') NUM_PROCESSO, "+
+				"	m.dt_timestamp DATA_DESPACHO,  m.dt_timestamp DATA_RECEBIMENTO, m.dt_timestamp DATA_SAIDA, m.id_tp_mov COD_DESPACHO, "+
+				"	tm.descr_tipo_movimentacao DESCR_DESPACHO,  c.codificacao COD_ASSUNTO,  c.descr_classificacao DESCR_ASSUNTO, ("+
+				sqlOrgaoOrigem + 	" ORG_ORIGEM, ("+	sqlOrgaoOrigemDescricao +" ) DESCR_ORG_ORIGEM,  l.id_lotacao ORG_DESTINO,  l.nome_lotacao DESCR_ORG_DESTINO,  "+
+				" 	m.id_cadastrante MATR_DIGITADOR " +
 				"	FROM siga.ex_documento d  "+
 				" 		INNER JOIN siga.ex_classificacao c on (c.id_classificacao = d.id_classificacao)  "+
 				" 		INNER JOIN siga.ex_mobil mb on (mb.id_doc = d.id_doc)  "+
@@ -206,12 +108,8 @@ public class RelTeste extends RelatorioTemplate {
 				" 		INNER JOIN siga.ex_forma_documento f on (f.id_forma_doc = d.id_forma_doc) "+
 				" 	ORDER BY NUM_PROCESSO , DATA_DESPACHO DESC) x1 "+
 				" 		INNER JOIN  (  "+
-				" 	SELECT  u.acronimo_orgao_usu || '-' ||"+
-				"	 	f.sigla_forma_doc || '-' ||"+
-				"	 	d.ano_emissao || '/' ||"+
-				"	 	lpad (d.num_expediente, 5, '0') NUM_PROCESSO,"+
-				" 		MAX(m.dt_timestamp) DATA_DESPACHO"+
-				"	 FROM siga.ex_documento d"+
+				" 	SELECT  u.acronimo_orgao_usu || '-' || f.sigla_forma_doc || '-' || 	d.ano_emissao || '/' || lpad (d.num_expediente, 5, '0') NUM_PROCESSO,"+
+				" 		MAX(m.dt_timestamp) DATA_DESPACHO FROM siga.ex_documento d"+
 				"		INNER JOIN siga.ex_classificacao c on (c.id_classificacao = d.id_classificacao)"+
 				"	 	INNER JOIN siga.ex_mobil mb on (mb.id_doc = d.id_doc)"+
 				"	 	INNER JOIN siga.ex_movimentacao m on (m.id_mobil = mb.id_mobil)"+
@@ -220,10 +118,13 @@ public class RelTeste extends RelatorioTemplate {
 				"	 	INNER JOIN corporativo.dp_lotacao l on (d.id_lota_cadastrante = l.id_lotacao)"+
 				"	 	INNER JOIN corporativo.cp_orgao_usuario u on (l.id_orgao_usu = u.id_orgao_usu)"+
 				"	 	INNER JOIN siga.ex_forma_documento f on (f.id_forma_doc = d.id_forma_doc)"+
-				"	where (tm.id_tp_mov >77) or tm.id_tp_mov in(1,3,4,9,17,18,19,20,23,48,49,56)"+
-				"	 GROUP BY u.acronimo_orgao_usu,f.sigla_forma_doc,d.ano_emissao,d.num_expediente"+
-				"	 ORDER BY NUM_PROCESSO desc  ) x2 on x1.NUM_PROCESSO = X2.NUM_PROCESSO AND X1.DATA_DESPACHO = X2.DATA_DESPACHO"+
-				"	AND x1.COD_DESPACHO <> 48";
+				"	WHERE (tm.id_tp_mov >77) OR tm.id_tp_mov in(1,3,4,9,17,18,19,20,23,48,49,56)"+
+				"	GROUP BY u.acronimo_orgao_usu,f.sigla_forma_doc,d.ano_emissao,d.num_expediente"+
+				"	ORDER BY NUM_PROCESSO desc  ) x2 on x1.NUM_PROCESSO = X2.NUM_PROCESSO AND X1.DATA_DESPACHO = X2.DATA_DESPACHO"+
+				"	AND x1.COD_DESPACHO <> 48 "+
+				"   GROUP BY " + sqlColunas + 
+				"   ORDER BY  x1.ORG_DESTINO, x1.NUM_PROCESSO ";
+				 
 		
 	
 	return sql;
@@ -235,49 +136,34 @@ public class RelTeste extends RelatorioTemplate {
 
 		List<String> d = new ArrayList<String>();
 
-//		String sql =
-//				"SELECT	u.acronimo_orgao_usu || '-' ||	f.sigla_forma_doc || '-' || d.ano_emissao || '/' ||	lpad (d.num_expediente, 5, '0') ,	 m.dt_timestamp,	"
-//				+ "	m.id_tp_mov ,		tm.descr_tipo_movimentacao,		c.codificacao ,		c.descr_classificacao ,		"
-//				+ " ( SELECT	l.id_lotacao    FROM corporativo.dp_lotacao l2  WHERE l2.id_lotacao = "
-//				+ " ( SELECT	m3.id_lota_resp FROM siga.ex_movimentacao m3    WHERE m3.id_mov ="
-//				+ " ( SELECT max (m2.id_mov)	FROM siga.ex_movimentacao m2 	INNER JOIN siga.ex_mobil mb2 on (mb2.id_mobil = m2.id_mobil)"
-//				+ "	WHERE mb2.id_doc = d.id_doc AND m2.id_mov < m.id_mov	)		)		), "
-//				+ "( SELECT		l.nome_lotacao	FROM corporativo.dp_lotacao l2 	WHERE l2.id_lotacao = "
-//				+ "( SELECT		m3.id_lota_resp	FROM siga.ex_movimentacao m3	WHERE m3.id_mov ="
-//				+ "( SELECT 	max (m2.id_mov)	FROM siga.ex_movimentacao m2	INNER JOIN siga.ex_mobil mb2 on (mb2.id_mobil = m2.id_mobil)"
-//				+ "	WHERE mb2.id_doc = d.id_doc	AND m2.id_mov < m.id_mov))), l.id_lotacao ,	l.nome_lotacao ,	m.id_cadastrante "
-//				+ " FROM siga.ex_documento d 	INNER JOIN siga.ex_classificacao c on (c.id_classificacao = d.id_classificacao)	"
-//				+ " INNER JOIN siga.ex_mobil mb on (mb.id_doc = d.id_doc)	"
-//				+ " INNER JOIN siga.ex_movimentacao m on (m.id_mobil = mb.id_mobil)	"
-//				+ " INNER JOIN siga.ex_tipo_movimentacao tm on (tm.id_tp_mov = m.id_tp_mov)	"
-//				+ " INNER JOIN siga.ex_classificacao c on (c.id_classificacao = d.id_classificacao)	"
-//				+ " INNER JOIN corporativo.dp_lotacao l on (d.id_lota_cadastrante = l.id_lotacao)	"
-//				+ " INNER JOIN corporativo.cp_orgao_usuario u on (l.id_orgao_usu = u.id_orgao_usu)"
-//				+ " INNER JOIN siga.ex_forma_documento f on (f.id_forma_doc = d.id_forma_doc) ";
-//
-		
-		Query query = ContextoPersistencia.em().createNativeQuery( montarQueryGeraldo() );
+		Query query = ContextoPersistencia.em().createNativeQuery( montarConsulta() );
 		
 
 		List<Object[]> lista = query.getResultList();
 		
 		List<String> listaFinal = new ArrayList<String>();
+		/*
+		 	this.addColuna("Número", 18, RelatorioRapido.ESQUERDA, false); //NUM. PROCESSO
+			this.addColuna("Dt.Movimentação", 10, RelatorioRapido.ESQUERDA, false); //DATA DESPACHO
+			this.addColuna("Despacho", 20, RelatorioRapido.ESQUERDA, false);   //COD. DESPACHO +DESCR. DESPACHO 
+			this.addColuna("Assunto", 30, RelatorioRapido.ESQUERDA, true);   //COD. ASSUNTO+ DESCR. ASSUNTO,
+			this.addColuna("Órgão Origem", 25, RelatorioRapido.ESQUERDA, false);   //ORG. ORIGEM + DESCR. ORG. ORIGEM
+			this.addColuna("Órgão Destino", 30, RelatorioRapido.ESQUERDA, true);   //ORG. DESTINO + DESCR. ORG. DESTINO
+			this.addColuna("Matr.Digitador", 10, RelatorioRapido.ESQUERDA, false);   //MATR. DIGITADOR
+		 */
 		
 		for (Object[] array : lista) {
 			
-//			for (int i = 0; i < array.length; i++) {
-//				listaFinal.add(String.valueOf( array[i] ));
-//			}
-			
 			listaFinal.add( String.valueOf( array[0] ));//NUM. PROCESSO
 			listaFinal.add( String.valueOf(formatter.format( array[1] )));//DATA DESPACHO
-			listaFinal.add( String.valueOf(formatter.format( array[1] )));//DATA RECEBIMENTO
-			listaFinal.add( String.valueOf(formatter.format( array[1] )));//,DATA SAÍDA
-			listaFinal.add( String.valueOf( array[2] ) +" "+ String.valueOf( array[3] ));//COD. DESPACHO + DESCR. DESPACHO,
-			listaFinal.add( String.valueOf( array[4] ) +" "+ String.valueOf( array[5] ));//COD. ASSUNTO  + DESCR. ASSUNTO
-			listaFinal.add( String.valueOf( array[6] ) +" "+ String.valueOf( array[7] ));//ORG. ORIGEM	 + DESCR. ORG. ORIGEM
-			listaFinal.add( String.valueOf( array[8] ) +" "+ String.valueOf( array[9] ));//ORG. DESTINO 	 + DESCR. ORG. DESTINO
-			listaFinal.add( String.valueOf( array[10]));//MATR. DIGITADOR
+//			listaFinal.add( String.valueOf(formatter.format( array[2] )));//DATA RECEBIMENTO
+//			listaFinal.add( String.valueOf(formatter.format( array[3] )));//,DATA SAÍDA
+			listaFinal.add( String.valueOf( array[4] ) +" "+ String.valueOf( array[5] ));//COD. DESPACHO + DESCR. DESPACHO,
+			listaFinal.add( String.valueOf( array[6] ) +" "+ String.valueOf( array[7] ));//COD. ASSUNTO  + DESCR. ASSUNTO
+			
+			listaFinal.add(	array[8] != null ? String.valueOf( array[8] ) +" "+ String.valueOf( array[9] ) : ""		);//ORG. ORIGEM	 + DESCR. ORG. ORIGEM
+			listaFinal.add( array[10]  != null ?  String.valueOf( array[10] ) +" "+ String.valueOf( array[11]  ):"");//ORG. DESTINO 	 + DESCR. ORG. DESTINO
+			listaFinal.add( String.valueOf( array[12]));//MATR. DIGITADOR
 			
 		}
 		

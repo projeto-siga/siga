6d87c4f915991667f3e2705c0bca2733160ad15f
git <darlansc@yahoo.com.br>
2021-03-09 17:35:32 -0300

    incluindo brasao PCRJ

--- siga-cp/src/main/resources/db/mysql/sigacp/V90.0__Cria_indicador_orgão_suspensa.sql
+++ siga-cp/src/main/resources/db/mysql/sigacp/V90.0__Cria_indicador_orgão_suspensa.sql
@@ -1,4 +1,4 @@
 -- -------------------------------------------------------------------------    
 --  Coluna de number 0 ou 1 para informar se lotação esta suspensa
 -- -------------------------------------------------------------------------
-ALTER TABLE CORPORATIVO.DP_LOTACAO ADD IS_SUSPENSA tinyint(4) DEFAULT 0;
+ALTER TABLE corporativo.dp_lotacao ADD IS_SUSPENSA tinyint(4) DEFAULT 0;

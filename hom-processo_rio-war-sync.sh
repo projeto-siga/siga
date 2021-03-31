278a5cac2a5e254b6827911b9c5d368167b98cc1
Ramiris Albuquerque da Silva <ramiris.silva@rio.rj.gov.br>
2021-03-31 11:16:54 -0300

    Update hom-processo_rio-war-sync.sh - teste de execução do script

--- hom-processo_rio-war-sync.sh
+++ hom-processo_rio-war-sync.sh
@@ -3,12 +3,6 @@
 #This scripts is part of your deployment process. First step is previously executed a rsync or scp command to copy all war's from developer #environment.  Second step is to check environment requirements and finally execute the deploy operation.
 #
 
-#verify args
-#if [ $# -eq 0 ]; then
-#        echo "Not found Maven Parameters: eg.:-T 1C -pl '!siga-dump,!siga-ex-gsa,!siga-ex-sinc,!siga-gc-gsa,!sigagc,!sigasr,!sigapp,!siga-ldap-cli,!sigatp,!siga-wf,!sigawf' clean install -DskipTests=true "
-#        exit 1
-#fi
-
 #set variables
 
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.272.b10-1.el7_9.x86_64/
@@ -23,7 +17,7 @@ echo "                            REQUIREMENTS VERIFICATION"
 echo "################################################################################"
 echo ""
 echo "JAVAC VERSION:"
-if j_version=`java -version 2>&1 |cut -d "\"" -f2|head -n 1`; then
+if j_version=`javac -version 2>&1 |cut -d "\"" -f2|head -n 1`; then
         echo "$j_version"
         echo "OK"
 
@@ -45,17 +39,6 @@ else
         exit 1
 fi
 echo ""
-#echo "MAVEN VERSION:"
-#if m_version=`mvn -version`; then
-#        echo "$m_version"
-#        echo "OK"
-#else
-#        echo m_version
-#        echo "FAIL"
-#        echo "ABORTING..."
-#        exit 1
-#fi
-echo ""
 echo "GRAPHVIZ VERSION:"
 if graphviz=`rpm -qa graphviz`; then
         echo "$graphviz - OK"
@@ -68,115 +51,22 @@ else
         exit 1
 fi
 echo ""
-echo "VIZSERVICE:"
-curl -s https://api.github.com/repos/projeto-siga/vizservice/releases/latest | grep browser_download_url | grep vizservice.war | cut -d '"' -f 4 | xargs wget -q downloading vizservice.war
-if verify_vizservice_war=`ls -l vizservice.war`; then
-        echo "DOWNLOAD: vizservice.war - OK"
-                if deploy_vizservice=`mv -f vizservice.war $JBOSS_HOME/standalone/deployments/`; then
-                        echo "DEPLOY: vizservice.war - OK"
-                else
-                        echo $deploy_vizservice
-                        echo "FAIL"
-                        echo "ABORTING..."
-                        exit 1
-                fi
-else
-        echo "FAIL"
-        echo "DO DOWNLOAD vizservice.war FROM https://api.github.com/repos/projeto-siga/vizservice/releases/latest AND MOVE TO $JBOSS_HOME/standalone/deployments/"
-        echo "ABORTING..."
-        exit 1
-fi
-echo ""
-echo "BLUCSERVICE:"
-curl -s https://api.github.com/repos/assijus/blucservice/releases/latest | grep browser_download_url | grep blucservice.war | cut -d '"' -f 4 | xargs wget -q downloading blucservice.war
-if verify_blucservice_war=`ls -l blucservice.war`; then
-        echo "DOWNLOAD: blucservice.war - OK"
-                if deploy_blucservice=`mv -f blucservice.war $JBOSS_HOME/standalone/deployments/`; then
-                        echo "DEPLOY: blucservice.war - OK"
-                else
-                        echo $deploy_blucservice
-                        echo "FAIL"
-                        echo "ABORTING..."
-                        exit 1
-                fi
-else
-        echo "FAIL"
-        echo "DO DOWNLOAD blucservice.war FROM https://api.github.com/repos/assijus/blucservice/releases/latest AND MOVE TO $JBOSS_HOME/standalone/deployments/"
-        echo "ABORTING..."
-        exit 1
-fi
-echo ""
-echo "CKEDITOR:"
-curl -s https://api.github.com/repos/projeto-siga/siga-docker/releases/latest | grep browser_download_url | grep .war | cut -d '"' -f 4 | xargs wget -q
-if verify_ckeditor_war=`ls -l ckeditor.war`; then
-        echo "DOWNLOAD: ckeditor.war - OK"
-                if deploy_ckeditor=`mv -f ckeditor.war $JBOSS_HOME/standalone/deployments/`; then
-                        echo "DEPLOY: ckeditor.war - OK"
-                else
-                        echo $deploy_ckeditor
-                        echo "FAIL"
-                        echo "ABORTING..."
-                        exit 1
-                fi
-else
-        echo "FAIL"
-        echo "DO DOWNLOAD ckeditor.war FROM  https://api.github.com/repos/projeto-siga/siga-docker/releases/latest AND MOVE TO $JBOSS_HOME/standalone/deployments/"
-        echo "ABORTING..."
-        exit 1
-fi
-echo ""
 echo "###############################################################################"
 echo "                                   END"
 echo "###############################################################################"
 echo ""
-#echo ""
-#echo ""
-#echo "###############################################################################"
-#echo "                              STARTING MAVEN"
-#echo "###############################################################################"
-#echo ""
-#echo ""
-#mvn $@
-#echo ""
-#echo "###############################################################################"
-#echo "                                   END"
-#echo "###############################################################################"
-#echo ""
-echo ""
 echo "###############################################################################"
-echo "                              STARTING DEPLOY"
+echo "                              STARTING SCP"
 echo "###############################################################################"
-echo ""
-echo "SIGA:"
-if deploy_siga=`/opt/java/jboss-eap-7.2/bin/jboss-cli.sh --connect --command="deployment deploy-file --replace /opt/java/jenkins/workspace/processo.rio/target/siga.war"`; then
-        echo "DEPLOY: siga.war - OK"
+scp jboss@jdevas135:/opt/java/jenkins/workspace/processo.rio/target/*.{war,jar} /tmp
+
+if list_war_jar=`ls /tmp/*.{war,jar}`; then
+        echo "$list_war_jar"
+        echo "OK"
+
 else
-        echo $deploy_siga
+        echo $list_war_jar
         echo "FAIL"
         echo "ABORTING..."
         exit 1
 fi
-echo ""
-echo "SIGAEX:"
-if deploy_sigaex=`/opt/java/jboss-eap-7.2/bin/jboss-cli.sh --connect --command="deployment deploy-file --replace /opt/java/jenkins/workspace/processo.rio/target/sigaex.war"`; then
-        echo "DEPLOY: sigaex.war - OK"
-else
-        echo $deploy_sigaex
-        echo "FAIL"
-        echo "ABORTING..."
-        exit 1
-fi
-echo ""
-echo "SIGA-EXT"
-if deploy_siga_ext=`/opt/java/jboss-eap-7.2/bin/jboss-cli.sh --connect --command="deployment deploy-file --replace /opt/java/jenkins/workspace/processo.rio/target/siga-ext.jar"`; then
-        echo "DEPLOY: siga-ext.war - OK"
-else
-        echo $deploy_siga_ext
-        echo "FAIL"
-        echo "ABORTING..."
-        exit 1
-fi
-echo ""
-echo "###############################################################################"
-echo "                              END"
-echo "###############################################################################"

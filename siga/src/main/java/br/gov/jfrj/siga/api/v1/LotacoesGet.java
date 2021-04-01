4e90aa649c9bc1d7b0e2d0829d215d2916263a17
Newton <ny2345@gmail.com>
2021-04-01 12:20:14 -0300

    GOVSP1945* - Pesquisa por órgão no WS de lotações
    e melhoria na documentação do swagger do siga

--- siga/src/main/java/br/gov/jfrj/siga/api/v1/LotacoesGet.java
+++ siga/src/main/java/br/gov/jfrj/siga/api/v1/LotacoesGet.java
@@ -16,16 +16,22 @@ import br.gov.jfrj.siga.base.Texto;
 import br.gov.jfrj.siga.dp.CpOrgaoUsuario;
 import br.gov.jfrj.siga.dp.DpLotacao;
 import br.gov.jfrj.siga.dp.dao.CpDao;
+import br.gov.jfrj.siga.dp.dao.CpOrgaoUsuarioDaoFiltro;
 import br.gov.jfrj.siga.dp.dao.DpLotacaoDaoFiltro;
 
 public class LotacoesGet implements ILotacoesGet {
 	@Override
 	public void run(LotacoesGetRequest req, LotacoesGetResponse resp) throws Exception {
 		try (ApiContext ctx = new ApiContext(false, true)) {
-			if (req.texto != null && req.idLotacaoIni != null) {
+			if ((((req.texto != null? 1:0) + (req.idLotacaoIni != null? 1:0) + (req.siglaOrgaoQuery != null? 1:0)) > 1)) {
 				throw new AplicacaoException("Pesquisa permitida somente por um dos argumentos.");
 			}
 	
+			if (req.siglaOrgaoQuery != null && !req.siglaOrgaoQuery.isEmpty()) {
+				resp.list = pesquisarPorOrgao(req, resp);
+				return;
+			}
+				
 			if (req.texto != null && !req.texto.isEmpty()) {
 				resp.list = pesquisarPorTexto(req, resp);
 				return;
@@ -43,6 +49,22 @@ public class LotacoesGet implements ILotacoesGet {
 			e.printStackTrace(System.out);
 			throw e;
 		}
+	}
+
+	private List<Lotacao> pesquisarPorOrgao(LotacoesGetRequest req, LotacoesGetResponse resp) {
+		final DpLotacaoDaoFiltro flt = new DpLotacaoDaoFiltro();
+		final CpOrgaoUsuarioDaoFiltro fltOrg = new CpOrgaoUsuarioDaoFiltro();
+		fltOrg.setSigla(req.siglaOrgaoQuery);
+		CpOrgaoUsuario org = (CpOrgaoUsuario) CpDao.getInstance()
+				.consultarPorSigla(fltOrg);
+		if (org != null)
+			flt.setIdOrgaoUsu(Long.valueOf(org.getId()));
+		else
+			throw new AplicacaoException("Órgão não encontrado.");
+		
+		List<DpLotacao> l; 
+		l = CpDao.getInstance().consultarLotacaoPorOrgao(org);
+		return l.stream().map(this::lotacaoToResultadoPesquisa).collect(Collectors.toList());
 	}
 
 	private List<Lotacao> pesquisarPorTexto(LotacoesGetRequest req, LotacoesGetResponse resp) {

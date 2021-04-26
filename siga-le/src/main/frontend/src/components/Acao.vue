52072a16c26acb9dc67b2e0e85b5f57667d2c9e5
Renato Crivano <renato.crivano@gmail.com>
2021-04-26 11:59:55 -0300

    Incluindo botões para as outras ações

---
+++ siga-le/src/main/frontend/src/components/Acao.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button
+      type="button"
+      @click="tramitar()"
+      class="btn btn-secondary d-print-none"
+    >
+      <span class="fa fa-paper-plane-o"></span>
+      {{ nome }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    acao: { required: true },
+  },
+  computed: {
+    nome() {
+      return this.acao.nome.replace("_", "");
+    },
+  },
+};
+</script>

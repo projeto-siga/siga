195ab34774ca2601cdbfa4a7d85fd522f73216dc
Renato Crivano <renato.crivano@gmail.com>
2021-06-09 16:29:45 -0300

    Removendo projetos que não estão sendo usados

--- sigapp/src/main/resources/db/migration/SIGAPMP_UTF8_V00000000000000000003_0__administradores.sql
+++
@@ -1,2 +0,0 @@
--- Script que tem por finalidade permitir o usuario SIGAPMP_CON inserir, alterar, excluir ou consultar a tabela SIGAPMP.ADMINISTRADORES
-grant select, insert, update, delete on SIGAPMP.ADMINISTRADORES to SIGAPMP_CON; 

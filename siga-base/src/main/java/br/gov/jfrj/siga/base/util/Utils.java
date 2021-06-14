368ae33728eab5f8e6ce4c36d64f05b5f6515983
Michel Risucci <michelrisucci@codata.pb.gov.br>
2021-06-14 11:18:58 -0300

    [#6] Otimizações gerais e melhorias de qualidade de software
    Utilização do StandardCharsets no parse de valores

--- siga-base/src/main/java/br/gov/jfrj/siga/base/util/Utils.java
+++ siga-base/src/main/java/br/gov/jfrj/siga/base/util/Utils.java
@@ -2,27 +2,36 @@ package br.gov.jfrj.siga.base.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.jboss.logging.Logger;
+
 import br.gov.jfrj.siga.base.Texto;
 
 public class Utils {
+
+	private static final Logger log = Logger.getLogger(Utils.class);
+
+	private Utils() {}
+
 	/**
 	 * @param map
 	 * @param form
 	 */
-	public static void mapFromUrlEncodedForm(Map map, final byte[] form) {
+	public static void mapFromUrlEncodedForm(final Map<String, Object> map, final byte[] form) {
 		if (form != null) {
 			final String as[] = new String(form).split("&");
 			for (final String s : as) {
 				final String param[] = s.split("=");
 				try {
 					if (param.length == 2) {
-						map.put(param[0], URLDecoder.decode(param[1], "iso-8859-1"));
+						map.put(param[0], URLDecoder.decode(param[1], StandardCharsets.ISO_8859_1.name()));
 					}
 				} catch (final UnsupportedEncodingException e) {
+					log.warn("Erro mapeando parâmetros do formulário da requisição Web", e);
 				}
 			}
 		}

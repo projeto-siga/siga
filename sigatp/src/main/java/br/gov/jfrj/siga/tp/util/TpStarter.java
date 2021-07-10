6c8674d78dc35e7cf0a27b17d6d0c50a36941e5f
Joao Luis <joaoluis@trf2.jus.br>
2021-07-10 18:17:20 -0300

    TP:Fix cpconfiguracao em cache

--- sigatp/src/main/java/br/gov/jfrj/siga/tp/util/TpStarter.java
+++ sigatp/src/main/java/br/gov/jfrj/siga/tp/util/TpStarter.java
@@ -1,22 +1,34 @@
 package br.gov.jfrj.siga.tp.util;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import org.jboss.logging.Logger;
+
+import br.gov.jfrj.siga.cp.model.enm.CpTipoDeConfiguracao;
 
 /**
  * 
  * @author Rodrigo Ramalho hodrigohamalho@gmail.com
  *
  */
+@Startup
+@Singleton
 @ApplicationScoped
 public class TpStarter {
+	
+	private final static org.jboss.logging.Logger log = Logger.getLogger(TpStarter.class);
 
 	public static EntityManagerFactory emf;
 
 	@PostConstruct
 	public void init() {
+		log.info("INICIANDO SIGATP.WAR");
+		CpTipoDeConfiguracao.mapear(CpTipoDeConfiguracao.values());
 		emf = Persistence.createEntityManagerFactory("default");
 	}
 }

9ea2f8e0af68a4b84a976731926145c44218d5e3
Ramiris Albuquerque da Silva <ramiris.silva@rio.rj.gov.br>
2021-10-15 20:21:31 +0000

    Update pro-processo_rio-war-sync.sh

--- pro-processo_rio-war-sync.sh
+++ pro-processo_rio-war-sync.sh
@@ -75,7 +75,7 @@ targets=(siga-ext.jar sigaex.war siga.war)
 
 ## DECLARE PRODUCTION SYNC SERVERS ARRAY
 #production_servers=(jproas135-1.infra.rio.gov.br jproas135-2.infra.rio.gov.br)
-production_servers=()
+production_servers=(jproas135.infra.rio.gov.br)
 
 
 

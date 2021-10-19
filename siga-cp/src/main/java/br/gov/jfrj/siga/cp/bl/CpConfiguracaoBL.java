8ac0d928829fd965f45591a7b9dcf192fcb23799
Adriana <adrigrassi@gmail.com>
2021-10-19 16:00:04 -0300

    TRF2-FIX-GI: Correção de erro para aparecer Pessoas Extras no Acessos a serviços.

--- siga-cp/src/main/java/br/gov/jfrj/siga/cp/bl/CpConfiguracaoBL.java
+++ siga-cp/src/main/java/br/gov/jfrj/siga/cp/bl/CpConfiguracaoBL.java
@@ -830,15 +830,15 @@ public class CpConfiguracaoBL {
 			if (configs != null) {
 				for (CpConfiguracaoCache c : configs) {
 					DpPessoa pesAtual = CpDao.getInstance().consultarPorIdInicial(c.dpPessoa);
-					if (c.dpPessoa == pesAtual.getIdInicial()) {
-						if (c.hisDtFim == null && pesAtual.getDataFim() == null && c.lotacao == lot.getIdInicial()) {
-							resultado.add(pesAtual);
+					if (pesAtual != null) {
+						if (c.dpPessoa == pesAtual.getIdInicial()) {
+							if (c.hisDtFim == null && pesAtual.getDataFim() == null && c.lotacao == lot.getIdInicial()) {
+								resultado.add(pesAtual);
+							}
 						}
 					}
 				}
 			}
-
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;

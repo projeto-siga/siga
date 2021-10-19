c540b8311f238dc6a21dcad653eb9ffe53ebe624
Renato Crivano <renato.crivano@gmail.com>
2021-10-19 12:03:38 -0300

    Revert "FIX: RM80931 - Correcao de HQL referente join das tabelas corporativo.cp_identidade e corporativo.dp_pessoa"
    This reverts commit d05a404ef990a014c8ee41d33690b0d2b915351c.

--- siga-cp/src/main/java/br/gov/jfrj/siga/cp/AbstractCpIdentidade.java
+++ siga-cp/src/main/java/br/gov/jfrj/siga/cp/AbstractCpIdentidade.java
@@ -54,7 +54,7 @@ import br.gov.jfrj.siga.sinc.lib.Desconsiderar;
 		@NamedQuery(name = "consultarIdentidadeCadastranteAtiva", query = ""
 				+ "select u "
 				+ "from CpIdentidade u "
-				+ "join DpPessoa pes ON u.dpPessoa  = pes.idPessoaIni "
+				+ "inner join u.dpPessoa pes "
 				+ "where "
 				+ "	((u.nmLoginIdentidade = :nmUsuario and pes.sesbPessoa = :sesbPessoa and pes.sesbPessoa is not null) "
 				+ "		or (pes.cpfPessoa is not null and pes.cpfPessoa <> :cpfZero and pes.cpfPessoa = :cpf)) "

2041cce4fb80ea57c8a8f97936a4b1235c166fc2
tah <tah@TRF2-030350.corp.jfrj.gov.br>
2022-02-15 13:27:06 -0300

    HOTFIX: EX: Acrescentando descrição do documento no preenchimento automático, quando houver

--- sigaex/src/main/java/br/gov/jfrj/siga/vraptor/ExDocumentoController.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/vraptor/ExDocumentoController.java
@@ -2402,6 +2402,9 @@ public class ExDocumentoController extends ExController {
 		final String[] aVars = vars;
 		final String[] aCampos = campos;
 		final ArrayList<String> aFinal = new ArrayList<String>();
+		String descrDocumento = param("exDocumentoDTO.descrDocumento");
+		if (descrDocumento != null && !descrDocumento.trim().isEmpty())
+			aFinal.add("descrDocumento");
 		if (aVars != null && aVars.length > 0) {
 			for (final String str : aVars) {
 				aFinal.add(str);

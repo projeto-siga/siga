dedde55c484560b49bd126691e03567741806a9a
Heverson Vasconcelos <heverson.vasconcelos@gmail.com>
2022-05-26 18:20:58 -0300

    Remoção dos scripts sql tipos de movimentação

--- siga-ex/src/main/resources/db/migration/SIGA_UTF8_V109__inclusao_movimentacao.sql
+++
@@ -1,6 +0,0 @@
----------------------------------------------------------
--- ACRESCENTANDO MOVIMENTAÇÃO
----------------------------------------------------------
-ALTER SESSION SET CURRENT_SCHEMA=siga;
-
-INSERT INTO SIGA.EX_TIPO_MOVIMENTACAO VALUES (86, 'Gerar link público do Processo');
--- siga-ex/src/main/resources/db/mysql/sigaex/V109.0__inclusao_tipo_mov_gerar_link_processo.sql
+++
@@ -1,5 +0,0 @@
--- ------------------------------------------------------------------------------------------------
---  Criação do Tipo de Movimentação: Gerar link público do Processo
---  Permite registrar o procedimento de gerar link público do Processo
--- ------------------------------------------------------------------------------------------------
-insert into siga.ex_tipo_movimentacao (id_tp_mov, DESCR_TIPO_MOVIMENTACAO) values (86, 'Gerar link público do Processo');

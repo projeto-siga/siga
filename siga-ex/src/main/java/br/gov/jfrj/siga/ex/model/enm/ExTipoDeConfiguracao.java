67e52b83c7db438bc713a2685026a58ac94e1d80
gabrielfilipy <filipy.hughes@gmail.com>
2022-06-08 10:32:55 -0300

    Alterando nome da configuração.

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/model/enm/ExTipoDeConfiguracao.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/model/enm/ExTipoDeConfiguracao.java
@@ -567,9 +567,7 @@ public enum ExTipoDeConfiguracao implements ITipoDeConfiguracao {
 					CpSituacaoDeConfiguracaoEnum.NAO_PODE },
 			CpSituacaoDeConfiguracaoEnum.NAO_PODE, true),
 	//
-	RESTRINGIR_VINCULACAO_DO_ORGAO_NO_CAMPO_BUSCAR(60, "Restringir Vinculação do Órgão no Campo Busca", "Esta configuração define que um órgão não poderá ser exibido aos usuários de outros órgãos quando utilizarem o campo de pesquisa. "
-			+ "“Pode”, ou seja, após a configuração realizada, o órgão poderá ser visto para todos ou alguns órgãos especificado na configuração.<br>"
-			+ "“Não Pode”, ou seja, após a configuração realizada, o órgão não poderá ser visto para todos ou alguns órgãos especificado na configuração.",
+	RESTRINGIR_VINCULACAO_DO_ORGAO_NO_CAMPO_BUSCAR(60, "Restringir Pesquisa de Órgão", "",
 			new Enum[] { CpParamCfg.PESSOA, CpParamCfg.LOTACAO, CpParamCfg.CARGO,CpParamCfg.FUNCAO, CpParamCfg.ORGAO,
 					CpParamCfg.PESSOA_OBJETO, CpParamCfg.LOTACAO_OBJETO, CpParamCfg.CARGO_OBJETO,
 					CpParamCfg.FUNCAO_OBJETO, CpParamCfg.ORGAO_OBJETO},

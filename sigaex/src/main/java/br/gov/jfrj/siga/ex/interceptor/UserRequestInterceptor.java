df48c4e820e0273e4b6921f57a79f6529ff161c8
Heverson Vasconcelos <heverson.vasconcelos@gmail.com>
2022-07-14 16:34:51 -0300

    Log dos dados do usuário requisitante

--- sigaex/src/main/java/br/gov/jfrj/siga/ex/interceptor/UserRequestInterceptor.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/interceptor/UserRequestInterceptor.java
@@ -4,6 +4,10 @@ import br.com.caelum.vraptor.AroundCall;
 import br.com.caelum.vraptor.Intercepts;
 import br.com.caelum.vraptor.interceptor.AcceptsWithAnnotations;
 import br.com.caelum.vraptor.interceptor.SimpleInterceptorStack;
+import br.gov.jfrj.siga.base.HttpRequestUtils;
+import br.gov.jfrj.siga.dp.DpPessoa;
+import br.gov.jfrj.siga.hibernate.ExDao;
+import br.gov.jfrj.siga.vraptor.SigaObjects;
 import br.gov.jfrj.siga.vraptor.TrackRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -16,15 +20,33 @@ import javax.servlet.http.HttpServletRequest;
 @RequestScoped
 @AcceptsWithAnnotations(TrackRequest.class)
 public class UserRequestInterceptor {
-    @Inject
     private HttpServletRequest request;
 
+    private DpPessoa cadastrante;
 
     private static Log log = LogFactory.getLog(UserRequestInterceptor.class);
 
+    public UserRequestInterceptor() {
+        this.request = null;
+        this.cadastrante = null;
+    }
+
+    @Inject
+    public UserRequestInterceptor(HttpServletRequest request, SigaObjects sigaObjects) {
+        this.request = request;
+        this.cadastrante = sigaObjects.getCadastrante();
+    }
+
     @AroundCall
     public void around(SimpleInterceptorStack stack) {
-        log.info(request.getRequestURI());
+        log.info(
+                request.getRequestURL() + ";"
+                        + this.cadastrante.getMatricula() + ";"
+                        + this.cadastrante.getLotacao() + ";"
+                        + this.cadastrante.getNomePessoa() + ";"
+                        + ExDao.getInstance().consultarDataEHoraDoServidor() + ";"
+                        + HttpRequestUtils.getIpAudit(request)
+        );
 
         stack.next();
     }

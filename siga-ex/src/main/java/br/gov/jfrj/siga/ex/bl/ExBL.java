be77615b3e042ad13f59b1f8162ef3d33f7d3a47
Dinarde Bezerra <dinarde@gmail.com>
2022-08-31 17:50:03 -0300

    Correção para Assijus que não tem Cadastrante no Hash
    Método obterCadastrante não é possível utilizar pois CPF não é enviado na requisição.
    Como documento já está salvo, valor é desprezado.
    Sendo assim será enviado Null

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExBL.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/bl/ExBL.java
@@ -8255,7 +8255,7 @@ public class ExBL extends CpBL {
 
 			if (! dataAtualSemTempo.equals(dataPrimeiraAssinatura)) {
 				doc.setDtPrimeiraAssinatura(dataAtualSemTempo);  
-				gravar(cadastrante, titular, titular.getLotacao(), doc);
+				gravar(cadastrante, titular, titular != null ? titular.getLotacao() : null, doc);
 			}
 		}
 	}

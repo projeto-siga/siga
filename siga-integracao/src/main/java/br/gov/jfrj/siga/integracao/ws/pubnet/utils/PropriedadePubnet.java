0950128b42638aca455e20d05a86afee9f7cd8c8
Fernando Henrique Pascott <fernando.pascott@apoioprodesp.sp.gov.br>
2022-10-05 16:35:37 -0300

    RM130566 - Alteracao classes de propriedades para funcionamento do fluxo

--- siga-integracao/src/main/java/br/gov/jfrj/siga/integracao/ws/pubnet/utils/PropriedadePubnet.java
+++ siga-integracao/src/main/java/br/gov/jfrj/siga/integracao/ws/pubnet/utils/PropriedadePubnet.java
@@ -1,10 +1,10 @@
 package br.gov.jfrj.siga.integracao.ws.pubnet.utils;
 
-//import br.gov.jfrj.siga.base.Prop;
+import br.gov.jfrj.siga.base.Prop;
 
 public class PropriedadePubnet {
-	public static final String URL_WSDL = "https://pubnetdes.imprensaoficial.com.br/wspubnet/pubnet.asmx?wsdl";//Prop.get("ws.siafem.url.wsdl");
-	public static final String URL_NAMESPACE = "http://pubnet.imprensaoficial.com.br/wspubnet"; //Prop.get("ws.siafem.url.namespace");
+	public static final String URL_WSDL = Prop.get("ws.pubnet.url.wsdl");
+	public static final String URL_NAMESPACE = "http://pubnet.imprensaoficial.com.br/wspubnet";
 	public static final String LOCAL_PART = "pubnet";
 	public static final String LOCAL_PART_SOAP = "pubnetSoap";
 	public static final String LOCAL_PART_SOAP12 = "pubnetSoap12";

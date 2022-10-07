e986aa60ace4ac27b38bd9bc1c0ddfd2be2fd725
Fernando Henrique Pascott <fernando.pascott@apoioprodesp.sp.gov.br>
2022-10-07 14:20:16 -0300

    RM130566 - Inclusao de properties do Sigaex para standalone

--- siga-integracao/src/main/java/br/gov/jfrj/siga/integracao/ws/pubnet/utils/PropriedadePubnet.java
+++ siga-integracao/src/main/java/br/gov/jfrj/siga/integracao/ws/pubnet/utils/PropriedadePubnet.java
@@ -3,7 +3,7 @@ package br.gov.jfrj.siga.integracao.ws.pubnet.utils;
 import br.gov.jfrj.siga.base.Prop;
 
 public class PropriedadePubnet {
-	public static final String URL_WSDL = "https://pubnetdes.imprensaoficial.com.br/wspubnet/pubnet.asmx?wsdl"; //Prop.get("ws.pubnet.url.wsdl");
+	public static final String URL_WSDL = Prop.get("ws.pubnet.url.wsdl");
 	public static final String URL_NAMESPACE = "http://pubnet.imprensaoficial.com.br/wspubnet";
 	public static final String LOCAL_PART = "pubnet";
 	public static final String LOCAL_PART_SOAP = "pubnetSoap";
--- sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/ExApiV1Servlet.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/ExApiV1Servlet.java
@@ -269,6 +269,9 @@ public class ExApiV1Servlet extends SwaggerServlet implements IPropertyProvider 
 		//GC Control - Concatenação de PDF
 		addPublicProperty("arquivo.tamanho.gc", "26214400"); //PDF - 25MB
 		addPublicProperty("arquivo.contagem.gc", "25"); //HTML - 25 documentos
+		
+		//Pubnet (DOE)
+		addPublicProperty("ws.pubnet.url.wsdl", null);
 	}
 
 	@Override

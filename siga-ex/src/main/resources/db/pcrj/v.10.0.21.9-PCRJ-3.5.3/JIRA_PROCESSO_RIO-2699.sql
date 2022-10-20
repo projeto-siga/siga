1f499eaf37873eab0b00b2cd94c706ba344a0312
Ivan Gonçalves Silvério <ivan.silverio@iplanrio.rio.rj.gov.br>
2022-10-20 10:07:38 -0300

    Script para criar numeração do modelo  Modelo - Licença Municipal de Operação - LMO https://jeap.rio.rj.gov.br/jira/browse/PROCESSO_RIO-2699 Modelo - Licença Municipal de Operação - LMO PROCESSO_RIO-2699

---
+++ siga-ex/src/main/resources/db/pcrj/v.10.0.21.9-PCRJ-3.5.3/JIRA_PROCESSO_RIO-2699.sql
@@ -0,0 +1,9 @@
+-- https://jeap.rio.rj.gov.br/jira/browse/PROCESSO_RIO-2699
+-- Modelo - Licença Municipal de Operação - LMO
+-- connect siga@srv000764.infra.rio.gov.br:1521/sigadoc.pcrj;
+-- Em siga.ex_forma_documento o códico 441 é Licença Municipal de Operação - LMO
+
+INSERT INTO SIGA.EX_DOCUMENTO_NUMERACAO
+( ID_ORGAO_USU, ID_FORMA_DOC, ANO_EMISSAO, NR_DOCUMENTO, NR_INICIAL, NR_FINAL, FL_ATIVO)
+VALUES( 5200, 441, 2022, xxxx, 1, NULL, '1');
+commit;

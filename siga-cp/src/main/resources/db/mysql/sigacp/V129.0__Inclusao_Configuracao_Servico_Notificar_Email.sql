57921697e01c99266f5c9846e5f9e8a79753fd11
Gabriel Filipy da Silva Correa <gabriel.correa@apoioprodesp.sp.gov.br>
2022-11-11 09:54:37 -0300

    Corrigindo migrate do Mysql.

--- siga-cp/src/main/resources/db/mysql/sigacp/V129.0__Inclusao_Configuracao_Servico_Notificar_Email.sql
+++ siga-cp/src/main/resources/db/mysql/sigacp/V129.0__Inclusao_Configuracao_Servico_Notificar_Email.sql
@@ -1,26 +1,43 @@
 -- CADASTRO DOS SERVICOS DO MÓDULO SIGA-CEMAIL
-INSERT INTO corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico)
-VALUES ('SIGA-CEMAIL',' Módulo de notificação por email',
-        (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL'),'2');
-INSERT INTO corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico)
-VALUES ('SIGA-CEMAIL',' Módulo de notificação por email',
-        (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL-DOCMARC'),'2');
-INSERT INTO corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico)
-VALUES ('SIGA-CEMAIL',' Módulo de notificação por email',
-        (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL-DOCUN'),'2');
-INSERT INTO corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico)
-VALUES ('SIGA-CEMAIL',' Módulo de notificação por email',
-        (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL-DOCUSU'),'2');
-INSERT INTO corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico)
-VALUES ('SIGA-CEMAIL',' Módulo de notificação por email',
-        (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL-COSSIG'),'2');
-INSERT INTO corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico)
-VALUES ('SIGA-CEMAIL',' Módulo de notificação por email',
-        (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL-RESPASSI'),'2');     
+Insert into corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico) 
+   SELECT  'SIGA-CEMAIL',' Módulo de notificação por email',  max(id_servico) , '2'
+   FROM corporativo.cp_servico 
+   WHERE sigla_servico = 'SIGA'   
+   ON DUPLICATE KEY UPDATE sigla_servico = 'SIGA-CEMAIL'; 
+   
+Insert into corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico) 
+   SELECT  'SIGA-CEMAIL-DOCMARC','Notificações de marcadores destinados a minha unidade',  max(id_servico) , '2'
+   FROM corporativo.cp_servico 
+   WHERE sigla_servico = 'SIGA-CEMAIL'   
+   ON DUPLICATE KEY UPDATE sigla_servico = 'SIGA-CEMAIL-DOCMARC';  
+   
+Insert into corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico) 
+   SELECT  'SIGA-CEMAIL-DOCUN','Documento foi tramitado para a minha unidade',  max(id_servico) , '2'
+   FROM corporativo.cp_servico 
+   WHERE sigla_servico = 'SIGA-CEMAIL'   
+   ON DUPLICATE KEY UPDATE sigla_servico = 'SIGA-CEMAIL-DOCUN';  
+   
+Insert into corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico) 
+   SELECT  'SIGA-CEMAIL-DOCUSU','Tramitar para o meu usuário',  max(id_servico) , '2'
+   FROM corporativo.cp_servico 
+   WHERE sigla_servico = 'SIGA-CEMAIL'   
+   ON DUPLICATE KEY UPDATE sigla_servico = 'SIGA-CEMAIL-DOCUSU';  
+   
+Insert into corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico) 
+   SELECT  'SIGA-CEMAIL-COSSIG','Fui incluído como cossignatário de um documento',  max(id_servico) , '2'
+   FROM corporativo.cp_servico 
+   WHERE sigla_servico = 'SIGA-CEMAIL'   
+   ON DUPLICATE KEY UPDATE sigla_servico = 'SIGA-CEMAIL-COSSIG'; 
+   
+Insert into corporativo.cp_servico (sigla_servico, desc_servico, id_servico_pai, id_tp_servico) 
+   SELECT  'SIGA-CEMAIL-RESPASSI','Fui incluído como responsável pela assinatura',  max(id_servico) , '2'
+   FROM corporativo.cp_servico 
+   WHERE sigla_servico = 'SIGA-CEMAIL'   
+   ON DUPLICATE KEY UPDATE sigla_servico = 'SIGA-CEMAIL-RESPASSI'; 
         
 -- PERMISSAO DEFAULT NAO PODE A TODOS PARA OS SERVICOS DO MÓDULO SIGA-CEMAIL
 INSERT INTO corporativo.cp_configuracao (his_dt_ini, id_sit_configuracao, id_tp_configuracao, id_servico, his_idc_ini)
-VALUES (current_timestamp(), '2', '200', (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL'), '1');
+VALUES (current_timestamp(), '2', '200', (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL'), '1'); 
 INSERT INTO corporativo.cp_configuracao (his_dt_ini, id_sit_configuracao, id_tp_configuracao, id_servico, his_idc_ini)
 VALUES (current_timestamp(), '2', '200', (SELECT id_servico FROM corporativo.cp_servico WHERE sigla_servico = 'SIGA-CEMAIL-DOCMARC'), '1');
 INSERT INTO corporativo.cp_configuracao (his_dt_ini, id_sit_configuracao, id_tp_configuracao, id_servico, his_idc_ini)

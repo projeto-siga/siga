f24891fa1a9e602b74f0b9c97b1477859d533bab
Renato Crivano <renato.crivano@gmail.com>
2023-01-26 13:49:14 -0300

    Corrigindo migration MySQL

--- siga-cp/src/main/resources/db/mysql/sigacp/V132.0__motivo-inativacao-ativacao-lotacao.sql
+++ siga-cp/src/main/resources/db/mysql/sigacp/V132.0__motivo-inativacao-ativacao-lotacao.sql
@@ -1,6 +1,6 @@
 -- Adicionado 2 campos para Persistência do Motivo da Inativação e Ativação. 
 -- Campos são separados devido a estrutura de versionamento permitir Ativar com Motivo e com a mesma linha Inativar com Motivo adicionando 
 
-ALTER TABLE `corporativo`.`dp_lotacao`
-ADD COLUMN `MOTIVO_INATIVACAO` VARCHAR(500) NULL AFTER `IS_SUSPENSA`,
-ADD COLUMN `MOTIVO_ATIVACAO` VARCHAR(500) NULL AFTER `MOTIVO_INATIVACAO`;
+ALTER TABLE corporativo.dp_lotacao
+ADD MOTIVO_INATIVACAO VARCHAR(500) NULL,
+ADD MOTIVO_ATIVACAO VARCHAR(500) NULL;

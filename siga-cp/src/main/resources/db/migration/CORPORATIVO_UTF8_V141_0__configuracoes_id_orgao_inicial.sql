0e1c040d463e15e6c4533cc9ab4ea762e2673ad8
Gisele <akemi_igami@hotmail.com>
2023-02-06 15:03:41 -0300

    RM151754 - Versionamento de orgão / configurações id inicial do orgão

---
+++ siga-cp/src/main/resources/db/migration/CORPORATIVO_UTF8_V141_0__configuracoes_id_orgao_inicial.sql
@@ -0,0 +1,14 @@
+--configurações ativas para o orgao inicial que precisam ser desativadas
+update corporativo.cp_configuracao set HIS_DT_FIM = sysdate, his_idc_fim = his_idc_ini where id_configuracao in (
+select id_configuracao from corporativo.cp_configuracao conf
+inner join corporativo.cp_orgao_usuario org on org.id_orgao_usu = conf.id_orgao_usu
+where conf.HIS_DT_FIM is null and id_orgao_usu_inicial = org.id_orgao_usu and id_orgao_usu_inicial in (
+select id_orgao_usu_inicial from corporativo.cp_orgao_usuario group by id_orgao_usu_inicial having count(1) > 1));
+
+--configurações de orgãos que já foram versionados que precisam converter o id_orgao_usu para id_inicial
+update corporativo.cp_configuracao c set id_orgao_usu = (select id_orgao_usu_inicial from corporativo.cp_orgao_usuario where id_orgao_usu = c.id_orgao_usu)
+where id_configuracao in (
+select id_configuracao from corporativo.cp_configuracao conf
+inner join corporativo.cp_orgao_usuario org on org.id_orgao_usu = conf.id_orgao_usu
+where conf.HIS_DT_FIM is null  and org.HIS_DT_FIM is null and id_orgao_usu_inicial <> org.id_orgao_usu and id_orgao_usu_inicial in (
+select id_orgao_usu_inicial from corporativo.cp_orgao_usuario group by id_orgao_usu_inicial having count(1) > 1));

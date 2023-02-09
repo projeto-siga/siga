ae75002014bd1fd9998f44b63bc241c7e501aa14
Gabriel Filipy da Silva Correa <gabriel.correa@apoioprodesp.sp.gov.br>
2023-02-09 11:18:15 -0300

    Refatorando a forma de acesso ao módulo da SECC.

--- siga/src/main/java/br/gov/jfrj/siga/vraptor/SeccController.java
+++ siga/src/main/java/br/gov/jfrj/siga/vraptor/SeccController.java
@@ -1,33 +1,24 @@
 package br.gov.jfrj.siga.vraptor;
 
-import java.util.HashMap;
-
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 
-import com.auth0.jwt.JWTSigner;
-
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Result;
-import br.com.caelum.vraptor.view.HttpResult;
-import br.com.caelum.vraptor.view.Results;
 import br.gov.jfrj.siga.base.Prop;
-import br.gov.jfrj.siga.base.SigaHTTP;
 import br.gov.jfrj.siga.dp.dao.CpDao;
 
 @Controller
 public class SeccController extends SigaController {
-	
+
 	/**
 	 * @deprecated CDI eyes only
 	 */
 	public SeccController() {
 		super();
 	}
-	
-	static final String PROVIDER_ISSUER = "secret";
 
 	@Inject
 	public SeccController(HttpServletRequest request, Result result, CpDao dao,
@@ -36,35 +27,8 @@ public class SeccController extends SigaController {
 	}
 	
 	@Get("app/secc/acesso")
-	public void modulo() {       
-		assertAcesso("SECC: Módulo serviço de compra e contratação");
-		final SigaHTTP http = new SigaHTTP();
-		String url = Prop.get("/secc.api.url");
-		String contentType = "application/json";
-		
-		String cookieDomain = Prop.get("/siga.jwt.cookie.domain");
-		final JWTSigner signer = new JWTSigner(Prop.get("/secc.jwt.secret"));
-		final HashMap<String, Object> claims = new HashMap<String, Object>();
-		final long iat = System.currentTimeMillis() / 1000L;
-		
-		final long exp = iat + (60 * 60L); 
-		claims.put("sub", getTitular().getSigla());
-		claims.put("nome", getTitular().getNomePessoa());
-		claims.put("aud", cookieDomain); 
-		claims.put("nbf", iat);
-		claims.put("lota", getLotaTitular().getSigla());
-		claims.put("exp", exp);
-		claims.put("iat", iat);
-		String token = signer.sign(claims);
-		
-		HashMap<String, String> headers = new HashMap<>();
-		headers.put("Authorization", "Bearer " + token);
-		String response = http.getNaWeb(url, headers, 60000, null);
-
-		HttpResult httpr = result.use(Results.http());
-		httpr.addHeader("Content-Type", contentType);
-		httpr.body(response).setStatusCode(200);
-		
+	public void pesquisa(String q) throws Exception {
+		result.redirectTo(Prop.get("/secc.ui.url"));
 	}
 	
 }

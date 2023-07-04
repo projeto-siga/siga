f1ab2b6d5fb29497a80c0fe7dc868dfe6a139d6c
Cássio Augusto Machado <cassio_0101@hotmail.com>
2023-07-04 19:46:26 -0300

    Método para juntar sigla_lotacao e preenchimento

--- sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/ModelosIdLotacoesIdLotacaoPreenchimentosGet.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/ModelosIdLotacoesIdLotacaoPreenchimentosGet.java
@@ -17,6 +17,8 @@ public class ModelosIdLotacoesIdLotacaoPreenchimentosGet implements IModelosIdLo
 		resp.list = listarPreenchimentos(Long.parseLong(req.id), Long.parseLong(req.idLotacao));	
 	}
 	
+	// TODO: agrupar os textos padrão por lotação ao invés de ordem alfabética
+	// TODO: mostrar a lotação antes do nome
 	// Lista todos os preenchimentos, primeiro os preenchimentos da lotação selecionada, depois os das outras
 	public static List<PreenchimentoItem> listarPreenchimentos(Long idModelo, Long idLotacao) {
 	    ExDao dao = ExDao.getInstance();
@@ -25,26 +27,53 @@ public class ModelosIdLotacoesIdLotacaoPreenchimentosGet implements IModelosIdLo
 	    ExPreenchimento filtro = new ExPreenchimento();
 	    filtro.setExModelo(mod);
 	    List<ExPreenchimento> l = dao.consultar(filtro);
+	    
+	    // variaveis para gravar os preenchimentos/textos-padrões
 	    List<PreenchimentoItem> list = new ArrayList<>();
 	    List<PreenchimentoItem> preenchimentosDeOutrasLotacoes = new ArrayList<>();
-
+	    
+	    // variaveis para gravar as siglas das lotações
+	    List siglas = new ArrayList<>();
+	    List siglasDeOutrasLotacoes = new ArrayList<>();
+	    
 	    for (ExPreenchimento i : l) {
 	        PreenchimentoItem item = new PreenchimentoItem();
 	        item.idPreenchimento = Long.toString(i.getIdPreenchimento());
 	        item.nome = i.getNomePreenchimento();
-
+	        
+	        // variaveis para gravar as siglas das lotações
+	        String siglaLotacao = new String();
+	        siglaLotacao = i.getDpLotacao().getSigla();
+	        
 	        // Se a lotação do preenchimento atual corresponder à lotação selecionada, adiciona à lista principal
 	        if (i.getDpLotacao().getId().equals(idLotacao)) {
+	        	//Pegar as siglas das  lotações selecionadas aqui e adicionar em uma lista
 	            list.add(item);
+	            siglas.add(siglaLotacao);
 	        } else {
+	        	//Pegar as siglas das outras lotações aqui e adicionar em uma lista
 	        	preenchimentosDeOutrasLotacoes.add(item);
+	        	siglasDeOutrasLotacoes.add(siglaLotacao);
 	        }
 	    }
 
 	    list.addAll(preenchimentosDeOutrasLotacoes);
-
+	    siglas.addAll(siglasDeOutrasLotacoes);
+	    
+	    //TODO: Concatenar no padrão SIGLA + Preenchimento para montar saida
+	    List<String> resultadoConcatenacao = concatenarSiglasComPreenchimentos(siglas, list);
+	    
+	    //resultado precisa ser do tipo PreenchimentoItem
 	    return list;
 	}
+	
+    public static List<String> concatenarSiglasComPreenchimentos(List<String> siglas, List<PreenchimentoItem> preenchimentos) {
+        List<String> resultado = new ArrayList<String>();
+        for(int i = 0; i < siglas.size(); i++) {
+            resultado.add(siglas.get(i) + " " + preenchimentos.get(i).nome);
+        }
+        return resultado;
+    }
 
 	@Override
 	public String getContext() {

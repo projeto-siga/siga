fd20c6ad09b7142ebda8f36a23e5d8fa2caa0e1b
Cássio Augusto Machado <cassio_0101@hotmail.com>
2023-07-06 15:12:48 -0300

    exibir descrição dos textos-padrão agrupados por lotação com a sigla da lotação na frente da descrição

--- sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/ModelosIdLotacoesIdLotacaoPreenchimentosGet.java
+++ sigaex/src/main/java/br/gov/jfrj/siga/ex/api/v1/ModelosIdLotacoesIdLotacaoPreenchimentosGet.java
@@ -1,6 +1,7 @@
 package br.gov.jfrj.siga.ex.api.v1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import br.gov.jfrj.siga.dp.DpLotacao;
@@ -14,14 +15,10 @@ public class ModelosIdLotacoesIdLotacaoPreenchimentosGet implements IModelosIdLo
 
 	@Override
 	public void run(Request req, Response resp, ExApiV1Context ctx) throws Exception {
-
-		//TODO: agrupar os textos padrão por lotação ao invés de ordem alfabética das descrições
-		List<PreenchimentoItem> preenchimentos = listarPreenchimentos(Long.parseLong(req.id), Long.parseLong(req.idLotacao));
-		resp.list = preenchimentos;
+		resp.list = listarPreenchimentos(Long.parseLong(req.id), Long.parseLong(req.idLotacao));
 	}
 	
 	
-	// Lista todos os preenchimentos, primeiro os preenchimentos da lotação selecionada, depois os das outras
 	public static List<PreenchimentoItem> listarPreenchimentos(Long idModelo, Long idLotacao) {
 	    ExDao dao = ExDao.getInstance();
 	    ExModelo mod = dao.consultar(idModelo, ExModelo.class, false);
@@ -29,67 +26,36 @@ public class ModelosIdLotacoesIdLotacaoPreenchimentosGet implements IModelosIdLo
 	    ExPreenchimento filtro = new ExPreenchimento();
 	    filtro.setExModelo(mod);
 	    List<ExPreenchimento> l = dao.consultar(filtro);
-	    List<PreenchimentoItem> list = new ArrayList<>();
-	    List<PreenchimentoItem> preenchimentosDeOutrasLotacoes = new ArrayList<>();
+
+	    List<PreenchimentoItem> lotacaoSelecionadaList = new ArrayList<>();
+	    HashMap<Long, List<PreenchimentoItem>> lotacoesMap = new HashMap<>();
 	    for (ExPreenchimento i : l) {
 	        PreenchimentoItem item = new PreenchimentoItem();
 	        item.idPreenchimento = Long.toString(i.getIdPreenchimento());
 	        String nome = i.getNomePreenchimento();
 	        String siglaLotacaoEPreenchimento = i.getSiglaDaLotacaoENomedoPreenchimento();
 	        item.nome = siglaLotacaoEPreenchimento;
+
+	        Long idLotacaoAtual = i.getDpLotacao().getId();
 	        
-	        // Se a lotação do preenchimento atual corresponder à lotação selecionada, adiciona à lista principal
-	        if (i.getDpLotacao().getId().equals(idLotacao)) {
-	            list.add(item);
+	        if (idLotacaoAtual.equals(idLotacao)) {
+	            lotacaoSelecionadaList.add(item);
 	        } else {
-	        	preenchimentosDeOutrasLotacoes.add(item);
-	        }
-	    }
-	    
-	    list.addAll(preenchimentosDeOutrasLotacoes);
-	    return list;
-	}
-	
-	
-	private List<String> listarLotacoes(long idModelo, long idLotacao) {
-	    ExDao dao = ExDao.getInstance();
-	    ExModelo mod = dao.consultar(idModelo, ExModelo.class, false);
-	    mod = dao.consultar(mod.getIdInicial(), ExModelo.class, false);
-	    ExPreenchimento filtro = new ExPreenchimento();
-	    filtro.setExModelo(mod);
-	    List<ExPreenchimento> l = dao.consultar(filtro);
-	    //List<PreenchimentoItem> list = new ArrayList<>();
-	    //List<PreenchimentoItem> preenchimentosDeOutrasLotacoes = new ArrayList<>();
-	    
-	    // variaveis para gravar as siglas das lotações
-	    List siglas = new ArrayList<>();
-	    List siglasDeOutrasLotacoes = new ArrayList<>();
-	    
-	    for (ExPreenchimento i : l) {
-	        PreenchimentoItem item = new PreenchimentoItem();
-	        item.idPreenchimento = Long.toString(i.getIdPreenchimento());
-	        item.nome = i.getNomePreenchimento();
-	        
-	        // variaveis para gravar as siglas das lotações
-	        String siglaLotacao = new String();
-	        siglaLotacao = i.getDpLotacao().getSigla();
-	        
-	        // Se a lotação do preenchimento atual corresponder à lotação selecionada, adiciona à lista principal
-	        if (i.getDpLotacao().getId().equals(idLotacao)) {
-	        	//Pegar as siglas das  lotações selecionadas aqui e adicionar em uma lista
-	            //list.add(item);
-	            siglas.add(siglaLotacao);
-	        } else {
-	        	//Pegar as siglas das outras lotações aqui e adicionar em uma lista
-	        	//preenchimentosDeOutrasLotacoes.add(item);
-	        	siglasDeOutrasLotacoes.add(siglaLotacao);
+	            if (!lotacoesMap.containsKey(idLotacaoAtual)) {
+	                lotacoesMap.put(idLotacaoAtual, new ArrayList<>());
+	            }
+	            lotacoesMap.get(idLotacaoAtual).add(item);
 	        }
 	    }
 
-	    siglas.addAll(siglasDeOutrasLotacoes);
-	    
-	    return siglas;
+	    List<PreenchimentoItem> list = new ArrayList<>();
+	    list.addAll(lotacaoSelecionadaList);
+	    for (Long id : lotacoesMap.keySet()) {
+	        list.addAll(lotacoesMap.get(id));
+	    }
+	    return list;
 	}
+
 	
 
 	@Override

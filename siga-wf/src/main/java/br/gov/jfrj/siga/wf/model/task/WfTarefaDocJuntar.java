8e0c453bb2ddf088114ad8c1f2bcd08ad213fdb3
Cássio Augusto Machado <cassio_0101@hotmail.com>
2023-08-15 19:21:14 -0300

    WF tarefa juntar doc principal no doc informado pelo usuário. Opção documento pai fixo(número preenchido na elaboração do diagrama)

--- siga-wf/src/main/java/br/gov/jfrj/siga/wf/model/task/WfTarefaDocJuntar.java
+++ siga-wf/src/main/java/br/gov/jfrj/siga/wf/model/task/WfTarefaDocJuntar.java
@@ -14,67 +14,71 @@ import br.gov.jfrj.siga.wf.model.WfDefinicaoDeTarefa;
 import br.gov.jfrj.siga.wf.model.WfProcedimento;
 import br.gov.jfrj.siga.wf.util.WfResp;
 
-public class WfTarefaDocJuntar implements Task<WfDefinicaoDeTarefa, WfProcedimento> {
-	/*TODO: 
-	   []1 - A nova tarefa deve juntar o documento principal no documento informado pelo usuário.
- 	   []2 - O documento pai pode ser fixo (número preenchido na elaboração do diagrama) ou
- 	   []3 - Obtido através do valor de uma variável váriável do WF (incluindo documentos criados pelo próprio workflow) ou campo constante de algum documento juntado ao principal.
- 	   []3 - Também deve ser possível informar se o documento pai deve se tornar ou não o novo Principal do WF. 
+public class WfTarefaDocJuntar implements Task<WfDefinicaoDeTarefa, WfProcedimento> {	
+	/**
+	 * Esse método é responsável por Juntar 2 documentos recebidos na tarefa Juntar(Em um Workflow)
+	 * Junta o documento principal(Documento Filho) no documento informado pelo usuário(Documento Pai).
+	 * O documento pai é fixo (número preenchido na elaboração do diagrama)
+	 *
+	 * @param td Tarefa.
+	 * @param pi Procedimento.
+	 * @return TaskResult Resultado da execução da tarefa.
 	 */
-	//TODO: Diminuir tamanho do campo no front-end
-	//TODO: Refatoração - apagar comentários e códigos desnecessários
 	@Override
 	public TaskResult execute(WfDefinicaoDeTarefa td, WfProcedimento pi, Engine engine) throws Exception {
 		String siglaDestino = "";
 		String siglaCadastrante = "";
-		String codigoDocumentoViaFilho = "";
-		String codigoDocumentoViaPai = "";
-			
-		//TODO: RECEBE O DOCUMENTO PAI DIGITADO PELO USUÁRIO
-		//String text = engine.getHandler().evalTemplate(pi, td.getText());
-		String documento_pai = engine.getHandler().evalTemplate(pi, td.getSubject());
-		//TODO: Corrigir erro: text recebeu teste 09-08-23, tem que RECEBER DOCUMENTO FILHO
-		/*
-		 * No campo Tipo de resolponsável, a opção cadastrante retorna o que deverá sar cadastrado no [siglaCadastrante]
-		 * No campo Tipo de resolponsável, a opção lotaçãop do titular o que deverá sar cadastrado no [siglaResponsável]
-		 */
-		//DpPessoa titular = pi.getTitular();
-		//DpLotacao lotacaoDoTitular = pi.getLotaTitular();
-		//if (!Utils.empty(pi.getPrincipal())) { //pi.getprincipal = documento principal: OTZZ-MEM-2023/00004-A
+		String codigoDocumentoPai = "";
+		String codigoDocumentoPrincipal = "";
+		String codigoDocumentoFilho = "";
 		
-		String documentoPrincipal = pi.getPrincipal(); //pi.getprincipal = documento principal: OTZZ-MEM-2023/00004-A	
-		if (!Utils.empty(documentoPrincipal)) { 
-			WfResp destino = pi.calcResponsible(td);
+		//String codigoDocumentoViaPai = "";
+		//String documento_pai = engine.getHandler().evalTemplate(pi, td.getSubject());
+		//String codigoDocumentoPai = engine.getHandler().evalTemplate(pi, td.getSubject());
+		codigoDocumentoPai = recebeDocumentoPaiDaEntradaDoUsuario(td, pi, engine);
+		codigoDocumentoPrincipal = pi.getPrincipal(); //pi.getprincipal = documento principal: OTZZ-MEM-2023/00004-A	
+		codigoDocumentoFilho = codigoDocumentoPrincipal;
+		
+		if (!Utils.empty(codigoDocumentoPrincipal)) { 
+			WfResp responsavel = pi.calcResponsible(td);
+			//String sigla = geraSigla(destino);
+			//siglaCadastrante = siglaDestino;
+			siglaDestino = geraSiglaDoResponsavel(responsavel);	// siglaDestino = ZZLTEST
+			siglaCadastrante = geraSiglaDoResponsavel(responsavel); // siglaCadastrante = ZZLTEST
+			//codigoDocumentoViaFilho = "OTZZ-MEM-2023/00004-A"; //codigoDocumentoViaFilho = OTZZ-MEM-2023/00005-A // String codigoDocumentoViaFilho = documentoPrincipal;
+			//codigoDocumentoViaPai = documento_pai; // codigoDocumentoPai = OTZZ-MEM-2023/00005-A
+			//codigoDocumentoViaPai = pi.getPrincipal();
 			
-			if (destino != null) { 
-				//siglaDestino = SiglaParser.makeSigla(destino.getPessoa(), destino.getLotacao());
-				DpPessoa pessoa = destino.getPessoa(); // veio null
-				DpLotacao lotacao = destino.getLotacao(); // veio null
-				//siglaDestino = SiglaParser.makeSigla(pessoa, lotacao); //sigladestino = @ZZLTEST
-				try {
-				    siglaDestino = SiglaParser.makeSigla(pessoa, lotacao);
-				} catch (NullPointerException npe) {
-					siglaDestino = "";
-					}
-				}
-			// String codigoDocumentoViaFilho = documentoPrincipal;
-			//siglaDestino = "@ZZLTEST";
-			siglaCadastrante = siglaDestino;
-			codigoDocumentoViaFilho = "OTZZ-MEM-2023/00004-A"; // de onde vem doc filho?
-			codigoDocumentoViaPai = documento_pai;
 			
-			//Esse código deverá receber o documento selecionado pelo usuário em um combobox
-			codigoDocumentoViaPai = pi.getPrincipal();
-			//codigoDocumentoViaPai = "OTZZ-MEM-2023/00005-A";   // de onde vem doc pai?
-			//assert siglaDestino != null;
-			/*
-			 * codigoDocumentoPai = OTZZ-MEM-2023/00004-A
-			 * codigoDocumentoViaFilho = OTZZ-MEM-2023/00005-A
-			 * siglaDestino = ZZLTEST
-			 * siglaCadastrante = ZZLTEST
-			 */
-			Service.getExService().juntar(codigoDocumentoViaFilho, codigoDocumentoViaPai, siglaDestino, siglaCadastrante);
+			Service.getExService().juntar(codigoDocumentoFilho, codigoDocumentoPai, siglaDestino, siglaCadastrante);
 		}
 		return new TaskResult(TaskResultKind.DONE, null, null, null, null);
 	}
+	
+	private String geraSiglaDoResponsavel(WfResp destino) {
+		String siglaDestino = "";
+		if (destino != null) { 
+			DpPessoa pessoa = destino.getPessoa();
+			DpLotacao lotacao = destino.getLotacao(); //siglaDestino = SiglaParser.makeSigla(pessoa, lotacao); //sigladestino = @ZZLTEST
+			try {
+			    siglaDestino = SiglaParser.makeSigla(pessoa, lotacao); //siglaDestino = "@ZZLTEST";
+			} catch (NullPointerException npe) {
+				siglaDestino = "";
+				}
+			}
+		return siglaDestino;
+	}
+	
+	private String recebeDocumentoPaiDaEntradaDoUsuario(WfDefinicaoDeTarefa td, WfProcedimento pi, Engine engine) {
+	    try {
+	        return engine.getHandler().evalTemplate(pi, td.getSubject());
+	    } catch (NullPointerException npe) {
+	        System.err.println("Erro: Um valor nulo foi encontrado.");
+	        return null;
+	    } catch (Exception e) {
+	        System.err.println("Erro ao avaliar o template: " + e.getMessage());
+	        return null;
+	    }
+	}
+
 }

6a9101141d9d9606432f70aacfc636448decd893
Renato Crivano <renato.crivano@gmail.com>
2023-08-28 17:30:14 -0300

    Permitindo concluir notificação quando o documento está juntado

--- siga-rest-test/src/test/java/br/gov/jfrj/siga/ex/api/v1/unit/Notificar.java
+++ siga-rest-test/src/test/java/br/gov/jfrj/siga/ex/api/v1/unit/Notificar.java
@@ -59,4 +59,115 @@ public class Notificar extends DocTest {
 
     }
 
+    @Test
+    public void test_NotificarLotacaoEArquivar_OK() {
+        String sigla = Criar.criarMemorando(Pessoa.ZZ99999);
+
+//        notificar(Pessoa.ZZ99999, sigla, Lotacao.ZZLTEST2);
+        notificar(Pessoa.ZZ99999, sigla, Pessoa.ZZ99998);
+        notificar(Pessoa.ZZ99999, sigla, Pessoa.ZZ99997);
+
+        ArquivarCorrente.arquivar(Pessoa.ZZ99999, sigla);
+
+        consultar(Pessoa.ZZ99999, sigla);
+//        contemMarca(CpMarcadorEnum.CAIXA_DE_ENTRADA, Lotacao.ZZLTEST2);
+        contemMarca(CpMarcadorEnum.CAIXA_DE_ENTRADA, Pessoa.ZZ99998);
+        contemAcao("arquivar_corrente_gravar", false);
+        contemAcao("reabrir_gravar", true);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", false);
+        contemVizNode("vizTramitacao", Lotacao.ZZLTEST.name(), "oval", "red");
+//        contemVizNode("vizTramitacao", Lotacao.ZZLTEST2.name(), "rectangle", "red");
+        contemVizNode("vizTramitacao", Pessoa.ZZ99998.name(), "rectangle", "red");
+
+        consultar(Pessoa.ZZ99998, sigla);
+        contemAcao("receber", true);
+        contemAcao("concluir_gravar", false);
+
+        consultar(Pessoa.ZZ99997, sigla);
+        contemAcao("receber", true);
+        contemAcao("concluir_gravar", false);
+
+        Receber.receber(Pessoa.ZZ99998, sigla);
+        consultar(Pessoa.ZZ99998, sigla);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", true);
+
+        Receber.receber(Pessoa.ZZ99997, sigla);
+        consultar(Pessoa.ZZ99997, sigla);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", true);
+
+    }
+
+    @Test
+    public void test_TramitarDevolverNotificarArquivarReceber_OK() {
+        String sigla = Criar.criarMemorando(Pessoa.ZZ99999);
+        Tramitar.tramitarParaLotacao(Pessoa.ZZ99999, sigla, Lotacao.ZZLTEST3);
+        Receber.receber(Pessoa.ZZ99997, sigla);
+        Tramitar.tramitarParaLotacao(Pessoa.ZZ99997, sigla, Lotacao.ZZLTEST);
+        Receber.receber(Pessoa.ZZ99999, sigla);
+
+        notificar(Pessoa.ZZ99999, sigla, Lotacao.ZZLTEST2);
+
+        ArquivarCorrente.arquivar(Pessoa.ZZ99999, sigla);
+
+        consultar(Pessoa.ZZ99999, sigla);
+        contemMarca(CpMarcadorEnum.CAIXA_DE_ENTRADA, Lotacao.ZZLTEST2);
+        contemAcao("arquivar_corrente_gravar", false);
+        contemAcao("reabrir_gravar", true);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", false);
+        contemVizNode("vizTramitacao", Lotacao.ZZLTEST.name(), "oval", "red");
+        contemVizNode("vizTramitacao", Lotacao.ZZLTEST2.name(), "rectangle", "red");
+
+        consultar(Pessoa.ZZ99998, sigla);
+        contemAcao("receber", true);
+        contemAcao("concluir_gravar", false);
+
+        Receber.receber(Pessoa.ZZ99998, sigla);
+        consultar(Pessoa.ZZ99998, sigla);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", true);
+        
+        Concluir.concluir(Pessoa.ZZ99998, sigla);
+        consultar(Pessoa.ZZ99998, sigla);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", false);
+    }
+
+    @Test
+    public void test_NotificarJuntarReceberConcluir_OK() {
+        String siglaPai = Criar.criarMemorando(Pessoa.ZZ99999);
+        String sigla = Criar.criarMemorando(Pessoa.ZZ99999);
+
+        notificar(Pessoa.ZZ99999, sigla, Lotacao.ZZLTEST2);
+
+        Juntar.juntar(Pessoa.ZZ99999, sigla, siglaPai);
+
+        consultar(Pessoa.ZZ99999, sigla);
+        contemMarca(CpMarcadorEnum.CAIXA_DE_ENTRADA, Lotacao.ZZLTEST2);
+        contemAcao("arquivar_corrente_gravar", false);
+        contemAcao("reabrir_gravar", false);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", false);
+        contemVizNode("vizTramitacao", Lotacao.ZZLTEST.name(), "oval", "red");
+        contemVizNode("vizTramitacao", Lotacao.ZZLTEST2.name(), "rectangle", "red");
+
+        consultar(Pessoa.ZZ99998, sigla);
+        contemAcao("receber", true);
+        contemAcao("concluir_gravar", false);
+
+        Receber.receber(Pessoa.ZZ99998, sigla);
+        consultar(Pessoa.ZZ99998, sigla);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", true);
+        
+        Concluir.concluir(Pessoa.ZZ99998, sigla);
+        consultar(Pessoa.ZZ99998, sigla);
+        contemAcao("receber", false);
+        contemAcao("concluir_gravar", false);
+        
+    }
+
 }

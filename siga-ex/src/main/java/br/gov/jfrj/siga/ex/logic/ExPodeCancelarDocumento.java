0a57d72ff4aa4dd5fc5416c09150332098bb892b
Adriana <adrigrassi@gmail.com>
2024-07-16 18:49:48 -0300

    DOC Correção no Finalizar para contemplar Migração para o SEI

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/logic/ExPodeCancelarDocumento.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/logic/ExPodeCancelarDocumento.java
@@ -52,7 +52,7 @@ public class ExPodeCancelarDocumento extends CompositeExpressionSupport {
 
 				Or.of(Not.of(new ExEEletronico(doc)), new ExEstaPendenteDeAssinatura(doc)),
 				
-				Not.of(new ExEstaMigradoSEI(doc.getUltimoVolume())),
+				And.of(new ExEProcesso(doc), Not.of(new ExEstaMigradoSEI(doc.getUltimoVolume()))),
 
 				If.of(new ExECapturado(doc),
 
